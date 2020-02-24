e39d14235c133a97a7a1534f0227955a8a579a30
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-02-24 13:56:13 -0800

    Create input.tf

---
+++ caas/kubernetes/pod/input.tf
@@ -0,0 +1,9 @@
+variable "error_threshold_critical" {
+	default = 0.5
+	description = "error critical threshold"
+}
+
+variable "terminated_threshold_critical" {
+	default = 0.5
+	description = "terminated critical threshold"
+}
