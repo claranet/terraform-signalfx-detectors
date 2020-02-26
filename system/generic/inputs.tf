c9117d49aeb3e4a10f0437162202db02afc2c55d
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-02-26 07:45:55 -0800

    Update inputs.tf

--- system/generic/inputs.tf
+++ system/generic/inputs.tf
@@ -1,42 +1,57 @@
 # System generic specific
 
 variable "cpu_threshold_warning" {
-  description = "CPU high warning threshold"
-  default     = 85
+	description = "CPU high warning threshold"
+	default = 85
 }
 
 variable "cpu_threshold_critical" {
-  description = "CPU high critical threshold"
-  default     = 90
+	description = "CPU high critical threshold"
+	default = 90
+}
+
+variable "load_timeframe" {
+	description = "Monitor timeframe in Second(s), Minute(m), Hour(h), Day(d)"
+	default = "30m"
 }
 
 variable "load_threshold_warning" {
-  description = "CPU load ratio warning threshold"
-  default     = 2
+	description = "CPU load ratio warning threshold"
+	default = 2
 }
 
 variable "load_threshold_critical" {
-  description = "CPU load ratio critical threshold"
-  default     = 2.5
+	description = "CPU load ratio critical threshold"
+	default = 2.5
+}
+
+variable "disk_space_timeframe" {
+  description = "Monitor timeframe in Second(s), Minute(m), Hour(h), Day(d)"
+  default = "5m"
 }
 
 variable "disk_space_threshold_warning" {
-  description = "Free disk space warning threshold"
-  default     = 80
+	description = "Free disk space warning threshold"
+	default = 80
 }
 
 variable "disk_space_threshold_critical" {
-  description = "Free disk space critical threshold"
-  default     = 90
+	description = "Free disk space critical threshold"
+	default = 90
+}
+
+variable "memory_timeframe" {
+  description = "Monitor timeframe in Second(s), Minute(m), Hour(h), Day(d)"
+  default = "5m"
 }
 
 variable "memory_threshold_warning" {
-  description = "Free disk space warning threshold"
-  default     = 10
+	description = "Free disk space warning threshold"
+	default = 10
 }
 
 variable "memory_threshold_critical" {
-  description = "Free disk space critical threshold"
-  default     = 5
+	description = "Free disk space critical threshold"
+	default = 5
 }
 
