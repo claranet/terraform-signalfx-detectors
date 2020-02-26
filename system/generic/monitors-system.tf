b47c211ffc3ea43e74ad22c0b922e778b3d1a82e
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-02-26 07:46:21 -0800

    Update monitors-system.tf

--- system/generic/monitors-system.tf
+++ system/generic/monitors-system.tf
@@ -17,12 +17,12 @@ resource "signalfx_detector" "load" {
 	name = "CPU load 5 ratio"
 
 	program_text = <<-EOF
-		signal = data('load.midterm').mean(by=['host']).min(over='30m')
+		signal = data('load.midterm').mean(by=['host']).min(over='${var.load_timeframe}')
 		detect(when(signal > ${var.load_threshold_critical})).publish('CRIT')
 	EOF
 
 	rule {
-		description = "Min > ${var.load_threshold_critical} for last 30m"
+		description = "Min > ${var.load_threshold_critical} for last ${var.load_timeframe}"
 		severity = "Critical"
 		detect_label = "CRIT"
 	}
@@ -32,12 +32,12 @@ resource "signalfx_detector" "disk_space" {
 	name = "Disk space usage"
 
 	program_text = <<-EOF
-		signal = data('disk.utilization').max(by=['host']).max(over='5m')
+		signal = data('disk.utilization').max(by=['host']).max(over='${var.disk_space_timeframe}')
 		detect(when(signal > ${var.disk_space_threshold_critical})).publish('CRIT')
 	EOF
 
 	rule {
-		description = "Max > ${var.disk_space_threshold_critical} for last 5m"
+		description = "Max > ${var.disk_space_threshold_critical} for last ${var.disk_space_timeframe}"
 		severity = "Critical"
 		detect_label = "CRIT"
 	}
@@ -48,12 +48,12 @@ resource "signalfx_detector" "memory" {
 
 	program_text = <<-EOF
 		A = data('memory.utilization').mean(by=['host'])
-		signal = (100-A).max(over='5m')
+		signal = (100-A).max(over='${var.memory_timeframe}')
 		detect(when(signal > ${var.memory_threshold_critical})).publish('CRIT')
 	EOF
 
 	rule {
-		description = "Max > ${var.memory_threshold_critical} for last 5m"
+		description = "Max > ${var.memory_threshold_critical} for last ${var.memory_timeframe}"
 		severity = "Critical"
 		detect_label = "CRIT"
 	}
