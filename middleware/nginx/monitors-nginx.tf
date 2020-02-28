9f8d37734c06f0488110e9203136c0496c635a1e
mel-splunk <58855914+mel-splunk@users.noreply.github.com>
2020-02-28 06:21:12 -0800

    Update monitors-nginx.tf
    added nginx heaertbeat detector

--- middleware/nginx/monitors-nginx.tf
+++ middleware/nginx/monitors-nginx.tf
@@ -1,3 +1,20 @@
+resource "signalfx_detector" "nginx_heartbeat" {
+	name = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] System Heartbeat Check"
+
+	program_text = <<-EOF
+		from signalfx.detectors.not_reporting import not_reporting
+		signal = data('nginx_requests', filter=(not filter('aws_state', '*terminated}', '*stopped}')) and (not filter('gcp_status', '*TERMINATED}', '*STOPPING}')) and (not filter('azure_power_state', 'stop*', 'deallocat*')))
+		not_reporting.detector(stream=signal, resource_identifier=['host'], duration='${var.nginx_heartbeat_timeframe}').publish('CRIT')
+	EOF
+
+	rule {
+		description = "System has not reported in ${var.nginx_heartbeat_timeframe}"
+		severity = "Critical"
+		detect_label = "CRIT"
+		disabled = "${var.nginx_heartbeat_disabled_flag}"
+	}
+}
+
 resource "signalfx_detector" "nginx_dropped_connections" {
 	name = "Nginx dropped connections"
 
