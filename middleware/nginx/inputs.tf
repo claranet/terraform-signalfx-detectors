20e1522bdd60d34e1178f328e4327c99a5e62f1d
mel-splunk <58855914+mel-splunk@users.noreply.github.com>
2020-02-28 05:59:26 -0800

    Update inputs.tf
    Add nginx heartbeat variables

--- middleware/nginx/inputs.tf
+++ middleware/nginx/inputs.tf
@@ -1,4 +1,29 @@
+#Global
+
+variable "environment" {
+	description = "Architecture Environment"
+	type = string
+}
+
+#SignalFx Specific
+
+variable "prefix_slug" {
+	description = "Prefix string to prepend between brackets on every monitors names"
+	default = ""
+}
+
+
 # Nginx Detector specific
+
+variable "nginx_heartbeat_disabled_flag" {
+	description = "(Optional) When true, notifications and events will not be generated for the detect label. false by default."
+	default = "false"
+}
+
+variable "nginx_heartbeat_timeframe" {
+	description = "Timeframe for system not reporting"
+	default = "20m"
+}
 
 variable "nginx_dropped_threshold_critical" {
 	description = "Nginx dropped connections critical threshold"
