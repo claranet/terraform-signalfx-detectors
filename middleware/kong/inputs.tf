61532407b3db49cd2dc45f7ce6342e4fc88f57b6
mel-splunk <58855914+mel-splunk@users.noreply.github.com>
2020-02-28 05:44:35 -0800

    Update inputs.tf
    Add Kong heartbeat, prefix_slug and environment variables

--- middleware/kong/inputs.tf
+++ middleware/kong/inputs.tf
@@ -1,4 +1,28 @@
+# Global
+
+variable "environment" {
+	description = "Architecture Environment"
+	type = string
+}
+
+# SignalFx specific
+
+variable "prefix_slug" {
+	description = "Prefix string to prepend between brackets on every monitors names"
+	default = ""
+}
+
 # Kong Detector specific
+
+variable "kong_service_heartbeat_timeframe" {
+	description = "Timeframe Kong Server has not reported for"
+	default = 20
+}
+
+variable "kong_service_heartbeat_disabled_flag" {
+	description = "Timeframe Kong Server has not reported for"
+	default = 20
+}
 
 variable "treatment_limit_threshold_critical" {
 	description = "Docker Container memory usage critical threshold"
