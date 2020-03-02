5b727eae986545640791ac11493bcaf4604cc024
mel-splunk <58855914+mel-splunk@users.noreply.github.com>
2020-03-02 10:44:15 -0800

    Update inputs.tf

--- common/filter-tags/inputs.tf
+++ common/filter-tags/inputs.tf
@@ -8,12 +8,12 @@ variable "filter_defaults" {
   default     = "filter('env', '${var.environment}') and filter('claranet_monitored', 'true')"
 }
 
-variable "filter_custom_include" {
+variable "filter_custom_includes" {
   description = "Tags to filter signals on when custom filtering is used. i.e [\"tag1:val1\", \"tag2:val2\"] "
   default     = []
 }
 
-variable "filter_custom_exclude" {
+variable "filter_custom_excludes" {
   description = "Tags to exclude when using custom filtering. i.e [\"tag1:val1\", \"tag2:val2\"] "
   default     = []
 }
