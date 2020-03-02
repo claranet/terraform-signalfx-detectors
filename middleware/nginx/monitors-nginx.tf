e2525c436dcd0f4d5d403d4cd2d40b0282bf7421
mel-splunk <58855914+mel-splunk@users.noreply.github.com>
2020-03-02 03:18:45 -0800

    implement new features like filters module and notifications

--- middleware/nginx/monitors-nginx.tf
+++ middleware/nginx/monitors-nginx.tf
@@ -3,7 +3,7 @@ resource "signalfx_detector" "nginx_heartbeat" {
 
 	program_text = <<-EOF
 		from signalfx.detectors.not_reporting import not_reporting
-		signal = data('nginx_requests', filter=(not filter('aws_state', '*terminated}', '*stopped}')) and (not filter('gcp_status', '*TERMINATED}', '*STOPPING}')) and (not filter('azure_power_state', 'stop*', 'deallocat*')))
+		signal = data('nginx_requests', filter=(not filter('aws_state', '*terminated}', '*stopped}')) and (not filter('gcp_status', '*TERMINATED}', '*STOPPING}')) and (not filter('azure_power_state', 'stop*', 'deallocat*')) and ${module.filter-tags.filter_custom})
 		not_reporting.detector(stream=signal, resource_identifier=['host'], duration='${var.nginx_heartbeat_timeframe}').publish('CRIT')
 	EOF
 
@@ -11,7 +11,8 @@ resource "signalfx_detector" "nginx_heartbeat" {
 		description = "System has not reported in ${var.nginx_heartbeat_timeframe}"
 		severity = "Critical"
 		detect_label = "CRIT"
-		disabled = "${var.nginx_heartbeat_disabled_flag}"
+		disabled = coalesce(var.nginx_heartbeat_disabled_flag,var.disable_detectors)
+                notifications = coalesce(split(";",var.nginx_heartbeat_notifications),split(";",var.notifications))
 	}
 }
 
@@ -19,15 +20,24 @@ resource "signalfx_detector" "nginx_dropped_connections" {
 	name = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] Nginx dropped connections"
 
 	program_text = <<-EOF
-		signal = data('connections.failed'"${var.nginx_filter == "" ? "" : "${var.nginx_filter}"}")${var.nginx_aggregation_function}.${var.nginx_transformation_function}(over='${var.nginx_transformation_window}')
+		signal = data('connections.failed', filter=${module.filter-tags.filter_custom})${var.nginx_aggregation_function}.${var.nginx_transformation_function}(over='${var.nginx_transformation_window}')
 		detect(when(signal > ${var.nginx_threshold_critical})).publish('CRIT')
+		detect(when(signal > ${var.nginx_threshold_warning})).publish('WARN')
 	EOF
 
 	rule {
-		description = "${var.nginx_transformation_function} nginx dropped over ${var.nginx_transformation_window} > ${var.nginx_threshold_critical}"
+		description = "${var.nginx_transformation_function} nginx dropped connections over ${var.nginx_transformation_window} > ${var.nginx_threshold_critical}"
 		severity = "Critical"
 		detect_label = "CRIT"
-		disabled = var.nginx_critical_disabled_flag
+		disabled = coalesce(var.nginx_critical_disabled_flag,var.disable_nginx_detector,var.disable_detectors)
+		notifications = coalesce(split(";",var.nginx_critical_notifications),split(";",var.nginx_notifications),split(";",var.notifications))
 	}
-
+        
+		rule {
+		description = "${var.nginx_transformation_function} nginx dropped connections over ${var.nginx_transformation_window} > ${var.nginx_threshold_warning}"
+		severity = "Warning"
+		detect_label = "WARN"
+		disabled = coalesce(var.nginx_warning_disabled_flag,var.disable_nginx_detector,var.disable_detectors)
+		notifications = coalesce(split(";",var.nginx_warning_notifications),split(";",var.nginx_notifications),split(";",var.notifications))
+	}
 }
