a39d2b12a32d47a3ff04603e7dc888fed900384a
mel-splunk <58855914+mel-splunk@users.noreply.github.com>
2020-03-03 03:25:21 -0800

    Update outputs.tf
    validate's in terraform

--- common/filter-tags/outputs.tf
+++ common/filter-tags/outputs.tf
@@ -37,26 +37,3 @@ output "filter_custom" {
      )}"
    
 }   
-      
-
-   /*formatted_filter_custom_includes = "{${replace(var.filter_custom_includes, ":", "', '")}}"
-       # "aws_state', 'stopped;aws_region', 'eu-west1" 
-
-    list_filter_custom_includes = "{${split(";", var.formatted_filter_custom_includes)}}"
-       ["aws_state', 'stopped", "aws_region', 'eu-west1"] 
-
-     formatted_list_filter_custom_includes = "{${(formatlist("filter(', %s')", var.list_filter_custom_includes)}}"
-     ["filter('aws_state', 'stopped')", "filter('aws_region', 'eu-west1')"] 
-        
-    formatted_filter_custom_excludes = "{${replace(var.filter_custom_excludes, ":", "', '")}}"
-      "aws_state', 'stopped;aws_region', 'eu-west1" 
-
-    list_filter_custom_excludes = "{${split(";", var.formatted_filter_custom_excludes)}}"
-     ["aws_state', 'stopped", "aws_region', 'eu-west1"] 
-
-    formatted_list_filter_custom_excludes = "{${formatlist("(not filter(', %s'))", var.list_filter_custom_excludes)}}"
-     ["(not filter('aws_state', 'stopped'))", "(not filter('aws_region', 'eu-west1'))"]   */    
-
-
-        
-        
