d49c1f23a470da41876e3d43af95eb30ccf0ad61
mel-splunk <58855914+mel-splunk@users.noreply.github.com>
2020-03-03 02:42:31 -0800

    Update outputs.tf

--- common/filter-tags/outputs.tf
+++ common/filter-tags/outputs.tf
@@ -4,35 +4,38 @@ output "filter_custom" {
       if var.filter_use_defaults = "true"  
       value  = "${var.filter_defaults}"
     else
-      
-    formatted_filter_custom_includes = "{${replace(var.filter_custom_includes, ":", "', '")}}"
-      /* # "aws_state', 'stopped;aws_region', 'eu-west1" */
-
-    list_filter_custom_includes = "{${split(";", var.formatted_filter_custom_includes)}}"
-    /* ["aws_state', 'stopped", "aws_region', 'eu-west1"] */
-
-    formatted_list_filter_custom_includes = "{${formatlist("filter(', %s')", var.list_filter_custom_includes)}}"
-    /* ["filter('aws_state', 'stopped')", "filter('aws_region', 'eu-west1')"] */
-        
-    formatted_filter_custom_excludes = "{${replace(var.filter_custom_excludes, ":", "', '")}}"
-    /*  "aws_state', 'stopped;aws_region', 'eu-west1" */
-
-    list_filter_custom_excludes = "{${split(";", var.formatted_filter_custom_excludes)}}"
-    /* ["aws_state', 'stopped", "aws_region', 'eu-west1"] */
-
-    formatted_list_filter_custom_excludes = "{${formatlist("(not filter(', %s'))", var.list_filter_custom_excludes)}}"
-    /* ["(not filter('aws_state', 'stopped'))", "(not filter('aws_region', 'eu-west1'))"]   */    
     
    custum_filter = "${join(
      " and ", 
      list(
        join(
           " and ", 
-           var.formatted_list_filter_custom_includes), 
-        join(
+           formatlist(
+              "filter(', %s')", 
+              split(
+                ";", 
+                 replace(
+                    var.filter_custom_includes,
+                    ":",
+                    "', '"
+                 )
+              )
+           )
+       ), 
+       join(
           " and ", 
-          var.formatted_list_filter_custom_excludes)
-      )
+          formatlist(
+            "(not filter(', %s'))", 
+            split(
+              ";", 
+              replace(
+                var.filter_custom_excludes,
+                ":",
+                "', '"
+                )
+            )
+          )
+       )
 
      )}"
    
@@ -41,7 +44,23 @@ output "filter_custom" {
       
    
 
-  
+   /*formatted_filter_custom_includes = "{${replace(var.filter_custom_includes, ":", "', '")}}"
+       # "aws_state', 'stopped;aws_region', 'eu-west1" 
+
+    list_filter_custom_includes = "{${split(";", var.formatted_filter_custom_includes)}}"
+       ["aws_state', 'stopped", "aws_region', 'eu-west1"] 
+
+     formatted_list_filter_custom_includes = "{${(formatlist("filter(', %s')", var.list_filter_custom_includes)}}"
+     ["filter('aws_state', 'stopped')", "filter('aws_region', 'eu-west1')"] 
+        
+    formatted_filter_custom_excludes = "{${replace(var.filter_custom_excludes, ":", "', '")}}"
+      "aws_state', 'stopped;aws_region', 'eu-west1" */
+
+    list_filter_custom_excludes = "{${split(";", var.formatted_filter_custom_excludes)}}"
+     ["aws_state', 'stopped", "aws_region', 'eu-west1"] 
+
+    formatted_list_filter_custom_excludes = "{${formatlist("(not filter(', %s'))", var.list_filter_custom_excludes)}}"
+     ["(not filter('aws_state', 'stopped'))", "(not filter('aws_region', 'eu-west1'))"]   */    
 
 
         
