05290c40e8eb012167789d00e1918300b1d3b96b
mel-splunk <58855914+mel-splunk@users.noreply.github.com>
2020-03-03 09:04:57 -0800

    Update monitors-nginx.tf
    updated module.filter-tags.filter_custom

--- middleware/nginx/monitors-nginx.tf
+++ middleware/nginx/monitors-nginx.tf
@@ -3,7 +3,7 @@ resource "signalfx_detector" "nginx_heartbeat" {
 
 	program_text = <<-EOF
 		from signalfx.detectors.not_reporting import not_reporting
-		signal = data('nginx_requests', filter=(not filter('aws_state', '*terminated}', '*stopped}')) and (not filter('gcp_status', '*TERMINATED}', '*STOPPING}')) and (not filter('azure_power_state', 'stop*', 'deallocat*')) and ${module.filter_custom})
+		signal = data('nginx_requests', filter=(not filter('aws_state', '*terminated}', '*stopped}')) and (not filter('gcp_status', '*TERMINATED}', '*STOPPING}')) and (not filter('azure_power_state', 'stop*', 'deallocat*')) and ${module.filter-tags.filter_custom})
 		not_reporting.detector(stream=signal, resource_identifier=['host'], duration='${var.nginx_heartbeat_timeframe}').publish('CRIT')
 	EOF
 
@@ -19,7 +19,7 @@ resource "signalfx_detector" "nginx_dropped_connections" {
 	name = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] Nginx dropped connections"
 
 	program_text = <<-EOF
-		signal = data('connections.failed', filter=${module.filter_custom})${var.nginx_aggregation_function}.${var.nginx_transformation_function}(over='${var.nginx_transformation_window}')
+		signal = data('connections.failed', filter=${module.filter-tags.filter_custom})${var.nginx_aggregation_function}.${var.nginx_transformation_function}(over='${var.nginx_transformation_window}')
 		detect(when(signal > ${var.nginx_threshold_critical})).publish('CRIT')
 	EOF
 
