369fb51527b1a5dfdca2c4d15a4f04be2e76218c
mel-splunk <58855914+mel-splunk@users.noreply.github.com>
2020-03-03 01:16:33 -0800

    Update outputs.tf

--- common/filter-tags/outputs.tf
+++ common/filter-tags/outputs.tf
@@ -1,4 +1,11 @@
-formatted_filter_custom_includes = replace(var.filter_custom_includes, ":", "', '"),
+
+output "filter_custom" {
+  description = "The full filtering pattern to add to monitors"
+      if var.filter_use_defaults = "true"  
+      value  = var.filter_defaults
+    else
+      
+    formatted_filter_custom_includes = replace(var.filter_custom_includes, ":", "', '"),
       /* # "aws_state', 'stopped;aws_region', 'eu-west1" */
 
     list_filter_custom_includes = split(";", var.formatted_filter_custom_includes),
@@ -16,16 +23,10 @@ formatted_filter_custom_includes = replace(var.filter_custom_includes, ":", "', 
     formatted_list_filter_custom_excludes = formatlist("(not filter(', %s'))", var.list_filter_custom_excludes),
     /* ["(not filter('aws_state', 'stopped'))", "(not filter('aws_region', 'eu-west1'))"]   */    
     
-   custum_filter = join(" and ", list(join(" and ", var.formatted_list_filter_custom_includes) , join(" and ", var.formatted_list_filter_custom_excludes))
-
-output "filter_custom" {
-  description = "The full filtering pattern to add to monitors"
-      if var.filter_use_defaults = "true"  
-      value  = var.filter_defaults
-    else
+   custum_filter = join(" and ", list(join(" and ", var.formatted_list_filter_custom_includes) , join(" and ", var.formatted_list_filter_custom_excludes)),
+   
+         
         value = var.custom_filter
-      
-    
       
    }  
 
