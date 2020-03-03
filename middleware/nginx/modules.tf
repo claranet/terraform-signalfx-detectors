47c0861eeff31c03390db2c82b1db56d0845d221
mel-splunk <58855914+mel-splunk@users.noreply.github.com>
2020-03-03 08:59:13 -0800

    Update modules.tf

--- middleware/nginx/modules.tf
+++ middleware/nginx/modules.tf
@@ -3,6 +3,6 @@
 
   filter_defaults             = "filter('env', '${var.environment}') and filter('claranet_monitored', 'true')"
   filter_use_defaults         = var.filter_use_defaults
-  filter_custom_includes       = var.filter_custom_include
-  filter_custom_excludes       = var.filter_custom_excluded
+  filter_custom_includes       = var.filter_custom_includes
+  filter_custom_excludes       = var.filter_custom_excludes
 }
