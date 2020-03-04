c73fcabb845d579f2af8b7055df0728e73aabc43
mel-splunk <58855914+mel-splunk@users.noreply.github.com>
2020-03-04 02:24:34 -0800

    Create locals.tf

---
+++ common/filter-tags/locals.tf
@@ -0,0 +1,53 @@
+output "filter_custom" {
+  description = "The full filtering pattern to add to monitors"
+  value  = "${var.filter_use_defaults == "true" ? var.filter_defaults : 
+    "${var.filter_custom_includes == "" ? join(
+          " and ", 
+          formatlist(
+            "(not filter(', %s'))", 
+            split(
+              ";", 
+              replace(
+                var.filter_custom_excludes,
+                ":",
+                "', '"
+               )
+            )
+          )
+          : 
+    join(
+     " and ", 
+     list(
+       join(
+          " and ", 
+           formatlist(
+              "filter(', %s')", 
+              split(
+                ";", 
+                 replace(
+                    var.filter_custom_includes,
+                    ":",
+                    "', '"
+                 )
+              )
+           )
+       ), 
+       join(
+          " and ", 
+          formatlist(
+            "(not filter(', %s'))", 
+            split(
+              ";", 
+              replace(
+                var.filter_custom_excludes,
+                ":",
+                "', '"
+               )
+            )
+          )
+       )
+     )
+    )}" 
+}"
+   
+}   
