8aa53fe39febb16dca0fe4ccd5a8aab42784d51b
mel-splunk <58855914+mel-splunk@users.noreply.github.com>
2020-03-04 03:12:53 -0800

    Update outputs.tf
    updated to support null for excludes or includes

--- common/filter-tags/outputs.tf
+++ common/filter-tags/outputs.tf
@@ -1,25 +1,7 @@
-
 output "filter_custom" {
   description = "The full filtering pattern to add to monitors"
   value  = "${var.filter_use_defaults == "true" ? var.filter_defaults : 
-    join(
-     " and ", 
-     list(
-       join(
-          " and ", 
-           formatlist(
-              "filter(', %s')", 
-              split(
-                ";", 
-                 replace(
-                    var.filter_custom_includes,
-                    ":",
-                    "', '"
-                 )
-              )
-           )
-       ), 
-       join(
+    "${var.filter_custom_includes == "" ? join(
           " and ", 
           formatlist(
             "(not filter(', %s'))", 
@@ -29,12 +11,59 @@ output "filter_custom" {
                 var.filter_custom_excludes,
                 ":",
                 "', '"
-               )
+              )
             )
           )
-       )
-
-     )
-)}"
-   
+        )
+      : 
+      "${var.filter_custom_excludes == "" ? join(
+            " and ", 
+             formatlist(
+               "(not filter(', %s'))", 
+               split(
+                 ";", 
+                 replace(
+                   var.filter_custom_includes,
+                   ":",
+                   "', '"
+                 )
+               )
+             )
+          )
+        :
+        join(
+          " and ", 
+          list(
+             join(
+               " and ", 
+               formatlist(
+                 "filter(', %s')", 
+                  split(
+                    ";", 
+                    replace(
+                      var.filter_custom_includes,
+                      ":",
+                      "', '"
+                    )
+                  )
+               )
+             ), 
+             join(
+               " and ", 
+               formatlist(
+                 "(not filter(', %s'))", 
+                 split(
+                   ";", 
+                   replace(
+                     var.filter_custom_excludes,
+                     ":",
+                     "', '"
+                   )
+                 )
+               )
+             )
+          )
+      )}"
+  }"
+}"
 }   
