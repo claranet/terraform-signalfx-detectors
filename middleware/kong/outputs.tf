f064ab4ce5e846fe413bf0438af24c2aef692aa9
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-03-05 23:42:24 -0800

    Create outputs.tf

---
+++ middleware/kong/outputs.tf
@@ -0,0 +1,9 @@
+output "heartbeat_id" {
+	description 	= "id for detector heartbeat"
+	value		= signalfx_detector.heartbeat.*.id
+}
+
+output "treatment_limit_id" {
+	description 	= "id for detector treatment limit"
+	value		= signalfx_detector.treatment_limit.*.id
+}
