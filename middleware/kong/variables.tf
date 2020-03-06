9c8d112c5cb1f42902406d211b44f76e097eb729
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-03-05 23:37:48 -0800

    Update variables.tf

--- middleware/kong/variables.tf
+++ middleware/kong/variables.tf
@@ -2,111 +2,130 @@
 
 variable "environment" {
 	description = "Architecture Environment"
-	type = string
+	type        = string
 }
 
 # SignalFx Module specific
 
-variable "prefix_slug" {
-	description = "Prefix string to prepend between brackets on every monitors names"
-	default = ""
+variable "notifications" {
+	description = "Notification recipients semicolon separated (i.e. \"Email,my@mail.com;PagerDuty,credentialId\")"
+	type        = string
+}
+
+variable "prefixes_slug" {
+	description = "Prefixes list to prepend between brackets on every monitors names before environment"
+	type        = list
+	default     = []
 }
 
 variable "filter_use_defaults" {
-  	description = "Use default filtering convention"
-  	default     = "true"
+	description = "Use default filtering which follows tagging convention"
+	type        = bool
+	default     = true
 }
 
 variable "filter_custom_includes" {
-  	description = "Tags to filter signals on when custom filtering is used. Enter as string i.e \"tag1:val1;tag2:val2\""
-  	default     = ""
+	description = "Tags to filter signals on when custom filtering is used (i.e \"tag1:val1;tag2:val2\")"
+	type        = string
+	default     = ""
 }
 
 variable "filter_custom_excludes" {
-  	description = "Tags to exclude when using custom filtering. Enter as string i.e \"tag1:val1;tag2:val2\""
-  	default = ""
+	description = "Tags to exclude when using custom filtering (i.e \"tag1:val1;tag2:val2\")"
+	type        = string
+	default     = ""
 }
 
-variable "disable_detectors" {
-  	description = "(Optional), set string to true if you want to disbale all detectors in this module"
-  	default = "false"
-}
-
-variable "notifications" {
-  	description = "(Required), notification recipients.  Example \"PagerDuty,credentialId\" or \"PagerDuty,credentialId;Slack,credentialId,channel\""
-  	default = ""
+variable "detectors_disabled" {
+	description = "Disable all detectors in this module"
+	type        = bool
+	default     = false
 }
 
 # Kong Detector specific
 
-variable "kong_heartbeat_disabled_flag" {
-	description = "(Optional) When true, heartbeat detector will be disabled"
-	default = "false"
+variable "heartbeat_disabled" {
+	description = "Disable all alerting rules for heartbeat detector"
+	type        = bool
+	default     = null
 }
 
-variable "kong_heartbeat_timeframe" {
-	description = "Timeframe for system not reporting"
-	default = "20m"
+variable "heartbeat_notifications" {
+	description = "Notification recipients semicolon for every alerting rules of heartbeat detector"
+	type        = string
+	default     = ""
 }
 
-variable "kong_heartbeat_notifications" {
-  	description = "(Optional), notification recipients.  Example \"PagerDuty,credentialId\" or \"PagerDuty,credentialId;Slack,credentialId,channel\""
-  	default = ""
+variable "heartbeat_timeframe" {
+	description = "Timeframe for system not reporting detector (i.e. \"10m\")"
+	type        = string
+	default     = "20m"
 }
 
 #####
 
-variable "disable_kong_detector" {
-	description = "(Optional) When true, notifications and events will not be generated for the detect label. false by default."
-	default = ""
+variable "treatment_limit_disabled" {
+	description = "Disable all alerting rules for treatment limit detector"
+	type        = bool
+	default     = null
 }
 
-variable "treatment_limit_critical_disabled_flag" {
-	description = "(Optional) When true, treatment limit critical rule will be disabled"
-	default = "false"
+variable "treatment_limit_disabled_critical" {
+	description = "Disable critical alerting rule for treatment limit detector"
+	type        = bool
+	default     = null
 }
 
-variable "treatment_limit_warning_disabled_flag" {
-	description = "(Optional) When true, treatment limit warning rule will be disabled"
-	default = "false"
-}
-
-variable "treatment_limit_aggregation_function" {
-	description = "(Optional) Treatment limit aggregation function and group by. Examples \".mean(by=['host']).\" or \".max()\""
-	default = ".min(by=['host'])"
-}
-
-variable "treatment_limit_transformation_function" {
-	description = Treatment limit transformation function.  Valid options mean, min, max"
-	default = "min"
-}
-
-variable "treatment_limit_transformation_window" {
-	description = "Treatment limit transformation window. Examples 5m, 20m, 1h, 1d"
-	default = "15m"
-}
-
-variable "treatment_limit_threshold_critical" {
-	description = "Treatment limit critical threshold"
-	default = 20
-}
-
-variable "treatment_limit_threshold_warning" {
-	description = "Treatment limit warning threshold"
-	default = 0
+variable "treatment_limit_disabled_warning" {
+	description = "Disable warning alerting rule for treatment limit detector"
+	type        = bool
+	default     = null
 }
 
 variable "treatment_limit_notifications" {
-  	 description = "(Optional), notification recipients.  Example \"PagerDuty,credentialId\" or \"PagerDuty,credentialId;Slack,credentialId,channel\""
- 	 default = ""
+	description = "Notification recipients semicolon for every alerting rules of treatment limit detector"
+	type        = string
+	default     = ""
 }
 
-variable "treatment_limit_warning_notifications" {
-  	 description = "(Optional), notification recipients.  Example \"PagerDuty,credentialId\" or \"PagerDuty,credentialId;Slack,credentialId,channel\""
- 	 default = ""
+variable "treatment_limit_notifications_warning" {
+	description = "Notification recipients semicolon for warning alerting rule of treatment limit detector"
+	type        = string
+	default     = ""
 }
 
-variable "treatment_limit_critical_notifications" {
-  	 description = "(Optional), notification recipients.  Example \"PagerDuty,credentialId\" or \"PagerDuty,credentialId;Slack,credentialId,channel\""
-  	 default = ""
+variable "treatment_limit_notifications_critical" {
+	description = "Notification recipients semicolon for critical alerting rule of treatment limit detector"
+	type        = string
+	default     = ""
+}
+
+variable "treatment_limit_aggregation_function" {
+	description = "Aggregation function and group by for treatment limit detector (i.e. \".mean(by=['host']).\")"
+	type        = string
+	default 	= ".min(by=['host'])"
+}
+
+variable "treatment_limit_transformation_function" {
+	description = "Transformation function for treatment limit detector (mean, min, max)"
+	type        = string
+	default 	= "min"
+}
+
+variable "treatment_limit_transformation_window" {
+	description = "Transformation window for treatment limit detector (i.e. 5m, 20m, 1h, 1d)"
+	type        = string
+	default 	= "15m"
+}
+
+variable "treatment_limit_threshold_critical" {
+	description = "Critical threshold for treatment limit detector"
+	type        = number
+	default 	= 20
+}
+
+variable "treatment_limit_threshold_warning" {
+	description = "Warning threshold for treatment limit detector"
+	type        = number
+	default 	= 0
 }
