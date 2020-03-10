96d77a14bd90a9485dcdd2c1c00cf605b3418a79
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2020-03-10 17:33:15 +0100

    add types for variables and use bool for filter-tags

--- common/filter-tags/variables.tf
+++ common/filter-tags/variables.tf
@@ -1,20 +1,24 @@
 variable "filter_use_defaults" {
   description = "Use default filtering convention"
-  default     = "true"
+  type        = bool
+  default     = true
 }
 
 variable "filter_defaults" {
   description = "List of tags tu use as filters when using default filtering convention"
+  type        = string
   default     = ""
 }
 
 variable "filter_custom_includes" {
   description = "Semicolon separated string of filters to include when custom filtering is used (i.e \"in1:clude1;tag2:val2\")"
+  type        = string
   default     = ""
 }
 
 variable "filter_custom_excludes" {
   description = "Semicolon separated string of filters to exclude when custom filtering is used (i.e \"ex1:clude1;tag2:val2\")"
+  type        = string
   default     = ""
 }
 
