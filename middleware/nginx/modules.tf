0a68dfa21bade94eae832e8e12d0989b5346db7d
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2020-03-10 17:53:57 +0100

    use generic link for filter-tags module

--- middleware/nginx/modules.tf
+++ middleware/nginx/modules.tf
@@ -1,5 +1,5 @@
 module "filter-tags" {
-  source = "../../common/filter-tags"
+  source = "github.com/claranet/terraform-signalfx-detectors.git//common/filter-tags"
 
   filter_defaults        = "filter('env', '${var.environment}') and filter('claranet_monitored', 'true')"
   filter_use_defaults    = var.filter_use_defaults
