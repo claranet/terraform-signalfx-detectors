9c541621172fdfcd6626153ca4d3727a15238737
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2020-03-12 09:48:51 +0100

    split filter-tags outputs into locals.tf

--- common/filter-tags/outputs.tf
+++ common/filter-tags/outputs.tf
@@ -1,21 +1,3 @@
-locals {
-  filter_custom_includes = join(
-    " and ",
-    formatlist(
-      "(filter('%s'))",
-      [for tag in var.filter_custom_includes : replace(replace(tag, ":", "', '"), " ", "")]
-    )
-  )
-
-  filter_custom_excludes = join(
-    " and ",
-    formatlist(
-      "(not filter('%s'))",
-      [for tag in var.filter_custom_excludes : replace(replace(tag, ":", "', '"), " ", "")]
-    )
-  )
-}
-
 output "filter_custom" {
   description = "The full filtering pattern to use in detectors"
   value = coalesce(
