b675bd8a5561231df7271fa1fc8dc0b521f57842
xp-1000 <xp-1000@hotmail.fr>
2020-03-12 09:46:00 +0100

    SFX-3 improve filter tags module and use list
    Co-Authored-By: Patrick Decat <pdecat@gmail.com>

--- common/filter-tags/outputs.tf
+++ common/filter-tags/outputs.tf
@@ -1,89 +1,33 @@
+locals {
+  filter_custom_includes = join(
+    " and ",
+    formatlist(
+      "(filter('%s'))",
+      [for tag in var.filter_custom_includes : replace(replace(tag, ":", "', '"), " ", "")]
+    )
+  )
+
+  filter_custom_excludes = join(
+    " and ",
+    formatlist(
+      "(not filter('%s'))",
+      [for tag in var.filter_custom_excludes : replace(replace(tag, ":", "', '"), " ", "")]
+    )
+  )
+}
+
 output "filter_custom" {
   description = "The full filtering pattern to use in detectors"
-  value = (
-    var.filter_use_defaults == true ?
+  value = coalesce(
+    join(
+      " and ",
+      compact(
+        [
+          local.filter_custom_includes,
+          local.filter_custom_excludes,
+        ]
+      )
+    ),
     var.filter_defaults
-    :
-    var.filter_custom_includes == "" ?
-    join(
-      " and ",
-      formatlist(
-        "(not filter('%s'))",
-        split(
-          ";",
-          replace(
-            replace(
-              var.filter_custom_excludes,
-              ":",
-              "', '"
-            ),
-            " ",
-            ""
-          )
-        )
-      )
-    )
-    :
-    var.filter_custom_excludes == "" ?
-    join(
-      " and ",
-      formatlist(
-        "(not filter('%s'))",
-        split(
-          ";",
-          replace(
-            replace(
-              var.filter_custom_includes,
-              ":",
-              "', '"
-            ),
-            " ",
-            ""
-          )
-        )
-      )
-    )
-    :
-    join(
-      " and ",
-      list(
-        join(
-          " and ",
-          formatlist(
-            "filter('%s')",
-            split(
-              ";",
-              replace(
-                replace(
-                  var.filter_custom_includes,
-                  ":",
-                  "', '"
-                ),
-                " ",
-                ""
-              )
-            )
-          )
-        ),
-        join(
-          " and ",
-          formatlist(
-            "(not filter('%s'))",
-            split(
-              ";",
-              replace(
-                replace(
-                  var.filter_custom_excludes,
-                  ":",
-                  "', '"
-                ),
-                " ",
-                ""
-              )
-            )
-          )
-        )
-      )
-    )
   )
 }
