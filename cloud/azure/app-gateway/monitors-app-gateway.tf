abb51e9eaee1f8a79665e2476d57e6dbab1c38a7
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-03-17 15:24:10 -0700

    Create monitors-app-gateway.tf

---
+++ cloud/azure/app-gateway/monitors-app-gateway.tf
@@ -0,0 +1,254 @@
+resource "signalfx_detector" "heartbeat" {
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure app gateway heartbeat"
+
+	program_text = <<-EOF
+		from signalfx.detectors.not_reporting import not_reporting
+		signal = data('ResponseStatus', filter=filter('resource_type', 'Microsoft.Network/applicationGateways') and ${module.filter-tags.filter_custom})
+		not_reporting.detector(stream=signal, resource_identifier=['host'], duration='${var.heartbeat_timeframe}').publish('CRIT')
+	EOF
+
+	rule {
+		description           = "has not reported in ${var.heartbeat_timeframe}"
+		severity              = "Critical"
+		detect_label          = "CRIT"
+		disabled              = coalesce(var.heartbeat_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.heartbeat_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} on {{{dimensions}}}"
+	}
+}
+
+resource "signalfx_detector" "current_connection" {
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] App Gateway current connection"
+
+	program_text = <<-EOF
+		signal = data('CurrentConnections', filter=filter('resource_type', 'Microsoft.Network/applicationGateways') and ${module.filter-tags.filter_custom})${var.current_connection_aggregation_function}.${var.current_connection_transformation_function}(over='${var.current_connection_transformation_window}')
+		detect(when(signal < ${var.current_connection_threshold_critical})).publish('CRIT')
+		detect(when(signal < ${var.current_connection_threshold_warning})).publish('WARN')
+	EOF
+
+	rule {
+		description           = "is too low < ${var.current_connection_threshold_critical}"
+		severity              = "Critical"
+		detect_label          = "CRIT"
+		disabled              = coalesce(var.current_connection_disabled_critical, var.current_connection_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.current_connection_notifications_critical, var.current_connection_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
+
+	rule {
+		description           = "is too low < ${var.current_connection_threshold_warning}"
+		severity              = "Warning"
+		detect_label          = "WARN"
+		disabled              = coalesce(var.current_connection_disabled_warning, var.current_connection_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.current_connection_notifications_warning, var.current_connection_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
+}
+
+resource "signalfx_detector" "backend_connect_time" {
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure App Gateway backend connect time"
+
+	program_text = <<-EOF
+		signal = data('BackendConnectTime', filter=filter('resource_type', 'Microsoft.Network/applicationGateways') and ${module.filter-tags.filter_custom})${var.backend_connect_time_aggregation_function}.${var.backend_connect_time_transformation_function}(over='${var.backend_connect_time_transformation_window}')
+		detect(when(signal > ${var.backend_connect_time_threshold_critical})).publish('CRIT')
+		detect(when(signal > ${var.backend_connect_time_threshold_warning})).publish('WARN')
+	EOF
+
+	rule {
+		description           = "is too high > ${var.backend_connect_time_threshold_critical}"
+		severity              = "Critical"
+		detect_label          = "CRIT"
+		disabled              = coalesce(var.backend_connect_time_disabled_critical, var.backend_connect_time_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.backend_connect_time_notifications_critical, var.backend_connect_time_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
+
+	rule {
+		description           = "is too high > ${var.backend_connect_time_threshold_warning}"
+		severity              = "Warning"
+		detect_label          = "WARN"
+		disabled              = coalesce(var.backend_connect_time_disabled_warning, var.backend_connect_time_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.backend_connect_time_notifications_warning, var.backend_connect_time_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
+}
+
+resource "signalfx_detector" "failed_requests" {
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure App Gateway failed requests"
+
+	program_text = <<-EOF
+		A = data('FailedRequests', filter=filter('resource_type', 'Microsoft.Network/applicationGateways') and ${module.filter-tags.filter_custom})${var.failed_requests_aggregation_function}
+		B = data('TotalRequests', filter=filter('resource_type', 'Microsoft.Network/applicationGateways') and ${module.filter-tags.filter_custom})${var.failed_requests_aggregation_function}
+		signal = ((A/B)*100).${var.failed_requests_transformation_function}(over='${var.failed_requests_transformation_window}')
+		detect(when(signal > ${var.failed_requests_threshold_critical})).publish('CRIT')
+		detect(when(signal > ${var.failed_requests_threshold_warning})).publish('WARN')
+	EOF
+
+	rule {
+		description           = "is too high > ${var.failed_requests_threshold_critical}"
+		severity              = "Critical"
+		detect_label          = "CRIT"
+		disabled              = coalesce(var.failed_requests_disabled_critical, var.failed_requests_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.failed_requests_notifications_critical, var.failed_requests_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
+
+	rule {
+		description           = "is too high > ${var.failed_requests_threshold_warning}"
+		severity              = "Warning"
+		detect_label          = "WARN"
+		disabled              = coalesce(var.failed_requests_disabled_warning, var.failed_requests_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.failed_requests_notifications_warning, var.failed_requests_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
+}
+
+resource "signalfx_detector" "unhealthy_host_ratio" {
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure App Gateway backend unhealthy host ratio"
+
+	program_text = <<-EOF
+		A = data('UnhealthyHostCount', filter=filter('resource_type', 'Microsoft.Network/applicationGateways') and ${module.filter-tags.filter_custom})${var.unhealthy_host_ratio_aggregation_function}
+		B = data('HealthyHostCount', filter=filter('resource_type', 'Microsoft.Network/applicationGateways') and ${module.filter-tags.filter_custom})${var.unhealthy_host_ratio_aggregation_function}
+		signal = ((A/(A+B))*100).${var.unhealthy_host_ratio_transformation_function}(over='${var.unhealthy_host_ratio_transformation_window}')
+		detect(when(signal > ${var.unhealthy_host_ratio_threshold_critical})).publish('CRIT')
+		detect(when(signal > ${var.unhealthy_host_ratio_threshold_warning})).publish('WARN')
+	EOF
+
+	rule {
+		description           = "is too high > ${var.unhealthy_host_ratio_threshold_critical}"
+		severity              = "Critical"
+		detect_label          = "CRIT"
+		disabled              = coalesce(var.unhealthy_host_ratio_disabled_critical, var.unhealthy_host_ratio_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.unhealthy_host_ratio_notifications_critical, var.unhealthy_host_ratio_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
+
+	rule {
+		description           = "is too high > ${var.unhealthy_host_ratio_threshold_warning}"
+		severity              = "Warning"
+		detect_label          = "WARN"
+		disabled              = coalesce(var.unhealthy_host_ratio_disabled_warning, var.unhealthy_host_ratio_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.unhealthy_host_ratio_notifications_warning, var.unhealthy_host_ratio_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
+}
+
+resource "signalfx_detector" "http_4xx_errors" {
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure App Gateway HTTP 4xx errors rate"
+
+	program_text = <<-EOF
+		A = data('ResponseStatus', filter=filter('resource_type', 'Microsoft.Network/applicationGateways') and filter('httpstatusgroup', '4xx') and ${module.filter-tags.filter_custom})${var.http_4xx_errors_aggregation_function}
+		B = data('ResponseStatus', filter=filter('resource_type', 'Microsoft.Network/applicationGateways')and ${module.filter-tags.filter_custom})${var.http_4xx_errors_aggregation_function}
+		signal = ((A/B)*100).${var.http_4xx_errors_transformation_function}(over='${var.http_4xx_errors_transformation_window}')
+		detect(when(signal > ${var.http_4xx_errors_threshold_critical})).publish('CRIT')
+		detect(when(signal > ${var.http_4xx_errors_threshold_warning})).publish('WARN')
+	EOF
+
+	rule {
+		description           = "is too high > ${var.http_4xx_errors_threshold_critical}"
+		severity              = "Critical"
+		detect_label          = "CRIT"
+		disabled              = coalesce(var.http_4xx_errors_disabled_critical, var.http_4xx_errors_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.http_4xx_errors_notifications_critical, var.http_4xx_errors_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
+
+	rule {
+		description           = "is too high > ${var.http_4xx_errors_threshold_warning}"
+		severity              = "Warning"
+		detect_label          = "WARN"
+		disabled              = coalesce(var.http_4xx_errors_disabled_warning, var.http_4xx_errors_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.http_4xx_errors_notifications_warning, var.http_4xx_errors_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
+}
+
+resource "signalfx_detector" "http_5xx_errors" {
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure App Gateway HTTP 5xx errors rate"
+
+	program_text = <<-EOF
+		A = data('ResponseStatus', filter=filter('resource_type', 'Microsoft.Network/applicationGateways') and filter('httpstatusgroup', '5xx') and ${module.filter-tags.filter_custom})${var.http_5xx_errors_aggregation_function}
+		B = data('ResponseStatus', filter=filter('resource_type', 'Microsoft.Network/applicationGateways' and ${module.filter-tags.filter_custom})${var.http_5xx_errors_aggregation_function}
+		signal = ((A/B)*100).${var.http_5xx_errors_transformation_function}(over='${var.http_5xx_errors_transformation_window}')
+		detect(when(signal > ${var.http_5xx_errors_threshold_critical})).publish('CRIT')
+		detect(when(signal > ${var.http_5xx_errors_threshold_warning})).publish('WARN')
+	EOF
+
+	rule {
+		description           = "is too high > ${var.http_5xx_errors_threshold_critical}"
+		severity              = "Critical"
+		detect_label          = "CRIT"
+		disabled              = coalesce(var.http_5xx_errors_disabled_critical, var.http_5xx_errors_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.http_5xx_errors_notifications_critical, var.http_5xx_errors_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
+
+	rule {
+		description           = "is too high > ${var.http_5xx_errors_threshold_warning}"
+		severity              = "Warning"
+		detect_label          = "WARN"
+		disabled              = coalesce(var.http_5xx_errors_disabled_warning, var.http_5xx_errors_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.http_5xx_errors_notifications_warning, var.http_5xx_errors_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
+}
+
+resource "signalfx_detector" "backend_http_4xx_errors" {
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure App Gateway backend HTTP 4xx errors rate"
+
+	program_text = <<-EOF
+		A = data('BackendResponseStatus', filter=filter('resource_type', 'Microsoft.Network/applicationGateways') and filter('httpstatusgroup', '4xx') and ${module.filter-tags.filter_custom})${var.backend_http_4xx_errors_aggregation_function}
+		B = data('BackendResponseStatus', filter=filter('resource_type', 'Microsoft.Network/applicationGateways') and ${module.filter-tags.filter_custom})${var.backend_http_4xx_errors_aggregation_function}
+		signal = ((A/B)*100).${var.backend_http_4xx_errors_transformation_function}(over='${var.backend_http_4xx_errors_transformation_window}')
+		detect(when(signal > ${var.backend_http_4xx_errors_threshold_critical})).publish('CRIT')
+		detect(when(signal > ${var.backend_http_4xx_errors_threshold_warning})).publish('WARN')
+	EOF
+
+	rule {
+		description           = "is too high > ${var.backend_http_4xx_errors_threshold_critical}"
+		severity              = "Critical"
+		detect_label          = "CRIT"
+		disabled              = coalesce(var.backend_http_4xx_errors_disabled_critical, var.backend_http_4xx_errors_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.backend_http_4xx_errors_notifications_critical, var.backend_http_4xx_errors_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
+
+	rule {
+		description           = "is too high > ${var.backend_http_4xx_errors_threshold_warning}"
+		severity              = "Warning"
+		detect_label          = "WARN"
+		disabled              = coalesce(var.backend_http_4xx_errors_disabled_warning, var.backend_http_4xx_errors_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.backend_http_4xx_errors_notifications_warning, var.backend_http_4xx_errors_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
+}
+
+resource "signalfx_detector" "backend_http_5xx_errors" {
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure App Gateway backend HTTP 5xx errors rate"
+
+	program_text = <<-EOF
+		A = data('BackendResponseStatus', filter=filter('resource_type', 'Microsoft.Network/applicationGateways') and filter('httpstatusgroup', '5xx') and ${module.filter-tags.filter_custom})${var.backend_http_5xx_errors_aggregation_function}
+		B = data('BackendResponseStatus', filter=filter('resource_type', 'Microsoft.Network/applicationGateways') and ${module.filter-tags.filter_custom})${var.backend_http_5xx_errors_aggregation_function}
+		signal = ((A/B)*100).${var.backend_http_5xx_errors_transformation_function}(over='${var.backend_http_5xx_errors_transformation_window}')
+		detect(when(signal > ${var.backend_http_5xx_errors_threshold_critical})).publish('CRIT')
+		detect(when(signal > ${var.backend_http_5xx_errors_threshold_warning})).publish('WARN')
+	EOF
+
+	rule {
+		description           = "is too high > ${var.backend_http_5xx_errors_threshold_critical}"
+		severity              = "Critical"
+		detect_label          = "CRIT"
+		disabled              = coalesce(var.backend_http_5xx_errors_disabled_critical, var.backend_http_5xx_errors_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.backend_http_5xx_errors_notifications_critical, var.backend_http_5xx_errors_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
+
+	rule {
+		description           = "is too high > ${var.backend_http_5xx_errors_threshold_warning}"
+		severity              = "Warning"
+		detect_label          = "WARN"
+		disabled              = coalesce(var.backend_http_5xx_errors_disabled_warning, var.backend_http_5xx_errors_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.backend_http_5xx_errors_notifications_warning, var.backend_http_5xx_errors_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
+}
