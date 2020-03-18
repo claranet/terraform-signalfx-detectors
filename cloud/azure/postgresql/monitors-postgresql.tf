c0ad53f38fa425621f377d8ecc1db4e8977ef860
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-03-18 11:26:14 -0700

    Update monitors-postgresql.tf

--- cloud/azure/postgresql/monitors-postgresql.tf
+++ cloud/azure/postgresql/monitors-postgresql.tf
@@ -3,7 +3,7 @@ resource "signalfx_detector" "heartbeat" {
 
 	program_text = <<-EOF
 		from signalfx.detectors.not_reporting import not_reporting
-		signal = data('RequestCount', filter=filter('resource_type', 'Microsoft.DBforPostgreSQL/servers') and ${module.filter-tags.filter_custom})
+		signal = data('network_bytes_ingress', filter=filter('resource_type', 'Microsoft.DBforPostgreSQL/servers') and ${module.filter-tags.filter_custom})
 		not_reporting.detector(stream=signal, resource_identifier=['host'], duration='${var.heartbeat_timeframe}').publish('CRIT')
 	EOF
 
