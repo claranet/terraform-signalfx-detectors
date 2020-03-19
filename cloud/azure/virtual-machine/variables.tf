fb9b60acd75eb7ade0661f2b61b515c037fcaf65
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-03-18 18:31:22 -0700

    Create variables.tf

---
+++ cloud/azure/virtual-machine/variables.tf
@@ -0,0 +1,193 @@
+# Global
+
+variable "environment" {
+  description = "Infrastructure environment"
+  type        = string
+}
+
+# SignalFx module specific
+
+variable "notifications" {
+  description = "Notification recipients list for every detectors"
+  type        = list
+}
+
+variable "prefixes" {
+  description = "Prefixes list to prepend between brackets on every monitors names before environment"
+  type        = list
+  default     = []
+}
+
+variable "filter_custom_includes" {
+  description = "List of tags to include when custom filtering is used"
+  type        = list
+  default     = []
+}
+
+variable "filter_custom_excludes" {
+  description = "List of tags to exclude when custom filtering is used"
+  type        = list
+  default     = []
+}
+
+variable "detectors_disabled" {
+  description = "Disable all detectors in this module"
+  type        = bool
+  default     = false
+}
+
+# Azure virtual machine detectors specific
+
+variable "heartbeat_disabled" {
+  description = "Disable all alerting rules for heartbeat detector"
+  type        = bool
+  default     = null
+}
+
+variable "heartbeat_notifications" {
+  description = "Notification recipients list for every alerting rules of heartbeat detector"
+  type        = list
+  default     = []
+}
+
+variable "heartbeat_timeframe" {
+  description = "Timeframe for system not reporting detector (i.e. \"10m\")"
+  type        = string
+  default     = "20m"
+}
+
+# CPU_usage detectors
+
+variable "cpu_usage_disabled" {
+  description = "Disable all alerting rules for cpu_usage detector"
+  type        = bool
+  default     = null
+}
+
+variable "cpu_usage_disabled_critical" {
+  description = "Disable critical alerting rule for cpu_usage detector"
+  type        = bool
+  default     = null
+}
+
+variable "cpu_usage_disabled_warning" {
+  description = "Disable warning alerting rule for cpu_usage detector"
+  type        = bool
+  default     = null
+}
+
+variable "cpu_usage_notifications" {
+  description = "Notification recipients list for every alerting rules of cpu_usage detector"
+  type        = list
+  default     = []
+}
+
+variable "cpu_usage_notifications_warning" {
+  description = "Notification recipients list for warning alerting rule of cpu_usage detector"
+  type        = list
+  default     = []
+}
+
+variable "cpu_usage_notifications_critical" {
+  description = "Notification recipients list for critical alerting rule of cpu_usage detector"
+  type        = list
+  default     = []
+}
+
+variable "cpu_usage_aggregation_function" {
+  description = "Aggregation function and group by for cpu_usage detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ".mean(by=['azure_region', 'azure_resource_group_name', 'azure_resource_name'])"
+}
+
+variable "cpu_usage_transformation_function" {
+  description = "Transformation function for cpu_usage detector (mean, min, max)"
+  type        = string
+  default     = "min"
+}
+
+variable "cpu_usage_transformation_window" {
+  description = "Transformation window for cpu_usage detector (i.e. 5m, 20m, 1h, 1d)"
+  type        = string
+  default     = "15m"
+}
+
+variable "cpu_usage_threshold_critical" {
+  description = "Critical threshold for cpu_usage detector"
+  type        = number
+  default     = 90
+}
+
+variable "cpu_usage_threshold_warning" {
+  description = "Warning threshold for cpu_usage detector"
+  type        = number
+  default     = 80
+}
+
+# Credit_cpu detectors
+
+variable "credit_cpu_disabled" {
+  description = "Disable all alerting rules for credit_cpu detector"
+  type        = bool
+  default     = null
+}
+
+variable "credit_cpu_disabled_critical" {
+  description = "Disable critical alerting rule for credit_cpu detector"
+  type        = bool
+  default     = null
+}
+
+variable "credit_cpu_disabled_warning" {
+  description = "Disable warning alerting rule for credit_cpu detector"
+  type        = bool
+  default     = null
+}
+
+variable "credit_cpu_notifications" {
+  description = "Notification recipients list for every alerting rules of credit_cpu detector"
+  type        = list
+  default     = []
+}
+
+variable "credit_cpu_notifications_warning" {
+  description = "Notification recipients list for warning alerting rule of credit_cpu detector"
+  type        = list
+  default     = []
+}
+
+variable "credit_cpu_notifications_critical" {
+  description = "Notification recipients list for critical alerting rule of credit_cpu detector"
+  type        = list
+  default     = []
+}
+
+variable "credit_cpu_aggregation_function" {
+  description = "Aggregation function and group by for credit_cpu detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ".mean(by=['azure_region', 'azure_resource_group_name', 'azure_resource_name'])"
+}
+
+variable "credit_cpu_transformation_function" {
+  description = "Transformation function for credit_cpu detector (mean, min, max)"
+  type        = string
+  default     = "min"
+}
+
+variable "credit_cpu_transformation_window" {
+  description = "Transformation window for credit_cpu detector (i.e. 5m, 20m, 1h, 1d)"
+  type        = string
+  default     = "5m"
+}
+
+variable "credit_cpu_threshold_critical" {
+  description = "Critical threshold for credit_cpu detector"
+  type        = number
+  default     = 15
+}
+
+variable "credit_cpu_threshold_warning" {
+  description = "Warning threshold for credit_cpu detector"
+  type        = number
+  default     = 30
+}
