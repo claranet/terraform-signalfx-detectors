0986b122b56536b79aab057b67a0d02ab25bed2b
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-03-21 08:09:39 -0700

    Update monitors-sql-database.tf

--- cloud/azure/sql-database/monitors-sql-database.tf
+++ cloud/azure/sql-database/monitors-sql-database.tf
@@ -4,7 +4,7 @@ resource "signalfx_detector" "heartbeat" {
 	program_text = <<-EOF
 		from signalfx.detectors.not_reporting import not_reporting
 		signal = data('connection_successful', filter=filter('resource_type', 'Microsoft.Sql/servers/databases') and ${module.filter-tags.filter_custom})
-		not_reporting.detector(stream=signal, resource_identifier=['host'], duration='${var.heartbeat_timeframe}').publish('CRIT')
+		not_reporting.detector(stream=signal, resource_identifier=['azure_resource_group_name'], duration='${var.heartbeat_timeframe}').publish('CRIT')
 	EOF
 
 	rule {
