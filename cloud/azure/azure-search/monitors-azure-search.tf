48828f2c0ece9c3d469c52f37129f6b881c22101
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-03-25 14:14:57 -0700

    Update monitors-azure-search.tf

--- cloud/azure/azure-search/monitors-azure-search.tf
+++ cloud/azure/azure-search/monitors-azure-search.tf
@@ -3,7 +3,7 @@ resource "signalfx_detector" "heartbeat" {
 
 	program_text = <<-EOF
 		from signalfx.detectors.not_reporting import not_reporting
-		signal = data('SearchQueriesPerSecond', filter=filter('resource_type', 'Microsoft.Search/searchServices') and ${module.filter-tags.filter_custom})
+		signal = data('SearchQueriesPerSecond', filter=filter('resource_type', 'Microsoft.Search/searchServices') and ${module.filter-tags.filter_custom}).publish('signal')
 		not_reporting.detector(stream=signal, resource_identifier=['azure_resource_id'], duration='${var.heartbeat_timeframe}').publish('CRIT')
 	EOF
 
@@ -21,7 +21,7 @@ resource "signalfx_detector" "search_latency" {
 	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure search latency"
 
 	program_text = <<-EOF
-		signal = data('SearchLatency', filter=filter('resource_type', 'Microsoft.Search/searchServices') and ${module.filter-tags.filter_custom})${var.search_latency_aggregation_function}.${var.search_latency_transformation_function}(over='${var.search_latency_transformation_window}')
+		signal = data('SearchLatency', filter=filter('resource_type', 'Microsoft.Search/searchServices') and ${module.filter-tags.filter_custom})${var.search_latency_aggregation_function}.${var.search_latency_transformation_function}(over='${var.search_latency_transformation_window}').publish('signal')
 		detect(when(signal > ${var.search_latency_threshold_critical})).publish('CRIT')
 		detect(when(signal > ${var.search_latency_threshold_warning})).publish('WARN')
 	EOF
@@ -49,7 +49,7 @@ resource "signalfx_detector" "search_throttled_queries_rate" {
 	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure Search throttled queries rate"
 
 	program_text = <<-EOF
-		signal = data('ThrottledSearchQueriesPercentage', filter=filter('resource_type', 'Microsoft.Search/searchServices') and ${module.filter-tags.filter_custom})${var.search_throttled_queries_rate_aggregation_function}.${var.search_throttled_queries_rate_transformation_function}(over='${var.search_throttled_queries_rate_transformation_window}')
+		signal = data('ThrottledSearchQueriesPercentage', filter=filter('resource_type', 'Microsoft.Search/searchServices') and ${module.filter-tags.filter_custom})${var.search_throttled_queries_rate_aggregation_function}.${var.search_throttled_queries_rate_transformation_function}(over='${var.search_throttled_queries_rate_transformation_window}').publish('signal')
 		detect(when(signal > ${var.search_throttled_queries_rate_threshold_critical})).publish('CRIT')
 		detect(when(signal > ${var.search_throttled_queries_rate_threshold_warning})).publish('WARN')
 	EOF
