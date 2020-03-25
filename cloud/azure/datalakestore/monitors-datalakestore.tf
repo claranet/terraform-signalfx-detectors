53877c0088a057d5830782aad063c3484abfd991
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-03-25 14:16:17 -0700

    Update monitors-datalakestore.tf

--- cloud/azure/datalakestore/monitors-datalakestore.tf
+++ cloud/azure/datalakestore/monitors-datalakestore.tf
@@ -3,7 +3,7 @@ resource "signalfx_detector" "heartbeat" {
 
 	program_text = <<-EOF
 		from signalfx.detectors.not_reporting import not_reporting
-		signal = data('TotalStorage', filter=filter('resource_type', 'Microsoft.DataLakeStore/accounts') and ${module.filter-tags.filter_custom})
+		signal = data('TotalStorage', filter=filter('resource_type', 'Microsoft.DataLakeStore/accounts') and ${module.filter-tags.filter_custom}).publish('signal')
 		not_reporting.detector(stream=signal, resource_identifier=['azure_resource_id'], duration='${var.heartbeat_timeframe}').publish('CRIT')
 	EOF
 
