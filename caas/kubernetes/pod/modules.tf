f3ad60859b7a3f7f0e7015e4e611cce83805f199
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-03-28 10:41:36 -0700

    Update modules.tf

--- caas/kubernetes/pod/modules.tf
+++ caas/kubernetes/pod/modules.tf
@@ -1,8 +1,7 @@
 module "filter-tags" {
-	source = "../../common/filter-tags"
+  source = "github.com/claranet/terraform-signalfx-detectors.git//common/filter-tags"
 
-	filter_defaults = "filter('env', '${var.environment}') and filter('claranet_monitored', 'true')"
-	filter_use_defaults = var.filter_use_defaults
-	filter_custom_includes = var.filter_custom_include
-	filter_custom_excludes = var.filter_custom_excluded
+  filter_defaults        = "filter('env', '${var.environment}') and filter('sfx_monitored', 'true')"
+  filter_custom_includes = var.filter_custom_includes
+  filter_custom_excludes = var.filter_custom_excludes
 }
