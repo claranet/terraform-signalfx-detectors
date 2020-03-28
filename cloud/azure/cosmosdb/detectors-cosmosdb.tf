fc3fff37c1143c3eed1c95e2efc26b53c68ab351
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-03-28 07:10:49 -0700

    Update detectors-cosmosdb.tf

--- cloud/azure/cosmosdb/detectors-cosmosdb.tf
+++ cloud/azure/cosmosdb/detectors-cosmosdb.tf
@@ -22,37 +22,37 @@ resource "signalfx_detector" "db_4xx_requests" {
 
 	program_text = <<-EOF
 		from signalfx.detectors.aperiodic import aperiodic
-		A = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '400') and ${module.filter-tags.filter_custom})${var.4xx_requests_aggregation_function}
-		B = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '401') and ${module.filter-tags.filter_custom})${var.4xx_requests_aggregation_function}
-		C = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '403') and ${module.filter-tags.filter_custom})${var.4xx_requests_aggregation_function}
-		D = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '404') and ${module.filter-tags.filter_custom})${var.4xx_requests_aggregation_function}
-		E = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '408') and ${module.filter-tags.filter_custom})${var.4xx_requests_aggregation_function}
-		F = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '409') and ${module.filter-tags.filter_custom})${var.4xx_requests_aggregation_function}
-		G = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '412') and ${module.filter-tags.filter_custom})${var.4xx_requests_aggregation_function}
-		H = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '413') and ${module.filter-tags.filter_custom})${var.4xx_requests_aggregation_function}
-		I = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '429') and ${module.filter-tags.filter_custom})${var.4xx_requests_aggregation_function}
-		J = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '449') and ${module.filter-tags.filter_custom})${var.4xx_requests_aggregation_function}
-		K = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and ${module.filter-tags.filter_custom})${var.4xx_requests_aggregation_function}
-		signal = (((A+B+C+D+E+F+G+H+I+J)/K)*100).${var.4xx_requests_transformation_function}(over='${var.4xx_requests_transformation_window}').publish('signal')
-		aperiodic.above_or_below_detector(signal, ${var.4xx_requests_threshold_critical}, 'above', lasting('${var.4xx_requests_aperiodic_duration}', ${var.4xx_requests_aperiodic_percentage})).publish('CRIT')
-		aperiodic.above_or_below_detector(signal, ${var.4xx_requests_threshold_warning}, 'above', lasting('${var.4xx_requests_aperiodic_duration}', ${var.4xx_requests_aperiodic_percentage})).publish('WARN')
+		A = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '400') and ${module.filter-tags.filter_custom})${var.db_4xx_requests_aggregation_function}
+		B = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '401') and ${module.filter-tags.filter_custom})${var.db_4xx_requests_aggregation_function}
+		C = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '403') and ${module.filter-tags.filter_custom})${var.db_4xx_requests_aggregation_function}
+		D = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '404') and ${module.filter-tags.filter_custom})${var.db_4xx_requests_aggregation_function}
+		E = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '408') and ${module.filter-tags.filter_custom})${var.db_4xx_requests_aggregation_function}
+		F = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '409') and ${module.filter-tags.filter_custom})${var.db_4xx_requests_aggregation_function}
+		G = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '412') and ${module.filter-tags.filter_custom})${var.db_4xx_requests_aggregation_function}
+		H = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '413') and ${module.filter-tags.filter_custom})${var.db_4xx_requests_aggregation_function}
+		I = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '429') and ${module.filter-tags.filter_custom})${var.db_4xx_requests_aggregation_function}
+		J = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '449') and ${module.filter-tags.filter_custom})${var.db_4xx_requests_aggregation_function}
+		K = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and ${module.filter-tags.filter_custom})${var.db_4xx_requests_aggregation_function}
+		signal = (((A+B+C+D+E+F+G+H+I+J)/K)*100).${var.db_4xx_requests_transformation_function}(over='${var.db_4xx_requests_transformation_window}').publish('signal')
+		aperiodic.above_or_below_detector(signal, ${var.db_4xx_requests_threshold_critical}, 'above', lasting('${var.db_4xx_requests_aperiodic_duration}', ${var.db_4xx_requests_aperiodic_percentage})).publish('CRIT')
+		aperiodic.above_or_below_detector(signal, ${var.db_4xx_requests_threshold_warning}, 'above', lasting('${var.db_4xx_requests_aperiodic_duration}', ${var.db_4xx_requests_aperiodic_percentage})).publish('WARN')
 	EOF
 
 	rule {
-		description           = "is too high > ${var.4xx_requests_threshold_critical}"
+		description           = "is too high > ${var.db_4xx_requests_threshold_critical}"
 		severity              = "Critical"
 		detect_label          = "CRIT"
-		disabled              = coalesce(var.4xx_requests_disabled_critical, var.4xx_requests_disabled, var.detectors_disabled)
-		notifications         = coalescelist(var.4xx_requests_notifications_critical, var.4xx_requests_notifications, var.notifications)
+		disabled              = coalesce(var.db_4xx_requests_disabled_critical, var.db_4xx_requests_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.db_4xx_requests_notifications_critical, var.db_4xx_requests_notifications, var.notifications)
 		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
 	}
 
 	rule {
-		description           = "is too high > ${var.4xx_requests_threshold_warning}"
+		description           = "is too high > ${var.db_4xx_requests_threshold_warning}"
 		severity              = "Warning"
 		detect_label          = "WARN"
-		disabled              = coalesce(var.4xx_requests_disabled_warning, var.4xx_requests_disabled, var.detectors_disabled)
-		notifications         = coalescelist(var.4xx_requests_notifications_warning, var.4xx_requests_notifications, var.notifications)
+		disabled              = coalesce(var.db_4xx_requests_disabled_warning, var.db_4xx_requests_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.db_4xx_requests_notifications_warning, var.db_4xx_requests_notifications, var.notifications)
 		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
 	}
 }
@@ -62,29 +62,29 @@ resource "signalfx_detector" "db_5xx_requests" {
 
 	program_text = <<-EOF
 		from signalfx.detectors.aperiodic import aperiodic
-		A = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '500') and ${module.filter-tags.filter_custom})${var.5xx_requests_aggregation_function}
-		B = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '503') and ${module.filter-tags.filter_custom})${var.5xx_requests_aggregation_function}
-		C = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and ${module.filter-tags.filter_custom})${var.5xx_requests_aggregation_function}
-		signal = (((A+B)/C)*100).${var.5xx_requests_transformation_function}(over='${var.5xx_requests_transformation_window}').publish('signal')
-		aperiodic.above_or_below_detector(signal, ${var.5xx_requests_threshold_critical}, 'above', lasting('${var.5xx_requests_aperiodic_duration}', ${var.5xx_requests_aperiodic_percentage})).publish('CRIT')
-		aperiodic.above_or_below_detector(signal, ${var.4xx_requests_threshold_warning}, 'above', lasting('${var.5xx_requests_aperiodic_duration}', ${var.5xx_requests_aperiodic_percentage})).publish('WARN')
+		A = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '500') and ${module.filter-tags.filter_custom})${var.db_5xx_requests_aggregation_function}
+		B = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and filter('statuscode', '503') and ${module.filter-tags.filter_custom})${var.db_5xx_requests_aggregation_function}
+		C = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and ${module.filter-tags.filter_custom})${var.db_5xx_requests_aggregation_function}
+		signal = (((A+B)/C)*100).${var.db_5xx_requests_transformation_function}(over='${var.db_5xx_requests_transformation_window}').publish('signal')
+		aperiodic.above_or_below_detector(signal, ${var.db_5xx_requests_threshold_critical}, 'above', lasting('${var.db_5xx_requests_aperiodic_duration}', ${var.db_5xx_requests_aperiodic_percentage})).publish('CRIT')
+		aperiodic.above_or_below_detector(signal, ${var.db_4xx_requests_threshold_warning}, 'above', lasting('${var.db_5xx_requests_aperiodic_duration}', ${var.db_5xx_requests_aperiodic_percentage})).publish('WARN')
 	EOF
 
 	rule {
-		description           = "is too high > ${var.5xx_requests_threshold_critical}"
+		description           = "is too high > ${var.db_5xx_requests_threshold_critical}"
 		severity              = "Critical"
 		detect_label          = "CRIT"
-		disabled              = coalesce(var.5xx_requests_disabled_critical, var.5xx_requests_disabled, var.detectors_disabled)
-		notifications         = coalescelist(var.5xx_requests_notifications_critical, var.5xx_requests_notifications, var.notifications)
+		disabled              = coalesce(var.db_5xx_requests_disabled_critical, var.db_5xx_requests_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.db_5xx_requests_notifications_critical, var.db_5xx_requests_notifications, var.notifications)
 		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
 	}
 
 	rule {
-		description           = "is too high > ${var.5xx_requests_threshold_warning}"
+		description           = "is too high > ${var.db_5xx_requests_threshold_warning}"
 		severity              = "Warning"
 		detect_label          = "WARN"
-		disabled              = coalesce(var.5xx_requests_disabled_warning, var.5xx_requests_disabled, var.detectors_disabled)
-		notifications         = coalescelist(var.5xx_requests_notifications_warning, var.5xx_requests_notifications, var.notifications)
+		disabled              = coalesce(var.db_5xx_requests_disabled_warning, var.db_5xx_requests_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.db_5xx_requests_notifications_warning, var.db_5xx_requests_notifications, var.notifications)
 		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
 	}
 }
@@ -98,7 +98,7 @@ resource "signalfx_detector" "scaling" {
 		B = data('TotalRequests', filter=filter('resource_type', 'Microsoft.DocumentDb/databaseAccounts') and ${module.filter-tags.filter_custom})${var.scaling_aggregation_function}
 		signal = ((A/B)*100).${var.scaling_transformation_function}(over='${var.scaling_transformation_window}').publish('signal')
 		aperiodic.above_or_below_detector(signal, ${var.scaling_threshold_critical}, 'above', lasting('${var.scaling_aperiodic_duration}', ${var.scaling_aperiodic_percentage})).publish('CRIT')
-		aperiodic.above_or_below_detector(signal, ${var.4xx_requests_threshold_warning}, 'above', lasting('${var.scaling_aperiodic_duration}', ${var.scaling_aperiodic_percentage})).publish('WARN')
+		aperiodic.above_or_below_detector(signal, ${var.db_4xx_requests_threshold_warning}, 'above', lasting('${var.scaling_aperiodic_duration}', ${var.scaling_aperiodic_percentage})).publish('WARN')
 	EOF
 
 	rule {
