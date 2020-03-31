09796f969403d204eec1c53d84fe3e635eb4d19c
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-03-31 05:28:10 -0700

    bootstrap middleware

---
+++ middleware/php-fpm/detectors-fpm.tf
@@ -0,0 +1,49 @@
+resource "signalfx_detector" "heartbeat" {
+ 	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] PHP-FPM heartbeat"
+
+	program_text = <<-EOF
+		from signalfx.detectors.not_reporting import not_reporting
+		signal = data('phpfpm_requests', ${module.filter-tags.filter_custom})
+		not_reporting.detector(stream=signal, resource_identifier=['host'], duration='${var.heartbeat_timeframe}').publish('CRIT')
+	EOF
+
+	rule {
+		description           = "has not reported in ${var.heartbeat_timeframe}"
+		severity              = "Critical"
+		detect_label          = "CRIT"
+		disabled              = coalesce(var.heartbeat_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.heartbeat_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{readableRule}}} on {{{dimensions}}}"
+	}
+}
+
+resource "signalfx_detector" "php_fpm_connect_idle" {
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] PHP-FPM busy workers"
+
+	program_text = <<-EOF
+		A = data('phpfpm_processes.active', ${module.filter-tags.filter_custom})${var.php_fpm_connect_idle_aggregation_function}
+		B = data('phpfpm_processes.total', ${module.filter-tags.filter_custom})${var.php_fpm_connect_idle_aggregation_function}
+		signal = ((A/B)*100).${var.php_fpm_connect_idle_transformation_function}(over='${var.php_fpm_connect_idle_transformation_window}').publish('signal')
+		detect(when(signal > ${var.php_fpm_connect_idle_threshold_critical})).publish('CRIT')
+		detect(when(signal > ${var.php_fpm_connect_idle_threshold_warning}) and when(signal <= ${var.php_fpm_connect_idle_threshold_critical})).publish('WARN')
+	EOF
+
+	rule {
+		description           = "are too high > ${var.php_fpm_connect_idle_threshold_critical}"
+		severity              = "Critical"
+		detect_label          = "CRIT"
+		disabled              = coalesce(var.php_fpm_connect_idle_disabled_critical, var.php_fpm_connect_idle_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.php_fpm_connect_idle_notifications_critical, var.php_fpm_connect_idle_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{readableRule}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
+
+	rule {
+		description           = "are too high > ${var.php_fpm_connect_idle_threshold_warning}"
+		severity              = "Warning"
+		detect_label          = "WARN"
+		disabled              = coalesce(var.php_fpm_connect_idle_disabled_warning, var.php_fpm_connect_idle_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.php_fpm_connect_idle_notifications_warning, var.php_fpm_connect_idle_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{readableRule}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
+
+}
---
+++ middleware/php-fpm/variables.tf
@@ -0,0 +1,125 @@
+# Global
+
+variable "environment" {
+  description = "Infrastructure environment"
+  type        = string
+}
+
+# SignalFx module specific
+
+variable "notifications" {
+  description = "Notification recipients list for every detectors"
+  type        = list
+}
+
+variable "prefixes" {
+  description = "Prefixes list to prepend between brackets on every monitors names before environment"
+  type        = list
+  default     = []
+}
+
+variable "filter_custom_includes" {
+  description = "List of tags to include when custom filtering is used"
+  type        = list
+  default     = []
+}
+
+variable "filter_custom_excludes" {
+  description = "List of tags to exclude when custom filtering is used"
+  type        = list
+  default     = []
+}
+
+variable "detectors_disabled" {
+  description = "Disable all detectors in this module"
+  type        = bool
+  default     = false
+}
+
+# PHP FPM detectors specific
+
+variable "heartbeat_disabled" {
+  description = "Disable all alerting rules for heartbeat detector"
+  type        = bool
+  default     = null
+}
+
+variable "heartbeat_notifications" {
+  description = "Notification recipients list for every alerting rules of heartbeat detector"
+  type        = list
+  default     = []
+}
+
+variable "heartbeat_timeframe" {
+  description = "Timeframe for system not reporting detector (i.e. \"10m\")"
+  type        = string
+  default     = "20m"
+}
+
+# PHP_fpm_connect_idle detectors
+
+variable "php_fpm_connect_idle_disabled" {
+  description = "Disable all alerting rules for php_fpm_connect_idle detector"
+  type        = bool
+  default     = null
+}
+
+variable "php_fpm_connect_idle_disabled_critical" {
+  description = "Disable critical alerting rule for php_fpm_connect_idle detector"
+  type        = bool
+  default     = null
+}
+
+variable "php_fpm_connect_idle_disabled_warning" {
+  description = "Disable warning alerting rule for php_fpm_connect_idle detector"
+  type        = bool
+  default     = null
+}
+
+variable "php_fpm_connect_idle_notifications" {
+  description = "Notification recipients list for every alerting rules of php_fpm_connect_idle detector"
+  type        = list
+  default     = []
+}
+
+variable "php_fpm_connect_idle_notifications_warning" {
+  description = "Notification recipients list for warning alerting rule of php_fpm_connect_idle detector"
+  type        = list
+  default     = []
+}
+
+variable "php_fpm_connect_idle_notifications_critical" {
+  description = "Notification recipients list for critical alerting rule of php_fpm_connect_idle detector"
+  type        = list
+  default     = []
+}
+
+variable "php_fpm_connect_idle_aggregation_function" {
+  description = "Aggregation function and group by for php_fpm_connect_idle detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ""
+}
+
+variable "php_fpm_connect_idle_transformation_function" {
+  description = "Transformation function for php_fpm_connect_idle detector (mean, min, max)"
+  type        = string
+  default     = "mean"
+}
+
+variable "php_fpm_connect_idle_transformation_window" {
+  description = "Transformation window for php_fpm_connect_idle detector (i.e. 5m, 20m, 1h, 1d)"
+  type        = string
+  default     = "10m"
+}
+
+variable "php_fpm_connect_idle_threshold_critical" {
+  description = "Critical threshold for php_fpm_connect_idle detector"
+  type        = number
+  default     = 90
+}
+
+variable "php_fpm_connect_idle_threshold_warning" {
+  description = "Warning threshold for php_fpm_connect_idle detector"
+  type        = number
+  default     = 80
+}
