3b14647a024495668e488a1569e056e96f8d90c2
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-04-01 08:45:41 -0700

    Update detectors-keyvault.tf

--- cloud/azure/keyvault/detectors-keyvault.tf
+++ cloud/azure/keyvault/detectors-keyvault.tf
@@ -3,7 +3,7 @@ resource "signalfx_detector" "heartbeat" {
 
 	program_text = <<-EOF
 		from signalfx.detectors.not_reporting import not_reporting
-		signal = data('Availability', filter=filter('resource_type', 'Microsoft.KeyVault/vaults') and ${module.filter-tags.filter_custom}).publish('signal')
+		signal = data('Availability', filter=filter('resource_type', 'Microsoft.KeyVault/vaults') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom}).publish('signal')
 		not_reporting.detector(stream=signal, resource_identifier=['ActivityName'], duration='${var.heartbeat_timeframe}').publish('CRIT')
 	EOF
 
@@ -21,8 +21,8 @@ resource "signalfx_detector" "api_result" {
 	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure keyvault API result rate"
 
 	program_text = <<-EOF
-		A = data('ServiceApiResult', filter=filter('resource_type', 'Microsoft.KeyVault/vaults') and filter('statuscode', '200') and ${module.filter-tags.filter_custom})${var.api_result_aggregation_function}
-		B = data('ServiceApiResult', filter=filter('resource_type', 'Microsoft.KeyVault/vaults') and ${module.filter-tags.filter_custom})${var.api_result_aggregation_function}
+		A = data('ServiceApiResult', filter=filter('resource_type', 'Microsoft.KeyVault/vaults') and filter('statuscode', '200') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom})${var.api_result_aggregation_function}
+		B = data('ServiceApiResult', filter=filter('resource_type', 'Microsoft.KeyVault/vaults') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom})${var.api_result_aggregation_function}
 		signal = ((A/B)*100).fill(100).${var.api_result_transformation_function}(over='${var.api_result_transformation_window}').publish('signal')
 		detect(when(signal < ${var.api_result_threshold_critical})).publish('CRIT')
 		detect(when(signal < ${var.api_result_threshold_warning})).publish('WARN')
@@ -51,7 +51,7 @@ resource "signalfx_detector" "api_latency" {
 	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure keyvault API latency"
 
 	program_text = <<-EOF
-		signal = data('ServiceApiLatency', filter=filter('resource_type', 'Microsoft.KeyVault/vaults') and filter('activityname', 'secretlist') and ${module.filter-tags.filter_custom})${var.api_latency_aggregation_function}.${var.api_latency_transformation_function}(over='${var.api_latency_transformation_window}').publish('signal')
+		signal = data('ServiceApiLatency', filter=filter('resource_type', 'Microsoft.KeyVault/vaults') and filter('activityname', 'secretlist') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom})${var.api_latency_aggregation_function}.${var.api_latency_transformation_function}(over='${var.api_latency_transformation_window}').publish('signal')
 		detect(when(signal > ${var.api_latency_threshold_critical})).publish('CRIT')
 		detect(when(signal > ${var.api_latency_threshold_warning})).publish('WARN')
 	EOF
