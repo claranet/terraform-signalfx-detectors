5b6e1d92fdb382cd86d40ff18c54eeadf5b26364
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-04-01 08:54:00 -0700

    Update detectors-service-bus.tf

--- cloud/azure/servicebus/detectors-service-bus.tf
+++ cloud/azure/servicebus/detectors-service-bus.tf
@@ -3,7 +3,7 @@ resource "signalfx_detector" "heartbeat" {
 
 	program_text = <<-EOF
 		from signalfx.detectors.not_reporting import not_reporting
-		signal = data('SuccessfulRequests', filter=filter('resource_type', 'Microsoft.ServiceBus/namespaces') and ${module.filter-tags.filter_custom}).publish('signal')
+		signal = data('SuccessfulRequests', filter=filter('resource_type', 'Microsoft.ServiceBus/namespaces') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom}).publish('signal')
 		not_reporting.detector(stream=signal, resource_identifier=['EntityName'], duration='${var.heartbeat_timeframe}').publish('CRIT')
 	EOF
 
@@ -21,7 +21,7 @@ resource "signalfx_detector" "active_connections" {
 	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure servicebus active connection"
 
 	program_text = <<-EOF
-		signal = data('ActiveConnections', filter=filter('resource_type', 'Microsoft.ServiceBus/namespaces') and ${module.filter-tags.filter_custom})${var.active_connections_aggregation_function}.${var.active_connections_transformation_function}(over='${var.active_connections_transformation_window}').publish('signal')
+		signal = data('ActiveConnections', filter=filter('resource_type', 'Microsoft.ServiceBus/namespaces') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom})${var.active_connections_aggregation_function}.${var.active_connections_transformation_function}(over='${var.active_connections_transformation_window}').publish('signal')
 		detect(when(signal > ${var.active_connections_threshold_critical})).publish('CRIT')
 	EOF
 
@@ -41,8 +41,8 @@ resource "signalfx_detector" "user_errors" {
 
 	program_text = <<-EOF
 		from signalfx.detectors.aperiodic import aperiodic
-		A = data('UserErrors', filter=filter('resource_type', 'Microsoft.ServiceBus/namespaces') and ${module.filter-tags.filter_custom})${var.user_errors_aggregation_function}
-		B = data('IncomingRequests', filter=filter('resource_type', 'Microsoft.ServiceBus/namespaces') and ${module.filter-tags.filter_custom})${var.user_errors_aggregation_function}
+		A = data('UserErrors', filter=filter('resource_type', 'Microsoft.ServiceBus/namespaces') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom})${var.user_errors_aggregation_function}
+		B = data('IncomingRequests', filter=filter('resource_type', 'Microsoft.ServiceBus/namespaces') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom})${var.user_errors_aggregation_function}
 		signal = ((A/B)*100).${var.user_errors_transformation_function}(over='${var.user_errors_transformation_window}').publish('signal')
 		aperiodic.above_or_below_detector(signal, ${var.user_errors_threshold_critical}, 'above', lasting('${var.user_errors_aperiodic_duration}', ${var.user_errors_aperiodic_percentage})).publish('CRIT')
 		aperiodic.above_or_below_detector(signal, ${var.user_errors_threshold_warning}, 'above', lasting('${var.user_errors_aperiodic_duration}', ${var.user_errors_aperiodic_percentage})).publish('WARN')
@@ -72,8 +72,8 @@ resource "signalfx_detector" "server_errors" {
 
 	program_text = <<-EOF
 		from signalfx.detectors.aperiodic import aperiodic
-		A = data('ServerErrors', filter=filter('resource_type', 'Microsoft.ServiceBus/namespaces') and ${module.filter-tags.filter_custom})${var.server_errors_aggregation_function}
-		B = data('IncomingRequests', filter=filter('resource_type', 'Microsoft.ServiceBus/namespaces') and ${module.filter-tags.filter_custom})${var.server_errors_aggregation_function}
+		A = data('ServerErrors', filter=filter('resource_type', 'Microsoft.ServiceBus/namespaces') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom})${var.server_errors_aggregation_function}
+		B = data('IncomingRequests', filter=filter('resource_type', 'Microsoft.ServiceBus/namespaces') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom})${var.server_errors_aggregation_function}
 		signal = ((A/B)*100).${var.server_errors_transformation_function}(over='${var.server_errors_transformation_window}').publish('signal')
 		aperiodic.above_or_below_detector(signal, ${var.server_errors_threshold_critical}, 'above', lasting('${var.server_errors_aperiodic_duration}', ${var.server_errors_aperiodic_percentage})).publish('CRIT')
 		aperiodic.above_or_below_detector(signal, ${var.server_errors_threshold_warning}, 'above', lasting('${var.server_errors_aperiodic_duration}', ${var.server_errors_aperiodic_percentage})).publish('WARN')
