041a504916a873c20bcc518a2472b06c396733a9
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-04-01 14:01:38 -0700

    Update variables.tf

--- cloud/azure/virtual-machine/variables.tf
+++ cloud/azure/virtual-machine/variables.tf
@@ -97,7 +97,7 @@ variable "cpu_usage_notifications_critical" {
 variable "cpu_usage_aggregation_function" {
   description = "Aggregation function and group by for cpu_usage detector (i.e. \".mean(by=['host'])\")"
   type        = string
-  default     = ".mean(by=['azure_resource_group_name', 'azure_resource_name'])"
+  default     = ".mean(by=['azure_region', 'azure_resource_group_name', 'azure_resource_name'])"
 }
 
 variable "cpu_usage_transformation_function" {
@@ -165,7 +165,7 @@ variable "credit_cpu_notifications_critical" {
 variable "credit_cpu_aggregation_function" {
   description = "Aggregation function and group by for credit_cpu detector (i.e. \".mean(by=['host'])\")"
   type        = string
-  default     = ".mean(by=['azure_resource_group_name', 'azure_resource_name'])"
+  default     = ".mean(by=['azure_region', 'azure_resource_group_name', 'azure_resource_name'])"
 }
 
 variable "credit_cpu_transformation_function" {
@@ -190,4 +190,208 @@ variable "credit_cpu_threshold_warning" {
   description = "Warning threshold for credit_cpu detector"
   type        = number
   default     = 30
+}
+
+# RAM_reserved detectors
+
+variable "ram_reserved_disabled" {
+  description = "Disable all alerting rules for ram_reserved detector"
+  type        = bool
+  default     = null
+}
+
+variable "ram_reserved_disabled_critical" {
+  description = "Disable critical alerting rule for ram_reserved detector"
+  type        = bool
+  default     = null
+}
+
+variable "ram_reserved_disabled_warning" {
+  description = "Disable warning alerting rule for ram_reserved detector"
+  type        = bool
+  default     = null
+}
+
+variable "ram_reserved_notifications" {
+  description = "Notification recipients list for every alerting rules of ram_reserved detector"
+  type        = list
+  default     = []
+}
+
+variable "ram_reserved_notifications_warning" {
+  description = "Notification recipients list for warning alerting rule of ram_reserved detector"
+  type        = list
+  default     = []
+}
+
+variable "ram_reserved_notifications_critical" {
+  description = "Notification recipients list for critical alerting rule of ram_reserved detector"
+  type        = list
+  default     = []
+}
+
+variable "ram_reserved_aggregation_function" {
+  description = "Aggregation function and group by for ram_reserved detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ".mean(by=['azure_region', 'azure_resource_group_name', 'azure_resource_name'])"
+}
+
+variable "ram_reserved_transformation_function" {
+  description = "Transformation function for ram_reserved detector (mean, min, max)"
+  type        = string
+  default     = "min"
+}
+
+variable "ram_reserved_transformation_window" {
+  description = "Transformation window for ram_reserved detector (i.e. 5m, 20m, 1h, 1d)"
+  type        = string
+  default     = "15m"
+}
+
+variable "ram_reserved_threshold_critical" {
+  description = "Critical threshold for ram_reserved detector"
+  type        = number
+  default     = 95
+}
+
+variable "ram_reserved_threshold_warning" {
+  description = "Warning threshold for ram_reserved detector"
+  type        = number
+  default     = 90
+}
+
+# Http_success_status_rate detectors
+
+variable "http_success_status_rate_disabled" {
+  description = "Disable all alerting rules for http_success_status_rate detector"
+  type        = bool
+  default     = null
+}
+
+variable "http_success_status_rate_disabled_critical" {
+  description = "Disable critical alerting rule for http_success_status_rate detector"
+  type        = bool
+  default     = null
+}
+
+variable "http_success_status_rate_disabled_warning" {
+  description = "Disable warning alerting rule for http_success_status_rate detector"
+  type        = bool
+  default     = null
+}
+
+variable "http_success_status_rate_notifications" {
+  description = "Notification recipients list for every alerting rules of http_success_status_rate detector"
+  type        = list
+  default     = []
+}
+
+variable "http_success_status_rate_notifications_warning" {
+  description = "Notification recipients list for warning alerting rule of http_success_status_rate detector"
+  type        = list
+  default     = []
+}
+
+variable "http_success_status_rate_notifications_critical" {
+  description = "Notification recipients list for critical alerting rule of http_success_status_rate detector"
+  type        = list
+  default     = []
+}
+
+variable "http_success_status_rate_aggregation_function" {
+  description = "Aggregation function and group by for http_success_status_rate detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ".mean(by=['azure_region', 'azure_resource_group_name', 'azure_resource_name'])"
+}
+
+variable "http_success_status_rate_transformation_function" {
+  description = "Transformation function for http_success_status_rate detector (mean, min, max)"
+  type        = string
+  default     = "max"
+}
+
+variable "http_success_status_rate_transformation_window" {
+  description = "Transformation window for http_success_status_rate detector (i.e. 5m, 20m, 1h, 1d)"
+  type        = string
+  default     = "5m"
+}
+
+variable "http_success_status_rate_threshold_critical" {
+  description = "Critical threshold for http_success_status_rate detector"
+  type        = number
+  default     = 10
+}
+
+variable "http_success_status_rate_threshold_warning" {
+  description = "Warning threshold for http_success_status_rate detector"
+  type        = number
+  default     = 30
+}
+
+# Status detectors
+
+variable "status_disabled" {
+  description = "Disable all alerting rules for status detector"
+  type        = bool
+  default     = null
+}
+
+variable "status_disabled_critical" {
+  description = "Disable critical alerting rule for status detector"
+  type        = bool
+  default     = null
+}
+
+variable "status_disabled_warning" {
+  description = "Disable warning alerting rule for status detector"
+  type        = bool
+  default     = null
+}
+
+variable "status_notifications" {
+  description = "Notification recipients list for every alerting rules of status detector"
+  type        = list
+  default     = []
+}
+
+variable "status_notifications_warning" {
+  description = "Notification recipients list for warning alerting rule of status detector"
+  type        = list
+  default     = []
+}
+
+variable "status_notifications_critical" {
+  description = "Notification recipients list for critical alerting rule of status detector"
+  type        = list
+  default     = []
+}
+
+variable "status_aggregation_function" {
+  description = "Aggregation function and group by for status detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ".mean(by=['azure_region', 'azure_resource_group_name', 'azure_resource_name'])"
+}
+
+variable "status_transformation_function" {
+  description = "Transformation function for status detector (mean, min, max)"
+  type        = string
+  default     = "max"
+}
+
+variable "status_transformation_window" {
+  description = "Transformation window for status detector (i.e. 5m, 20m, 1h, 1d)"
+  type        = string
+  default     = "5m"
+}
+
+variable "status_threshold_critical" {
+  description = "Critical threshold for status detector"
+  type        = number
+  default     = 1
+}
+
+variable "status_threshold_warning" {
+  description = "Warning threshold for status detector"
+  type        = number
+  default     = 1
 }
