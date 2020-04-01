30269b7df1567c8d21c8927998d98506a6cf219b
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-04-01 08:05:58 -0700

    Update detectors-eventgrid.tf

--- cloud/azure/eventgrid/detectors-eventgrid.tf
+++ cloud/azure/eventgrid/detectors-eventgrid.tf
@@ -3,7 +3,7 @@ resource "signalfx_detector" "heartbeat" {
 
 	program_text = <<-EOF
 		from signalfx.detectors.not_reporting import not_reporting
-		signal = data('DeliverySuccessCount', filter=filter('resource_type', 'Microsoft.EventGrid/topics') and ${module.filter-tags.filter_custom}).publish('signal')
+		signal = data('DeliverySuccessCount', filter=filter('resource_type', 'Microsoft.EventGrid/topics') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom}).publish('signal')
 		not_reporting.detector(stream=signal, resource_identifier=['EventSubscriptionName'], duration='${var.heartbeat_timeframe}').publish('CRIT')
 	EOF
 
@@ -21,7 +21,7 @@ resource "signalfx_detector" "no_successful_message" {
 	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure eventgrid successful message"
 
 	program_text = <<-EOF
-		signal = data('PublishSuccessCount', filter=filter('resource_type', 'Microsoft.EventGrid/topics')and ${module.filter-tags.filter_custom})${var.no_successful_message_aggregation_function}.${var.no_successful_message_transformation_function}(over='${var.no_successful_message_transformation_window}').publish('signal')
+		signal = data('PublishSuccessCount', filter=filter('resource_type', 'Microsoft.EventGrid/topics')and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom})${var.no_successful_message_aggregation_function}.${var.no_successful_message_transformation_function}(over='${var.no_successful_message_transformation_window}').publish('signal')
 		detect(when(signal < ${var.no_successful_message_threshold_critical})).publish('CRIT')
 	EOF
 
@@ -41,9 +41,9 @@ resource "signalfx_detector" "failed_messages" {
 
 	program_text = <<-EOF
 		from signalfx.detectors.aperiodic import aperiodic
-		A = data('PublishFailCount', filter=filter('resource_type', 'Microsoft.EventGrid/topics') and ${module.filter-tags.filter_custom})${var.failed_messages_aggregation_function}
-		B = data('PublishSuccessCount', filter=filter('resource_type', 'Microsoft.EventGrid/topics') and ${module.filter-tags.filter_custom})${var.failed_messages_aggregation_function}
-		C = data('UnmatchedEventCount', filter=filter('resource_type', 'Microsoft.EventGrid/topics') and ${module.filter-tags.filter_custom})${var.failed_messages_aggregation_function}
+		A = data('PublishFailCount', filter=filter('resource_type', 'Microsoft.EventGrid/topics') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom})${var.failed_messages_aggregation_function}
+		B = data('PublishSuccessCount', filter=filter('resource_type', 'Microsoft.EventGrid/topics') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom})${var.failed_messages_aggregation_function}
+		C = data('UnmatchedEventCount', filter=filter('resource_type', 'Microsoft.EventGrid/topics') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom})${var.failed_messages_aggregation_function}
 		signal = ((A/(A+B+C))*100).${var.failed_messages_transformation_function}(over='${var.failed_messages_transformation_window}').publish('signal')
 		aperiodic.above_or_below_detector(signal, ${var.failed_messages_threshold_critical}, 'above', lasting('${var.failed_messages_aperiodic_duration}', ${var.failed_messages_aperiodic_percentage})).publish('CRIT')
 		aperiodic.above_or_below_detector(signal, ${var.failed_messages_threshold_warning}, 'above', lasting('${var.failed_messages_aperiodic_duration}', ${var.failed_messages_aperiodic_percentage})).publish('WARN')
@@ -73,9 +73,9 @@ resource "signalfx_detector" "unmatched_events" {
 
 	program_text = <<-EOF
 		from signalfx.detectors.aperiodic import aperiodic
-		A = data('UnmatchedEventCount', filter=filter('resource_type', 'Microsoft.EventGrid/topics') and ${module.filter-tags.filter_custom})${var.unmatched_events_aggregation_function}
-		B = data('PublishSuccessCount', filter=filter('resource_type', 'Microsoft.EventGrid/topics') and ${module.filter-tags.filter_custom})${var.unmatched_events_aggregation_function}
-		C = data('PublishFailCount', filter=filter('resource_type', 'Microsoft.EventGrid/topics') and ${module.filter-tags.filter_custom})${var.unmatched_events_aggregation_function}
+		A = data('UnmatchedEventCount', filter=filter('resource_type', 'Microsoft.EventGrid/topics') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom})${var.unmatched_events_aggregation_function}
+		B = data('PublishSuccessCount', filter=filter('resource_type', 'Microsoft.EventGrid/topics') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom})${var.unmatched_events_aggregation_function}
+		C = data('PublishFailCount', filter=filter('resource_type', 'Microsoft.EventGrid/topics') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom})${var.unmatched_events_aggregation_function}
 		signal = ((A/(A+B+C))*100).${var.unmatched_events_transformation_function}(over='${var.unmatched_events_transformation_window}').publish('signal')
 		aperiodic.above_or_below_detector(signal, ${var.unmatched_events_threshold_critical}, 'above', lasting('${var.unmatched_events_aperiodic_duration}', ${var.unmatched_events_aperiodic_percentage})).publish('CRIT')
 		aperiodic.above_or_below_detector(signal, ${var.unmatched_events_threshold_warning}, 'above', lasting('${var.unmatched_events_aperiodic_duration}', ${var.unmatched_events_aperiodic_percentage})).publish('WARN')
