f3f3007b33390b74695f431613c497c98261e034
mel-splunk <58855914+mel-splunk@users.noreply.github.com>
2020-04-03 09:15:44 -0700

    Update detectors-app_services.tf

--- cloud/azure/app-services/detectors-app_services.tf
+++ cloud/azure/app-services/detectors-app_services.tf
@@ -177,7 +177,6 @@ resource "signalfx_detector" "status" {
 	program_text = <<-EOF
 		signal = data('HealthCheckStatus', filter=filter('resource_type', 'Microsoft.Web/sites') and filter('is_Azure_Function', 'false') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom})${var.status_aggregation_function}.${var.status_transformation_function}(over='${var.status_transformation_window}').publish('signal')
 		detect(when(signal < ${var.status_threshold_critical})).publish('CRIT')
-		detect(when(signal < ${var.status_threshold_warning})).publish('WARN')
 	EOF
 
 	rule {
