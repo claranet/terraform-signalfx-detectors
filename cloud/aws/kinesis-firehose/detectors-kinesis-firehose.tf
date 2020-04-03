a8c4e0cff9ea00214e405c6cb64e61a2d79294ae
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-04-02 17:43:39 -0700

    Update detectors-kinesis-firehose.tf

--- cloud/aws/kinesis-firehose/detectors-kinesis-firehose.tf
+++ cloud/aws/kinesis-firehose/detectors-kinesis-firehose.tf
@@ -1,47 +1,47 @@
 resource "signalfx_detector" "heartbeat" {
-  name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] AWS Kinesis heartbeat"
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] AWS Kinesis heartbeat"
 
-  program_text = <<-EOF
+	program_text = <<-EOF
 		from signalfx.detectors.not_reporting import not_reporting
 		signal = data('IncomingBytes', filter=filter('stat', 'mean') and filter('namespace', 'AWS/Kinesis') and ${module.filter-tags.filter_custom}).publish('signal')
 		not_reporting.detector(stream=signal, resource_identifier=['StreamName'], duration='${var.heartbeat_timeframe}').publish('CRIT')
 	EOF
 
-  rule {
-    description           = "has not reported in ${var.heartbeat_timeframe}"
-    severity              = "Critical"
-    detect_label          = "CRIT"
-    disabled              = coalesce(var.heartbeat_disabled, var.detectors_disabled)
-    notifications         = coalescelist(var.heartbeat_notifications, var.notifications)
-    parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{readableRule}}} on {{{dimensions}}}"
-  }
+	rule {
+		description           = "has not reported in ${var.heartbeat_timeframe}"
+		severity              = "Critical"
+		detect_label          = "CRIT"
+		disabled              = coalesce(var.heartbeat_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.heartbeat_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{readableRule}}} on {{{dimensions}}}"
+	}
 }
 
 resource "signalfx_detector" "incoming_records" {
-  name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] AWS Kinesis incoming records"
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] AWS Kinesis incoming records"
 
-  program_text = <<-EOF
+	program_text = <<-EOF
 		signal = data('IncomingRecords', filter=filter('namespace', 'AWS/Kinesis') and filter('stat', 'lower') and (not filter('ShardId', '*')) and ${module.filter-tags.filter_custom})${var.incoming_records_aggregation_function}.${var.incoming_records_transformation_function}(over='${var.incoming_records_transformation_window}').publish('signal')
 		detect(when(signal <= ${var.incoming_records_threshold_critical})).publish('CRIT')
-		detect(when(signal <= ${var.incoming_records_threshold_warning})).publish('WARN')
+		detect(when(signal <= ${var.incoming_records_threshold_warning}) and when(signal >= ${var.incoming_records_threshold_critical})).publish('WARN')
 	EOF
 
-  rule {
-    description           = "are too low <= ${var.incoming_records_threshold_critical}"
-    severity              = "Critical"
-    detect_label          = "CRIT"
-    disabled              = coalesce(var.incoming_records_disabled_critical, var.incoming_records_disabled, var.detectors_disabled)
-    notifications         = coalescelist(var.incoming_records_notifications_critical, var.incoming_records_notifications, var.notifications)
-    parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{readableRule}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
-  }
+	rule {
+		description           = "are too low <= ${var.incoming_records_threshold_critical}"
+		severity              = "Critical"
+		detect_label          = "CRIT"
+		disabled              = coalesce(var.incoming_records_disabled_critical, var.incoming_records_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.incoming_records_notifications_critical, var.incoming_records_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{readableRule}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
 
-  rule {
-    description           = "are too low <= ${var.incoming_records_threshold_warning}"
-    severity              = "Warning"
-    detect_label          = "WARN"
-    disabled              = coalesce(var.incoming_records_disabled_warning, var.incoming_records_disabled, var.detectors_disabled)
-    notifications         = coalescelist(var.incoming_records_notifications_warning, var.incoming_records_notifications, var.notifications)
-    parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{readableRule}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
-  }
+	rule {
+		description           = "are too low <= ${var.incoming_records_threshold_warning}"
+		severity              = "Warning"
+		detect_label          = "WARN"
+		disabled              = coalesce(var.incoming_records_disabled_warning, var.incoming_records_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.incoming_records_notifications_warning, var.incoming_records_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{readableRule}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
 
 }
