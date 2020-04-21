1dbaeabf98ee7e45ab612e6247f58558fb7dc44a
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-04-21 09:56:51 -0700

    Update outputs.tf

--- cloud/azure/eventhub/outputs.tf
+++ cloud/azure/eventhub/outputs.tf
@@ -3,6 +3,11 @@ output "heartbeat_id" {
   value       = signalfx_detector.heartbeat.*.id
 }
 
+output "eventhub_failed_requests_id" {
+  description = "id for detector eventhub_failed_requests"
+  value       = signalfx_detector.eventhub_failed_requests.*.id
+}
+
 output "eventhub_errors_id" {
   description = "id for detector eventhub_errors"
   value       = signalfx_detector.eventhub_errors.*.id
