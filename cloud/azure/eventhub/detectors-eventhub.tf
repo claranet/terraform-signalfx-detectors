d3e787b4643891e88c4f6a19d90b379d33453433
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-04-21 09:57:11 -0700

    Update detectors-eventhub.tf

--- cloud/azure/eventhub/detectors-eventhub.tf
+++ cloud/azure/eventhub/detectors-eventhub.tf
@@ -1,5 +1,5 @@
 resource "signalfx_detector" "heartbeat" {
-	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure Event Hub heartbeat"
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure eventhub heartbeat"
 
 	program_text = <<-EOF
 		from signalfx.detectors.not_reporting import not_reporting
@@ -17,8 +17,41 @@ resource "signalfx_detector" "heartbeat" {
 	}
 }
 
+resource "signalfx_detector" "eventhub_failed_requests" {
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure eventhub failed requests"
+
+	program_text = <<-EOF
+		from signalfx.detectors.aperiodic import aperiodic
+		A = data('IncomingRequests', filter=filter('resource_type', 'Microsoft.EventHub/namespaces') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom})${var.eventhub_failed_requests_aggregation_function}
+		B = data('SuccessfulRequests', filter=filter('resource_type', 'Microsoft.EventHub/namespaces') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom})${var.eventhub_failed_requests_aggregation_function}
+		signal = (((A-B)/A)*100).${var.eventhub_failed_requests_transformation_function}(over='${var.eventhub_failed_requests_transformation_window}').publish('signal')
+		aperiodic.above_or_below_detector(signal, ${var.eventhub_failed_requests_threshold_critical}, 'above', lasting('${var.eventhub_failed_requests_aperiodic_duration}', ${var.eventhub_failed_requests_aperiodic_percentage})).publish('CRIT')
+		aperiodic.range_detector(signal, ${var.eventhub_failed_requests_threshold_warning}, ${var.eventhub_failed_requests_threshold_critical}, 'within_range', lasting('${var.eventhub_failed_requests_aperiodic_duration}', ${var.eventhub_failed_requests_aperiodic_percentage})).publish('WARN')
+
+	EOF
+
+	rule {
+		description           = "is too high > ${var.eventhub_failed_requests_threshold_critical}"
+		severity              = "Critical"
+		detect_label          = "CRIT"
+		disabled              = coalesce(var.eventhub_failed_requests_disabled_critical, var.eventhub_failed_requests_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.eventhub_failed_requests_notifications_critical, var.eventhub_failed_requests_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{readableRule}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
+
+	rule {
+		description           = "is too high > ${var.eventhub_failed_requests_threshold_warning}"
+		severity              = "Warning"
+		detect_label          = "WARN"
+		disabled              = coalesce(var.eventhub_failed_requests_disabled_warning, var.eventhub_failed_requests_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.eventhub_failed_requests_notifications_warning, var.eventhub_failed_requests_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{readableRule}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
+	}
+
+}
+
 resource "signalfx_detector" "eventhub_errors" {
-	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure Event Hub error rate"
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure eventhub errors"
 
 	program_text = <<-EOF
 		from signalfx.detectors.aperiodic import aperiodic
