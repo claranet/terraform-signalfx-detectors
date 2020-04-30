9596b4a135f2deddfc4ff5393688cac12c875d28
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-04-29 18:10:58 -0700

    Create outputs.tf

---
+++ caas/kubernetes/node/outputs.tf
@@ -0,0 +1,14 @@
+output "heartbeat_id" {
+  description = "id for detector heartbeat"
+  value       = signalfx_detector.heartbeat.*.id
+}
+
+output "ready_id" {
+  description = "id for detector ready"
+  value       = signalfx_detector.ready.*.id
+}
+
+output "volume_space_id" {
+  description = "id for detector volume_space"
+  value       = signalfx_detector.volume_space.*.id
+}
