d828cbe78189af6a4b7fb8a1a65ac76e13432d59
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-05-01 08:58:30 -0700

    Update detectors-k8s-workload.tf

--- caas/kubernetes/workload/detectors-k8s-workload.tf
+++ caas/kubernetes/workload/detectors-k8s-workload.tf
@@ -1,3 +1,22 @@
+resource "signalfx_detector" "heartbeat" {
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Kubernetes API server heartbeat"
+
+	program_text = <<-EOF
+		from signalfx.detectors.not_reporting import not_reporting
+		signal = data('kubernetes.job.failed' and ${module.filter-tags.filter_custom})
+		not_reporting.detector(stream=signal, resource_identifier=['host'], duration='${var.heartbeat_timeframe}').publish('CRIT')
+	EOF
+
+	rule {
+		description           = "has not reported in ${var.heartbeat_timeframe}"
+		severity              = "Critical"
+		detect_label          = "CRIT"
+		disabled              = coalesce(var.heartbeat_disabled, var.detectors_disabled)
+		notifications         = coalescelist(var.heartbeat_notifications, var.notifications)
+		parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} on {{{dimensions}}}"
+	}
+}
+
 resource "signalfx_detector" "replica_available" {
 	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Kubernetes workload available replicas"
 
