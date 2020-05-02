e599db7944dc71ece48a210ac0225ae76b297279
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-05-01 19:06:47 -0700

    Update variables.tf

--- cloud/azure/app-services/variables.tf
+++ cloud/azure/app-services/variables.tf
@@ -136,6 +136,12 @@ variable "response_time_aperiodic_percentage" {
   default     = 0.9
 }
 
+variable "response_time_aperiodic_upper_strict" {
+  description = "If True, compare stream against upper with strict inequality; if False, non-strict"
+  type        = bool
+  default     = "0"
+}
+
 # Memory_usage_count detectors
 
 variable "memory_usage_count_disabled" {
@@ -284,6 +290,12 @@ variable "http_5xx_errors_count_aperiodic_percentage" {
   default     = 0.9
 }
 
+variable "http_5xx_errors_count_aperiodic_upper_strict" {
+  description = "If True, compare stream against upper with strict inequality; if False, non-strict"
+  type        = bool
+  default     = "0"
+}
+
 # http_4xx_errors_count detectors
 
 variable "http_4xx_errors_count_disabled" {
@@ -364,6 +376,12 @@ variable "http_4xx_errors_count_aperiodic_percentage" {
   default     = 0.9
 }
 
+variable "http_4xx_errors_count_aperiodic_upper_strict" {
+  description = "If True, compare stream against upper with strict inequality; if False, non-strict"
+  type        = bool
+  default     = "0"
+}
+
 # Http_success_status_rate detectors
 
 variable "http_success_status_rate_disabled" {
@@ -442,6 +460,12 @@ variable "http_success_status_rate_aperiodic_percentage" {
   description = "Percentage for the http_success_status_rate block"
   type        = number
   default     = 0.9
+}
+
+variable "http_success_status_rate_aperiodic_upper_strict" {
+  description = "If True, compare stream against upper with strict inequality; if False, non-strict"
+  type        = bool
+  default     = "0"
 }
 
 # Status detectors
