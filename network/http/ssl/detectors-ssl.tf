3292f5dc8a3c16a4b9338eb777509268ad5476a5
mel-splunk <58855914+mel-splunk@users.noreply.github.com>
2020-05-02 12:33:44 -0700

    Update detectors-ssl.tf

--- network/http/ssl/detectors-ssl.tf
+++ network/http/ssl/detectors-ssl.tf
@@ -1,14 +1,14 @@
 resource "signalfx_detector" "invalid_ssl_certificate" {
-	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] SSL valid certificate count"
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] SSL invalid certificate count"
 
 	program_text = <<-EOF
-		signal = data('http.certificate_valid' and ${module.filter-tags.filter_custom})${var.invalid_ssl_certificate_aggregation_function}.${var.invalid_ssl_certificate_transformation_function}(over='${var.invalid_ssl_certificate_transformation_window}').publish('signal')
-		detect(when(signal < ${var.invalid_ssl_certificate_threshold_critical})).publish('CRIT')
-		detect(when(signal < ${var.invalid_ssl_certificate_threshold_warning}) and when(signal <= ${var.invalid_ssl_certificate_threshold_critical})).publish('WARN')
+		signal = data('http.cert_expiry', filter=filter('isValid', 'false') and ${module.filter-tags.filter_custom})${var.invalid_ssl_certificate_aggregation_function}.count(by=['serverName', 'url']).${var.invalid_ssl_certificate_transformation_function}(over='${var.invalid_ssl_certificate_transformation_window}').publish('signal')
+		detect(when(signal > ${var.invalid_ssl_certificate_threshold_critical})).publish('CRIT')
+		detect(when(signal > ${var.invalid_ssl_certificate_threshold_warning}) and when(signal <= ${var.invalid_ssl_certificate_threshold_critical})).publish('WARN')
 	EOF
 
 	rule {
-		description           = "is too low < ${var.invalid_ssl_certificate_threshold_critical}"
+		description           = "is too high > ${var.invalid_ssl_certificate_threshold_critical}"
 		severity              = "Critical"
 		detect_label          = "CRIT"
 		disabled              = coalesce(var.invalid_ssl_certificate_disabled_critical, var.invalid_ssl_certificate_disabled, var.detectors_disabled)
@@ -17,7 +17,7 @@ resource "signalfx_detector" "invalid_ssl_certificate" {
 	}
 
 	rule {
-		description           = "is too low < ${var.invalid_ssl_certificate_threshold_warning}"
+		description           = "is too high < ${var.invalid_ssl_certificate_threshold_warning}"
 		severity              = "Warning"
 		detect_label          = "WARN"
 		disabled              = coalesce(var.invalid_ssl_certificate_disabled_warning, var.invalid_ssl_certificate_disabled, var.detectors_disabled)
@@ -27,17 +27,17 @@ resource "signalfx_detector" "invalid_ssl_certificate" {
 }
 
 resource "signalfx_detector" "certificate_expiration_date" {
-	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] SSL certificate expiring in days"
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] SSL certificate expiring "
 
 	program_text = <<-EOF
-		A = data(' http.certificate_expiration' and ${module.filter-tags.filter_custom})${var.certificate_expiration_date_aggregation_function}
+		A = data(' http.certificate_expiry' and ${module.filter-tags.filter_custom})${var.certificate_expiration_date_aggregation_function}
 		signal = (A/86400).${var.certificate_expiration_date_transformation_function}(over='${var.certificate_expiration_date_transformation_window}').publish('signal')
 		detect(when(signal < ${var.certificate_expiration_date_threshold_critical})).publish('CRIT')
 		detect(when(signal < ${var.certificate_expiration_date_threshold_warning}) and when(signal >= ${var.certificate_expiration_date_threshold_critical})).publish('WARN')
 	EOF
 
 	rule {
-		description           = "is too low < ${var.certificate_expiration_date_threshold_critical} days"
+		description           = " < ${var.certificate_expiration_date_threshold_critical} days"
 		severity              = "Critical"
 		detect_label          = "CRIT"
 		disabled              = coalesce(var.certificate_expiration_date_disabled_critical, var.certificate_expiration_date_disabled, var.detectors_disabled)
@@ -46,7 +46,7 @@ resource "signalfx_detector" "certificate_expiration_date" {
 	}
 
 	rule {
-		description           = "is too low < ${var.certificate_expiration_date_threshold_warning} days"
+		description           = " < ${var.certificate_expiration_date_threshold_warning} days"
 		severity              = "Warning"
 		detect_label          = "WARN"
 		disabled              = coalesce(var.certificate_expiration_date_disabled_warning, var.certificate_expiration_date_disabled, var.detectors_disabled)
