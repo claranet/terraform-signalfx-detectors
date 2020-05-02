a47f6d14328078c0c5d30c14bd380dd0774511fd
mel-splunk <58855914+mel-splunk@users.noreply.github.com>
2020-05-02 13:29:27 -0700

    Update detectors-k8s-cluster.tf

--- caas/kubernetes/cluster/detectors-k8s-cluster.tf
+++ caas/kubernetes/cluster/detectors-k8s-cluster.tf
@@ -4,7 +4,7 @@ resource "signalfx_detector" "heartbeat" {
 	program_text = <<-EOF
 		from signalfx.detectors.not_reporting import not_reporting
 		signal = data('kubernetes_build_info' and ${module.filter-tags.filter_custom})
-		not_reporting.detector(stream=signal, resource_identifier=['host'], duration='${var.heartbeat_timeframe}').publish('CRIT')
+		not_reporting.detector(stream=signal, resource_identifier=['kubernetes_node'], duration='${var.heartbeat_timeframe}').publish('CRIT')
 	EOF
 
 	rule {
