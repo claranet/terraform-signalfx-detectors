8d850e71ba454cd927b6c50ef25e2fc47bfdc098
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-05-01 20:48:06 -0700

    Update variables.tf

--- cloud/azure/eventgrid/variables.tf
+++ cloud/azure/eventgrid/variables.tf
@@ -204,6 +204,12 @@ variable "failed_messages_aperiodic_percentage" {
   default     = 0.9
 }
 
+variable "failed_messages_aperiodic_upper_strict" {
+  description = "If True, compare stream against upper with strict inequality; if False, non-strict"
+  type        = bool
+  default     = "0"
+}
+
 # unmatched_events detectors
 
 variable "unmatched_events_disabled" {
@@ -282,4 +288,10 @@ variable "unmatched_events_aperiodic_percentage" {
   description = "Percentage for the unmatched_events block"
   type        = number
   default     = 0.9
+}
+
+variable "unmatched_events_aperiodic_upper_strict" {
+  description = "If True, compare stream against upper with strict inequality; if False, non-strict"
+  type        = bool
+  default     = "0"
 }
