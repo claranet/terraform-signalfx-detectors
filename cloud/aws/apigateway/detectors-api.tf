5d221b5d206b7a4270c80a5af070d6f68a5b567c
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-05-01 18:51:56 -0700

    Update detectors-api.tf

--- cloud/aws/apigateway/detectors-api.tf
+++ cloud/aws/apigateway/detectors-api.tf
@@ -25,7 +25,7 @@ resource "signalfx_detector" "latency" {
 		from signalfx.detectors.aperiodic import aperiodic
 		signal = data('Latency', filter=filter('namespace', 'AWS/ApiGateway') and filter('stat', 'mean')and (not filter('Stage', '*'))and (not filter('Method', '*'))and (not filter('Resource', '*')) and ${module.filter-tags.filter_custom})${var.latency_aggregation_function}.${var.latency_transformation_function}(over='${var.latency_transformation_window}').publish('signal')
 		aperiodic.above_or_below_detector(signal, ${var.latency_threshold_critical}, 'above', lasting('${var.latency_aperiodic_duration}', ${var.latency_aperiodic_percentage})).publish('CRIT')
-		aperiodic.range_detector(signal, ${var.latency_threshold_warning}, ${var.latency_threshold_critical}, 'within_range', lasting('${var.latency_aperiodic_duration}', ${var.latency_aperiodic_percentage})).publish('WARN')
+		aperiodic.range_detector(signal, ${var.latency_threshold_warning}, ${var.latency_threshold_critical}, 'within_range', lasting('${var.latency_aperiodic_duration}', ${var.latency_aperiodic_percentage}), upper_strict=False).publish('WARN')
 	EOF
 
   rule {
