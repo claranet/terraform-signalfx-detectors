b653e2f8e263258b23045e73a5729c287617f6ef
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-05-02 04:01:04 -0700

    Update variables.tf

--- caas/kubernetes/ingress/vts/variables.tf
+++ caas/kubernetes/ingress/vts/variables.tf
@@ -139,7 +139,7 @@ variable "nginx_ingress_too_many_5xx_aperiodic_percentage" {
 variable "nginx_ingress_too_many_5xx_aperiodic_upper_strict" {
   description = "If True, compare stream against upper with strict inequality; if False, non-strict"
   type        = bool
-  default     = "0"
+  default     = false
 }
 
 # Nginx_ingress_too_many_4xx detectors
@@ -225,5 +225,5 @@ variable "nginx_ingress_too_many_4xx_aperiodic_percentage" {
 variable "nginx_ingress_too_many_4xx_aperiodic_upper_strict" {
   description = "If True, compare stream against upper with strict inequality; if False, non-strict"
   type        = bool
-  default     = "0"
+  default     = false
 }
