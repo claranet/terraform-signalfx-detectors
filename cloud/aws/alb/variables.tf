031fcabcdbc691ff5a09e2747db94a25533f0ac3
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-05-02 03:50:49 -0700

    Update variables.tf

--- cloud/aws/alb/variables.tf
+++ cloud/aws/alb/variables.tf
@@ -207,7 +207,8 @@ variable "latency_aperiodic_percentage" {
 variable "latency_aperiodic_upper_strict" {
   description = "If True, compare stream against upper with strict inequality; if False, non-strict"
   type        = bool
-  default     = "0"
+  default     = false
+
 }
 
 # Httpcode_5xx detectors
