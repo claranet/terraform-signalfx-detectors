1673480c36be9ba3fb7bf656d8f1d9fe1433658f
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-05-05 08:38:52 -0700

    Update variables.tf

--- cloud/aws/apigateway/variables.tf
+++ cloud/aws/apigateway/variables.tf
@@ -136,13 +136,6 @@ variable "latency_aperiodic_percentage" {
   default     = 0.9
 }
 
-variable "latency_aperiodic_upper_strict" {
-  description = "If True, compare stream against upper with strict inequality; if False, non-strict"
-  type        = bool
-  default     = False
-
-}
-
 # Http_5xx_errors detectors
 
 variable "http_5xx_errors_disabled" {
