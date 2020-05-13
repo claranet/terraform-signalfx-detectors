e34a44ee0828d74b0ecb2c49073e3529fabecf90
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-05-13 16:26:31 -0700

    Update detectors-apache.tf

--- middleware/apache/detectors-apache.tf
+++ middleware/apache/detectors-apache.tf
@@ -3,7 +3,7 @@ resource "signalfx_detector" "heartbeat" {
 
 	program_text = <<-EOF
 		from signalfx.detectors.not_reporting import not_reporting
-		signal = data('apache_connections' and ${module.filter-tags.filter_custom})
+		signal = data('apache_connections', ${module.filter-tags.filter_custom})
 		not_reporting.detector(stream=signal, resource_identifier=['host'], duration='${var.heartbeat_timeframe}').publish('CRIT')
 	EOF
 
