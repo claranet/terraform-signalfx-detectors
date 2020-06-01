568c33e2bedd052898deb6c0f580a4212f116ccc
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-06-01 15:43:13 -0700

    Delete outputs.tf

--- network/http/ssl/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "invalid_ssl_certificate_id" {
-  description = "id for detector invalid_ssl_certificate"
-  value       = signalfx_detector.invalid_ssl_certificate.*.id
-}
-
-output "certificate_expiration_date_id" {
-  description = "id for detector certificate_expiration_date"
-  value       = signalfx_detector.certificate_expiration_date.*.id
-}
