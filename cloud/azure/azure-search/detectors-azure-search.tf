bea2aa9a44c519d92886cc5e06496042186baeee
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-06-03 13:04:08 -0700

    Update detectors-azure-search.tf

--- cloud/azure/azure-search/detectors-azure-search.tf
+++ cloud/azure/azure-search/detectors-azure-search.tf
@@ -1,22 +1,3 @@
-resource "signalfx_detector" "heartbeat" {
-  name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure Search heartbeat"
-
-  program_text = <<-EOF
-		from signalfx.detectors.not_reporting import not_reporting
-		signal = data('SearchQueriesPerSecond', filter=filter('resource_type', 'Microsoft.Search/searchServices') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom}).publish('signal')
-		not_reporting.detector(stream=signal, resource_identifier=['azure_resource_id'], duration='${var.heartbeat_timeframe}').publish('CRIT')
-	EOF
-
-  rule {
-    description           = "has not reported in ${var.heartbeat_timeframe}"
-    severity              = "Critical"
-    detect_label          = "CRIT"
-    disabled              = coalesce(var.heartbeat_disabled, var.detectors_disabled)
-    notifications         = coalescelist(var.heartbeat_notifications, var.notifications)
-    parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{readableRule}}} on {{{dimensions}}}"
-  }
-}
-
 resource "signalfx_detector" "search_latency" {
   name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure Search latency"
 
