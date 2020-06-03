3113b3e32b1ccaa16a493739cead9e0fe9151679
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-06-03 12:56:35 -0700

    Update detectors-app-gateway.tf

--- cloud/azure/app-gateway/detectors-app-gateway.tf
+++ cloud/azure/app-gateway/detectors-app-gateway.tf
@@ -3,7 +3,7 @@ resource "signalfx_detector" "heartbeat" {
 
 	program_text = <<-EOF
 		from signalfx.detectors.not_reporting import not_reporting
-		signal = data('CpuUtilization', filter=filter('resource_type', 'Microsoft.Network/applicationGateways') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom}).publish('signal')
+		signal = data('Throughput', filter=filter('resource_type', 'Microsoft.Network/applicationGateways') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom}).publish('signal')
 		not_reporting.detector(stream=signal, resource_identifier=['subscription_id'], duration='${var.heartbeat_timeframe}').publish('CRIT')
 	EOF
 
