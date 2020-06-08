2187cc4277740bf7ceba0ff35581f72046e63498
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-06-08 08:43:40 -0700

    Update detectors-k8s-workload.tf

--- caas/kubernetes/workload/detectors-k8s-workload.tf
+++ caas/kubernetes/workload/detectors-k8s-workload.tf
@@ -1,5 +1,5 @@
 resource "signalfx_detector" "heartbeat" {
-	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Kubernetes API server heartbeat"
+	name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Kubernetes workload heartbeat"
 
 	program_text = <<-EOF
 		from signalfx.detectors.not_reporting import not_reporting
