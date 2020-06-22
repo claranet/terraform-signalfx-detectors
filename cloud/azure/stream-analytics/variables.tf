ed0c1774c4e0ecc35f23fd69cd93def3ebfb6b96
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-06-22 15:58:53 -0700

    Update variables.tf

--- cloud/azure/stream-analytics/variables.tf
+++ cloud/azure/stream-analytics/variables.tf
@@ -36,7 +36,7 @@ variable "detectors_disabled" {
   default     = false
 }
 
-# Azure stream analytics detectors specific
+# GCP Pub/Sub Topic detectors specific
 
 variable "heartbeat_disabled" {
   description = "Disable all alerting rules for heartbeat detector"
@@ -56,292 +56,260 @@ variable "heartbeat_timeframe" {
   default     = "20m"
 }
 
-# SU_utilization detectors
+# sending_operations detectors
 
-variable "su_utilization_disabled" {
-  description = "Disable all alerting rules for su_utilization detector"
+variable "sending_operations_disabled" {
+  description = "Disable all alerting rules for sending_operations detector"
   type        = bool
   default     = null
 }
 
-variable "su_utilization_disabled_critical" {
-  description = "Disable critical alerting rule for su_utilization detector"
+variable "sending_operations_disabled_critical" {
+  description = "Disable critical alerting rule for sending_operations detector"
   type        = bool
   default     = null
 }
 
-variable "su_utilization_disabled_warning" {
-  description = "Disable warning alerting rule for su_utilization detector"
+variable "sending_operations_disabled_warning" {
+  description = "Disable warning alerting rule for sending_operations detector"
   type        = bool
   default     = null
 }
 
-variable "su_utilization_notifications" {
-  description = "Notification recipients list for every alerting rules of su_utilization detector"
+variable "sending_operations_notifications" {
+  description = "Notification recipients list for every alerting rules of sending_operations detector"
   type        = list
   default     = []
 }
 
-variable "su_utilization_notifications_warning" {
-  description = "Notification recipients list for warning alerting rule of su_utilization detector"
+variable "sending_operations_notifications_warning" {
+  description = "Notification recipients list for warning alerting rule of sending_operations detector"
   type        = list
   default     = []
 }
 
-variable "su_utilization_notifications_critical" {
-  description = "Notification recipients list for critical alerting rule of su_utilization detector"
+variable "sending_operations_notifications_critical" {
+  description = "Notification recipients list for critical alerting rule of sending_operations detector"
   type        = list
   default     = []
 }
 
-variable "su_utilization_aggregation_function" {
-  description = "Aggregation function and group by for su_utilization detector (i.e. \".mean(by=['host'])\")"
+variable "sending_operations_aggregation_function" {
+  description = "Aggregation function and group by for sending_operations detector (i.e. \".mean(by=['host'])\")"
   type        = string
-  default     = ".mean(by=['azure_resource_name', 'azure_resource_group_name', 'azure_region'])"
+  default     = ""
 }
 
-variable "su_utilization_transformation_function" {
-  description = "Transformation function for su_utilization detector (mean, min, max)"
+variable "sending_operations_transformation_function" {
+  description = "Transformation function for sending_operations detector (mean, min, max)"
   type        = string
-  default     = "min"
+  default     = "sum"
 }
 
-variable "su_utilization_transformation_window" {
-  description = "Transformation window for su_utilization detector (i.e. 5m, 20m, 1h, 1d)"
+variable "sending_operations_transformation_window" {
+  description = "Transformation window for sending_operations detector (i.e. 5m, 20m, 1h, 1d)"
   type        = string
-  default     = "5m"
+  default     = "30m"
 }
 
-variable "su_utilization_threshold_critical" {
-  description = "Critical threshold for su_utilization detector"
-  type        = number
-  default     = 95
-}
-
-variable "su_utilization_threshold_warning" {
-  description = "Warning threshold for su_utilization detector"
-  type        = number
-  default     = 80
-}
-
-# Failed_requests detectors
-
-variable "failed_requests_disabled" {
-  description = "Disable all alerting rules for failed_requests detector"
-  type        = bool
-  default     = null
-}
-
-variable "failed_requests_disabled_critical" {
-  description = "Disable critical alerting rule for failed_requests detector"
-  type        = bool
-  default     = null
-}
-
-variable "failed_requests_disabled_warning" {
-  description = "Disable warning alerting rule for failed_requests detector"
-  type        = bool
-  default     = null
-}
-
-variable "failed_requests_notifications" {
-  description = "Notification recipients list for every alerting rules of failed_requests detector"
-  type        = list
-  default     = []
-}
-
-variable "failed_requests_notifications_warning" {
-  description = "Notification recipients list for warning alerting rule of failed_requests detector"
-  type        = list
-  default     = []
-}
-
-variable "failed_requests_notifications_critical" {
-  description = "Notification recipients list for critical alerting rule of failed_requests detector"
-  type        = list
-  default     = []
-}
-
-variable "failed_requests_aggregation_function" {
-  description = "Aggregation function and group by for failed_requests detector (i.e. \".mean(by=['host'])\")"
-  type        = string
-  default     = ".mean(by=['azure_resource_name', 'azure_resource_group_name', 'azure_region'])"
-}
-
-variable "failed_requests_transformation_function" {
-  description = "Transformation function for failed_requests detector (mean, min, max)"
-  type        = string
-  default     = "min"
-}
-
-variable "failed_requests_transformation_window" {
-  description = "Transformation window for failed_requests detector (i.e. 5m, 20m, 1h, 1d)"
-  type        = string
-  default     = "5m"
-}
-
-variable "failed_requests_threshold_critical" {
-  description = "Critical threshold for failed_requests detector"
-  type        = number
-  default     = 10
-}
-
-variable "failed_requests_threshold_warning" {
-  description = "Warning threshold for failed_requests detector"
+variable "sending_operations_threshold_critical" {
+  description = "Critical threshold for sending_operations detector"
   type        = number
   default     = 0
 }
 
-variable "failed_requests_aperiodic_duration" {
-  description = "Duration for the failed_requests block"
+variable "sending_operations_threshold_warning" {
+  description = "Warning threshold for sending_operations detector"
+  type        = number
+  default     = 1
+}
+
+variable "sending_operations_aperiodic_duration" {
+  description = "Duration for the sending_operations block"
   type        = string
   default     = "10m"
 }
 
-variable "failed_requests_aperiodic_percentage" {
-  description = "Percentage for the failed_requests block"
+variable "sending_operations_aperiodic_percentage" {
+  description = "Percentage for the sending_operations block"
   type        = number
   default     = 0.9
 }
 
-variable "failed_requests_clear_duration" {
-  description = "Duration for the failed_requests clear condition"
+variable "sending_operations_clear_duration" {
+  description = "Duration for the sending_operations clear condition"
   type        = string
   default     = "15m"
 }
 
-# Conversion_errors detectors
+# Unavailable_sending_operations detectors
 
-variable "conversion_errors_disabled" {
-  description = "Disable all alerting rules for conversion_errors detector"
+variable "unavailable_sending_operations_disabled" {
+  description = "Disable all alerting rules for unavailable_sending_operations detector"
   type        = bool
   default     = null
 }
 
-variable "conversion_errors_disabled_critical" {
-  description = "Disable critical alerting rule for conversion_errors detector"
+variable "unavailable_sending_operations_disabled_critical" {
+  description = "Disable critical alerting rule for unavailable_sending_operations detector"
   type        = bool
   default     = null
 }
 
-variable "conversion_errors_disabled_warning" {
-  description = "Disable warning alerting rule for conversion_errors detector"
+variable "unavailable_sending_operations_disabled_warning" {
+  description = "Disable warning alerting rule for unavailable_sending_operations detector"
   type        = bool
   default     = null
 }
 
-variable "conversion_errors_notifications" {
-  description = "Notification recipients list for every alerting rules of conversion_errors detector"
+variable "unavailable_sending_operations_notifications" {
+  description = "Notification recipients list for every alerting rules of unavailable_sending_operations detector"
   type        = list
   default     = []
 }
 
-variable "conversion_errors_notifications_warning" {
-  description = "Notification recipients list for warning alerting rule of conversion_errors detector"
+variable "unavailable_sending_operations_notifications_warning" {
+  description = "Notification recipients list for warning alerting rule of unavailable_sending_operations detector"
   type        = list
   default     = []
 }
 
-variable "conversion_errors_notifications_critical" {
-  description = "Notification recipients list for critical alerting rule of conversion_errors detector"
+variable "unavailable_sending_operations_notifications_critical" {
+  description = "Notification recipients list for critical alerting rule of unavailable_sending_operations detector"
   type        = list
   default     = []
 }
 
-variable "conversion_errors_aggregation_function" {
-  description = "Aggregation function and group by for conversion_errors detector (i.e. \".mean(by=['host'])\")"
+variable "unavailable_sending_operations_aggregation_function" {
+  description = "Aggregation function and group by for unavailable_sending_operations detector (i.e. \".mean(by=['host'])\")"
   type        = string
-  default     = ".mean(by=['azure_resource_name', 'azure_resource_group_name', 'azure_region'])"
+  default     = ""
 }
 
-variable "conversion_errors_transformation_function" {
-  description = "Transformation function for conversion_errors detector (mean, min, max)"
+variable "unavailable_sending_operations_transformation_function" {
+  description = "Transformation function for unavailable_sending_operations detector (mean, min, max)"
   type        = string
-  default     = "min"
+  default     = "sum"
 }
 
-variable "conversion_errors_transformation_window" {
-  description = "Transformation window for conversion_errors detector (i.e. 5m, 20m, 1h, 1d)"
+variable "unavailable_sending_operations_transformation_window" {
+  description = "Transformation window for unavailable_sending_operations detector (i.e. 5m, 20m, 1h, 1d)"
   type        = string
-  default     = "5m"
+  default     = "10m"
 }
 
-variable "conversion_errors_threshold_critical" {
-  description = "Critical threshold for conversion_errors detector"
+variable "unavailable_sending_operations_threshold_critical" {
+  description = "Critical threshold for unavailable_sending_operations detector"
+  type        = number
+  default     = 4
+}
+
+variable "unavailable_sending_operations_threshold_warning" {
+  description = "Warning threshold for unavailable_sending_operations detector"
+  type        = number
+  default     = 2
+}
+
+variable "unavailable_sending_operations_aperiodic_duration" {
+  description = "Duration for the unavailable_sending_operations block"
+  type        = string
+  default     = "10m"
+}
+
+variable "unavailable_sending_operations_aperiodic_percentage" {
+  description = "Percentage for the unavailable_sending_operations block"
+  type        = number
+  default     = 0.9
+}
+
+variable "unavailable_sending_operations_clear_duration" {
+  description = "Duration for the unavailable_sending_operations clear condition"
+  type        = string
+  default     = "15m"
+}
+
+# Unavailable_sending_operations_ratio detectors
+
+variable "unavailable_sending_operations_ratio_disabled" {
+  description = "Disable all alerting rules for unavailable_sending_operations_ratio detector"
+  type        = bool
+  default     = null
+}
+
+variable "unavailable_sending_operations_ratio_disabled_critical" {
+  description = "Disable critical alerting rule for unavailable_sending_operations_ratio detector"
+  type        = bool
+  default     = null
+}
+
+variable "unavailable_sending_operations_ratio_disabled_warning" {
+  description = "Disable warning alerting rule for unavailable_sending_operations_ratio detector"
+  type        = bool
+  default     = null
+}
+
+variable "unavailable_sending_operations_ratio_notifications" {
+  description = "Notification recipients list for every alerting rules of unavailable_sending_operations_ratio detector"
+  type        = list
+  default     = []
+}
+
+variable "unavailable_sending_operations_ratio_notifications_warning" {
+  description = "Notification recipients list for warning alerting rule of unavailable_sending_operations_ratio detector"
+  type        = list
+  default     = []
+}
+
+variable "unavailable_sending_operations_ratio_notifications_critical" {
+  description = "Notification recipients list for critical alerting rule of unavailable_sending_operations_ratio detector"
+  type        = list
+  default     = []
+}
+
+variable "unavailable_sending_operations_ratio_aggregation_function" {
+  description = "Aggregation function and group by for unavailable_sending_operations_ratio detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ""
+}
+
+variable "unavailable_sending_operations_ratio_transformation_function" {
+  description = "Transformation function for unavailable_sending_operations_ratio detector (mean, min, max)"
+  type        = string
+  default     = "sum"
+}
+
+variable "unavailable_sending_operations_ratio_transformation_window" {
+  description = "Transformation window for unavailable_sending_operations_ratio detector (i.e. 5m, 20m, 1h, 1d)"
+  type        = string
+  default     = "10m"
+}
+
+variable "unavailable_sending_operations_ratio_threshold_critical" {
+  description = "Critical threshold for unavailable_sending_operations_ratio detector"
+  type        = number
+  default     = 20
+}
+
+variable "unavailable_sending_operations_ratio_threshold_warning" {
+  description = "Warning threshold for unavailable_sending_operations_ratio detector"
   type        = number
   default     = 10
 }
 
-variable "conversion_errors_threshold_warning" {
-  description = "Warning threshold for conversion_errors detector"
-  type        = number
-  default     = 0
-}
-
-# Runtime_errors detectors
-
-variable "runtime_errors_disabled" {
-  description = "Disable all alerting rules for runtime_errors detector"
-  type        = bool
-  default     = null
-}
-
-variable "runtime_errors_disabled_critical" {
-  description = "Disable critical alerting rule for runtime_errors detector"
-  type        = bool
-  default     = null
-}
-
-variable "runtime_errors_disabled_warning" {
-  description = "Disable warning alerting rule for runtime_errors detector"
-  type        = bool
-  default     = null
-}
-
-variable "runtime_errors_notifications" {
-  description = "Notification recipients list for every alerting rules of runtime_errors detector"
-  type        = list
-  default     = []
-}
-
-variable "runtime_errors_notifications_warning" {
-  description = "Notification recipients list for warning alerting rule of runtime_errors detector"
-  type        = list
-  default     = []
-}
-
-variable "runtime_errors_notifications_critical" {
-  description = "Notification recipients list for critical alerting rule of runtime_errors detector"
-  type        = list
-  default     = []
-}
-
-variable "runtime_errors_aggregation_function" {
-  description = "Aggregation function and group by for runtime_errors detector (i.e. \".mean(by=['host'])\")"
+variable "unavailable_sending_operations_ratio_aperiodic_duration" {
+  description = "Duration for the unavailable_sending_operations_ratio block"
   type        = string
-  default     = ".mean(by=['azure_resource_name', 'azure_resource_group_name', 'azure_region'])"
+  default     = "10m"
 }
 
-variable "runtime_errors_transformation_function" {
-  description = "Transformation function for runtime_errors detector (mean, min, max)"
-  type        = string
-  default     = "min"
-}
-
-variable "runtime_errors_transformation_window" {
-  description = "Transformation window for runtime_errors detector (i.e. 5m, 20m, 1h, 1d)"
-  type        = string
-  default     = "5m"
-}
-
-variable "runtime_errors_threshold_critical" {
-  description = "Critical threshold for runtime_errors detector"
+variable "unavailable_sending_operations_ratio_aperiodic_percentage" {
+  description = "Percentage for the unavailable_sending_operations_ratio block"
   type        = number
-  default     = 10
+  default     = 0.9
 }
 
-variable "runtime_errors_threshold_warning" {
-  description = "Warning threshold for runtime_errors detector"
-  type        = number
-  default     = 0
+variable "unavailable_sending_operations_ratio_clear_duration" {
+  description = "Duration for the unavailable_sending_operations_ratio clear condition"
+  type        = string
+  default     = "15m"
 }
