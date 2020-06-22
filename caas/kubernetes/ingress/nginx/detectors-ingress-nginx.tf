fe0b6d3c4ea7107579af56b2661f172d8a32b51f
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-06-22 13:06:09 -0700

    Update detectors-ingress-nginx.tf

--- caas/kubernetes/ingress/nginx/detectors-ingress-nginx.tf
+++ caas/kubernetes/ingress/nginx/detectors-ingress-nginx.tf
@@ -2,13 +2,15 @@ resource "signalfx_detector" "nginx_ingress_too_many_5xx" {
   name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Nginx Ingress 5xx errors"
 
   program_text = <<-EOF
-		from signalfx.detectors.aperiodic import aperiodic
+		from signalfx.detectors.aperiodic import conditions
 		A = data('nginx_ingress_controller_requests', filter=filter('status', '5*') and ${module.filter-tags.filter_custom})${var.nginx_ingress_too_many_5xx_aggregation_function}
 		B = data('nginx_ingress_controller_requests', ${module.filter-tags.filter_custom})${var.nginx_ingress_too_many_5xx_aggregation_function}
 		signal = ((A/B)*100).${var.nginx_ingress_too_many_5xx_transformation_function}(over='${var.nginx_ingress_too_many_5xx_transformation_window}').publish('signal')
-		aperiodic.above_or_below_detector(signal, ${var.nginx_ingress_too_many_5xx_threshold_critical}, 'above', lasting('${var.nginx_ingress_too_many_5xx_aperiodic_duration}', ${var.nginx_ingress_too_many_5xx_aperiodic_percentage})).publish('CRIT')
-		aperiodic.range_detector(signal, ${var.nginx_ingress_too_many_5xx_threshold_warning}, ${var.nginx_ingress_too_many_5xx_threshold_critical}, 'within_range', lasting('${var.nginx_ingress_too_many_5xx_aperiodic_duration}', ${var.nginx_ingress_too_many_5xx_aperiodic_percentage}), upper_strict=False).publish('WARN')
-	EOF
+		ON_Condition_CRIT = conditions.generic_condition(signal, ${var.nginx_ingress_too_many_5xx_threshold_critical}, ${var.nginx_ingress_too_many_5xx_threshold_critical}, 'above', lasting('${var.nginx_ingress_too_many_5xx_aperiodic_duration}', ${var.nginx_ingress_too_many_5xx_aperiodic_percentage}), 'observed')
+		ON_Condition_WARN = conditions.generic_condition(signal, ${var.nginx_ingress_too_many_5xx_threshold_warning}, ${var.nginx_ingress_too_many_5xx_threshold_critical}, 'within_range', lasting('${var.nginx_ingress_too_many_5xx_aperiodic_duration}', ${var.nginx_ingress_too_many_5xx_aperiodic_percentage}), 'observed', strict_2=False)
+		detect(ON_Condition_CRIT, off=when(signal is None, '${var.nginx_ingress_too_many_5xx_clear_duration}')).publish('CRIT')
+		detect(ON_Condition_WARN, off=when(signal is None, '${var.nginx_ingress_too_many_5xx_clear_duration}')).publish('WARN')
+  EOF
 
   rule {
     description           = "are too high > ${var.nginx_ingress_too_many_5xx_threshold_critical}"
@@ -27,20 +29,21 @@ resource "signalfx_detector" "nginx_ingress_too_many_5xx" {
     notifications         = coalescelist(var.nginx_ingress_too_many_5xx_notifications_warning, var.nginx_ingress_too_many_5xx_notifications, var.notifications)
     parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{readableRule}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
   }
-
 }
 
 resource "signalfx_detector" "nginx_ingress_too_many_4xx" {
   name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Nginx Ingress 4xx errors"
 
   program_text = <<-EOF
-		from signalfx.detectors.aperiodic import aperiodic
+		from signalfx.detectors.aperiodic import conditions
 		A = data('nginx_ingress_controller_requests', filter=filter('status', '4*') and ${module.filter-tags.filter_custom})${var.nginx_ingress_too_many_4xx_aggregation_function}
 		B = data('nginx_ingress_controller_requests', ${module.filter-tags.filter_custom})${var.nginx_ingress_too_many_4xx_aggregation_function}
 		signal = ((A/B)*100).${var.nginx_ingress_too_many_4xx_transformation_function}(over='${var.nginx_ingress_too_many_4xx_transformation_window}').publish('signal')
-		aperiodic.above_or_below_detector(signal, ${var.nginx_ingress_too_many_4xx_threshold_critical}, 'above', lasting('${var.nginx_ingress_too_many_4xx_aperiodic_duration}', ${var.nginx_ingress_too_many_4xx_aperiodic_percentage})).publish('CRIT')
-		aperiodic.range_detector(signal, ${var.nginx_ingress_too_many_4xx_threshold_warning}, ${var.nginx_ingress_too_many_4xx_threshold_critical}, 'within_range', lasting('${var.nginx_ingress_too_many_4xx_aperiodic_duration}', ${var.nginx_ingress_too_many_4xx_aperiodic_percentage}), upper_strict=False).publish('WARN')
-	EOF
+		ON_Condition_CRIT = conditions.generic_condition(signal, ${var.nginx_ingress_too_many_4xx_threshold_critical}, ${var.nginx_ingress_too_many_4xx_threshold_critical}, 'above', lasting('${var.nginx_ingress_too_many_4xx_aperiodic_duration}', ${var.nginx_ingress_too_many_4xx_aperiodic_percentage}), 'observed')
+		ON_Condition_WARN = conditions.generic_condition(signal, ${var.nginx_ingress_too_many_4xx_threshold_warning}, ${var.nginx_ingress_too_many_4xx_threshold_critical}, 'within_range', lasting('${var.nginx_ingress_too_many_4xx_aperiodic_duration}', ${var.nginx_ingress_too_many_4xx_aperiodic_percentage}), 'observed', strict_2=False)
+		detect(ON_Condition_CRIT, off=when(signal is None, '${var.nginx_ingress_too_many_4xx_clear_duration}')).publish('CRIT')
+		detect(ON_Condition_WARN, off=when(signal is None, '${var.nginx_ingress_too_many_4xx_clear_duration}')).publish('WARN')
+  EOF
 
   rule {
     description           = "are too high > ${var.nginx_ingress_too_many_4xx_threshold_critical}"
@@ -59,5 +62,4 @@ resource "signalfx_detector" "nginx_ingress_too_many_4xx" {
     notifications         = coalescelist(var.nginx_ingress_too_many_4xx_notifications_warning, var.nginx_ingress_too_many_4xx_notifications, var.notifications)
     parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{readableRule}}} ({{inputs.signal.value}}) on {{{dimensions}}}"
   }
-
 }
