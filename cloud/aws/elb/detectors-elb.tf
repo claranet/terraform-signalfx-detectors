9333accf82ee827f7e7e318893b31bf8a69bbf28
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-07-06 07:29:29 -0700

    Update detectors-elb.tf

--- cloud/aws/elb/detectors-elb.tf
+++ cloud/aws/elb/detectors-elb.tf
@@ -5,7 +5,7 @@ resource "signalfx_detector" "heartbeat" {
 		from signalfx.detectors.not_reporting import not_reporting
 		signal = data('RequestCount', filter=filter('stat', 'sum') and filter('namespace', 'AWS/ELB')and (not filter('AvailabilityZone', '*')) and (not filter('aws_state', '{Code: 32,Name: shutting-down', '{Code: 48,Name: terminated}', '{Code: 62,Name: stopping}', '{Code: 80,Name: stopped}')) and ${module.filter-tags.filter_custom}).publish('signal')
 		not_reporting.detector(stream=signal, resource_identifier=['LoadBalancerName'], duration='${var.heartbeat_timeframe}').publish('CRIT')
-  EOF
+	EOF
 
   rule {
     description           = "has not reported in ${var.heartbeat_timeframe}"
@@ -26,7 +26,7 @@ resource "signalfx_detector" "no_healthy_instances" {
 		signal = (A/ (A + B)).scale(100).${var.no_healthy_instances_transformation_function}(over='${var.no_healthy_instances_transformation_window}').publish('signal')
 		detect(when(signal < ${var.no_healthy_instances_threshold_critical})).publish('CRIT')
 		detect(when(signal < ${var.no_healthy_instances_threshold_warning}) and when(signal >= ${var.no_healthy_instances_threshold_critical})).publish('WARN')
-  EOF
+	EOF
 
   rule {
     description           = "has fallen below critical capacity < ${var.no_healthy_instances_threshold_critical}%"
@@ -56,7 +56,7 @@ resource "signalfx_detector" "too_much_4xx" {
 		signal = (A/B).scale(100).${var.too_much_4xx_transformation_function}(over='${var.too_much_4xx_transformation_window}').publish('signal')
 		detect(when(signal > ${var.too_much_4xx_threshold_critical}) and when(B > ${var.too_much_4xx_threshold_number_requests})).publish('CRIT')
 		detect(when(signal > ${var.too_much_4xx_threshold_warning}) and when(B > ${var.too_much_4xx_threshold_number_requests}) and when(signal <= ${var.too_much_4xx_threshold_critical})).publish('WARN')
-  EOF
+	EOF
 
   rule {
     description           = "is too high > ${var.too_much_4xx_threshold_critical}%"
@@ -86,7 +86,7 @@ resource "signalfx_detector" "too_much_5xx" {
 		signal = (A/B).scale(100).${var.too_much_5xx_transformation_function}(over='${var.too_much_5xx_transformation_window}').publish('signal')
 		detect(when(signal > ${var.too_much_5xx_threshold_critical}) and when(B > ${var.too_much_5xx_threshold_number_requests})).publish('CRIT')
 		detect(when(signal > ${var.too_much_5xx_threshold_warning}) and when(B > ${var.too_much_5xx_threshold_number_requests}) and when(signal <= ${var.too_much_5xx_threshold_critical})).publish('WARN')
-  EOF
+	EOF
 
   rule {
     description           = "is too high > ${var.too_much_5xx_threshold_critical}%"
@@ -116,7 +116,7 @@ resource "signalfx_detector" "too_much_4xx_backend" {
 		signal = (A/B).scale(100).${var.too_much_4xx_backend_transformation_function}(over='${var.too_much_4xx_backend_transformation_window}').publish('signal')
 		detect(when(signal > ${var.too_much_4xx_backend_threshold_critical}) and when(B > ${var.too_much_4xx_backend_threshold_number_requests})).publish('CRIT')
 		detect(when(signal > ${var.too_much_4xx_backend_threshold_warning}) and when(B > ${var.too_much_4xx_backend_threshold_number_requests}) and when(signal <= ${var.too_much_4xx_backend_threshold_critical})).publish('WARN')
-  EOF
+	EOF
 
   rule {
     description           = "is too high > ${var.too_much_4xx_backend_threshold_critical}%"
@@ -146,7 +146,7 @@ resource "signalfx_detector" "too_much_5xx_backend" {
 		signal = (A/B).scale(100).${var.too_much_5xx_backend_transformation_function}(over='${var.too_much_5xx_backend_transformation_window}').publish('signal')
 		detect(when(signal > ${var.too_much_5xx_backend_threshold_critical}) and when(B > ${var.too_much_5xx_backend_threshold_number_requests})).publish('CRIT')
 		detect(when(signal > ${var.too_much_5xx_backend_threshold_warning}) and when(B > ${var.too_much_5xx_backend_threshold_number_requests}) and when(signal <= ${var.too_much_5xx_backend_threshold_critical})).publish('WARN')
-  EOF
+	EOF
 
   rule {
     description           = "is too high > ${var.too_much_5xx_backend_threshold_critical}%"
@@ -177,7 +177,7 @@ resource "signalfx_detector" "backend_latency" {
 		ON_Condition_WARN = conditions.generic_condition(signal, ${var.backend_latency_threshold_warning}, ${var.backend_latency_threshold_critical}, 'within_range', lasting('${var.backend_latency_aperiodic_duration}', ${var.backend_latency_aperiodic_percentage}), 'observed', strict_2=False)
 		detect(ON_Condition_CRIT, off=when(signal is None, '${var.backend_latency_clear_duration}')).publish('CRIT')
 		detect(ON_Condition_WARN, off=when(signal is None, '${var.backend_latency_clear_duration}')).publish('WARN')
-  EOF
+	EOF
 
   rule {
     description           = "is too high > ${var.backend_latency_threshold_critical}s"
