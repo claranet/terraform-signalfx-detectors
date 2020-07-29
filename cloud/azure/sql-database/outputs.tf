d05f52ef9fd2457e066ea63229ea9f58cf31d9fa
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2020-07-29 10:15:53 +0200

    Cloud Azure: SQL database detectors

---
+++ cloud/azure/sql-database/outputs.tf
@@ -0,0 +1,24 @@
+output "heartbeat_id" {
+  description = "id for detector heartbeat"
+  value       = signalfx_detector.heartbeat.*.id
+}
+
+output "cpu_id" {
+  description = "id for detector cpu"
+  value       = signalfx_detector.cpu.*.id
+}
+
+output "free_space_id" {
+  description = "id for detector free_space"
+  value       = signalfx_detector.free_space.*.id
+}
+
+output "dtu_consumption_id" {
+  description = "id for detector dtu_consumption"
+  value       = signalfx_detector.dtu_consumption.*.id
+}
+
+output "deadlocks_count_id" {
+  description = "id for detector deadlocks_count"
+  value       = signalfx_detector.deadlocks_count.*.id
+}
