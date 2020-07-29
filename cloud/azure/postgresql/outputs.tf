81c0b73ec643860d08c15b72b037a64152a0189f
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2020-07-29 11:50:32 +0200

    Cloud Azure: PostgreSQL detectors

---
+++ cloud/azure/postgresql/outputs.tf
@@ -0,0 +1,29 @@
+output "heartbeat_id" {
+  description = "id for detector heartbeat"
+  value       = signalfx_detector.heartbeat.*.id
+}
+
+output "cpu_usage_id" {
+  description = "id for detector cpu_usage"
+  value       = signalfx_detector.cpu_usage.*.id
+}
+
+output "no_connection_id" {
+  description = "id for detector no_connection"
+  value       = signalfx_detector.no_connection.*.id
+}
+
+output "free_storage_id" {
+  description = "id for detector free_storage"
+  value       = signalfx_detector.free_storage.*.id
+}
+
+output "io_consumption_id" {
+  description = "id for detector io_consumption"
+  value       = signalfx_detector.io_consumption.*.id
+}
+
+output "memory_usage_id" {
+  description = "id for detector memory_usage"
+  value       = signalfx_detector.memory_usage.*.id
+}
