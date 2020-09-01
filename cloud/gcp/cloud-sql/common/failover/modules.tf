1dccb53a9b39afe63e7a17a4de60e4bcdf099d38
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2020-09-01 12:31:00 +0200

    use project_id instead of gcp_project_name which could change

--- cloud/gcp/cloud-sql/common/failover/modules.tf
+++ cloud/gcp/cloud-sql/common/failover/modules.tf
@@ -1,7 +1,7 @@
 module "filter-tags" {
   source = "github.com/claranet/terraform-signalfx-detectors.git//common/filter-tags"
 
-  filter_defaults        = "filter('gcp_project_name', '${var.gcp_project_name}') and (not filter('database_id', '*-replica*'))"
+  filter_defaults        = "filter('project_id', '${var.gcp_project_id}') and (not filter('database_id', '*-replica*'))"
   filter_custom_includes = var.filter_custom_includes
   filter_custom_excludes = var.filter_custom_excludes
 }
