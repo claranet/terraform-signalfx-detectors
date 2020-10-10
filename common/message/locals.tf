ef7f367656cbc1a959fe483958b654cba5148d65
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2020-10-10 12:22:52 +0200

    #114 heartbeat filters as local

--- common/message/locals.tf
+++ common/message/locals.tf
@@ -1,4 +1,13 @@
 locals {
+  heartbeat_filters_gcp = "(not filter('gcp_status', '{Code=3, Name=STOPPING}', '{Code=4, Name=TERMINATED}'))"
+  heartbeat_filters_aws = "(not filter('aws_state', '{Code: 32,Name: shutting-down', '{Code: 48,Name: terminated}', '{Code: 62,Name: stopping}', '{Code: 80,Name: stopped}'))"
+  heartbeat_filters_azure = "(not filter('azure_power_state', 'PowerState/stopping', 'PowerState/stopped', 'PowerState/deallocating', 'PowerState/deallocated'))"
+  heartbeat_filters = format(
+    "%s and %s and %s",
+    local.heartbeat_filters_aws,
+    local.heartbeat_filters_gcp,
+    local.heartbeat_filters_azure
+  )
   name_start      = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}]"
   subject_start   = "[{{ruleSeverity}}]{{{detectorName}}} {{{readableRule}}}"
   subject_end     = "on {{{dimensions}}}"
