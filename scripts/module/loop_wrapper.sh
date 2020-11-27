532ad624440f340576a28798bbef9763525603e1
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2020-11-27 23:19:41 +0100

    #99 add wrapper to run scripts over all modules

---
+++ scripts/module/loop_wrapper.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -ue -o pipefail
+
+if [ $# -eq 0 ]; then
+    echo "Needs to provide a script to run for each module as parameter"
+    exit 1
+fi
+
+if ! [ -f $1 ]; then
+    echo "The provided script as parameter does not exist"
+    exit 2
+fi
+
+MODULES=modules
+SCRIPT=$1
+shift
+
+for module in ${MODULES}/*; do
+    echo "Run script $SCRIPT for module $module"
+    $SCRIPT ${module%"/"} $@
+done
