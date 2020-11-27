9baf4be7272753fb306eb83c6a407d7e9aa1d846
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2020-11-27 23:48:05 +0100

    #99 improve lint to ignore common vars

--- scripts/module/lint.sh
+++ scripts/module/lint.sh
@@ -12,6 +12,15 @@ for tflocal in $(grep '^[[:space:]]*[a-z0-9_]*[[:space:]]*=' common-locals.tf | 
 
 EOF
 done
+# Ignore common variables for "terraform_unused_declarations"
+for tfvar in $(grep -E '^variable[[:space:]]+"' common-variables.tf | cut -d '"' -f2); do
+    cat <<-EOF >> tmp-outputs.tf
+    output "${tfvar}" {
+      value = var.${tfvar}
+    }
+
+EOF
+done
 
 tflint --disable-rule=terraform_module_pinned_source --enable-rule=terraform_unused_declarations --loglevel=info
 rm tmp-outputs.tf
