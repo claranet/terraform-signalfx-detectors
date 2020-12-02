fb33041934b8299e7cff36615e60c469efb58c21
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2020-12-02 12:51:43 +0100

    use numeroted naming for detectors preset configs

--- modules/smart-agent_system-common/variables-gen.tf
+++ modules/smart-agent_system-common/variables-gen.tf
@@ -1,3 +1,29 @@
+# heartbeat detector
+
+variable "heartbeat_notifications" {
+  description = "Notification recipients list per severity overridden for heartbeat detector"
+  type        = map(list(string))
+  default     = {}
+}
+
+variable "heartbeat_aggregation_function" {
+  description = "Aggregation function and group by for heartbeat detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ""
+}
+
+variable "heartbeat_disabled" {
+  description = "Disable all alerting rules for heartbeat detector"
+  type        = bool
+  default     = null
+}
+
+variable "heartbeat_timeframe" {
+  description = "Timeframe for heartbeat detector (i.e. \"10m\")"
+  type        = string
+  default     = "20m"
+}
+
 # cpu detector
 
 variable "cpu_notifications" {
@@ -46,82 +72,6 @@ variable "cpu_threshold_major" {
   description = "Major threshold for cpu detector"
   type        = number
   default     = 85
-}
-
-# heartbeat detector
-
-variable "heartbeat_notifications" {
-  description = "Notification recipients list per severity overridden for heartbeat detector"
-  type        = map(list(string))
-  default     = {}
-}
-
-variable "heartbeat_aggregation_function" {
-  description = "Aggregation function and group by for heartbeat detector (i.e. \".mean(by=['host'])\")"
-  type        = string
-  default     = ""
-}
-
-variable "heartbeat_disabled" {
-  description = "Disable all alerting rules for heartbeat detector"
-  type        = bool
-  default     = null
-}
-
-variable "heartbeat_timeframe" {
-  description = "Timeframe for heartbeat detector (i.e. \"10m\")"
-  type        = string
-  default     = "20m"
-}
-
-# disk_inodes detector
-
-variable "disk_inodes_notifications" {
-  description = "Notification recipients list per severity overridden for disk_inodes detector"
-  type        = map(list(string))
-  default     = {}
-}
-
-variable "disk_inodes_aggregation_function" {
-  description = "Aggregation function and group by for disk_inodes detector (i.e. \".mean(by=['host'])\")"
-  type        = string
-  default     = ""
-}
-
-variable "disk_inodes_transformation_function" {
-  description = "Transformation function for disk_inodes detector (i.e. \".mean(over='5m')\")"
-  type        = string
-  default     = ".max(over='5m')"
-}
-
-variable "disk_inodes_disabled" {
-  description = "Disable all alerting rules for disk_inodes detector"
-  type        = bool
-  default     = null
-}
-
-variable "disk_inodes_disabled_critical" {
-  description = "Disable critical alerting rule for disk_inodes detector"
-  type        = bool
-  default     = null
-}
-
-variable "disk_inodes_disabled_major" {
-  description = "Disable major alerting rule for disk_inodes detector"
-  type        = bool
-  default     = null
-}
-
-variable "disk_inodes_threshold_critical" {
-  description = "Critical threshold for disk_inodes detector"
-  type        = number
-  default     = 95
-}
-
-variable "disk_inodes_threshold_major" {
-  description = "Major threshold for disk_inodes detector"
-  type        = number
-  default     = 90
 }
 
 # load detector
@@ -174,56 +124,6 @@ variable "load_threshold_major" {
   default     = 2
 }
 
-# memory detector
-
-variable "memory_notifications" {
-  description = "Notification recipients list per severity overridden for memory detector"
-  type        = map(list(string))
-  default     = {}
-}
-
-variable "memory_aggregation_function" {
-  description = "Aggregation function and group by for memory detector (i.e. \".mean(by=['host'])\")"
-  type        = string
-  default     = ""
-}
-
-variable "memory_transformation_function" {
-  description = "Transformation function for memory detector (i.e. \".mean(over='5m')\")"
-  type        = string
-  default     = ".min(over='5m')"
-}
-
-variable "memory_disabled" {
-  description = "Disable all alerting rules for memory detector"
-  type        = bool
-  default     = null
-}
-
-variable "memory_disabled_critical" {
-  description = "Disable critical alerting rule for memory detector"
-  type        = bool
-  default     = null
-}
-
-variable "memory_disabled_major" {
-  description = "Disable major alerting rule for memory detector"
-  type        = bool
-  default     = null
-}
-
-variable "memory_threshold_critical" {
-  description = "Critical threshold for memory detector"
-  type        = number
-  default     = 95
-}
-
-variable "memory_threshold_major" {
-  description = "Major threshold for memory detector"
-  type        = number
-  default     = 90
-}
-
 # disk_space detector
 
 variable "disk_space_notifications" {
@@ -272,5 +172,105 @@ variable "disk_space_threshold_major" {
   description = "Major threshold for disk_space detector"
   type        = number
   default     = 80
+}
+
+# disk_inodes detector
+
+variable "disk_inodes_notifications" {
+  description = "Notification recipients list per severity overridden for disk_inodes detector"
+  type        = map(list(string))
+  default     = {}
+}
+
+variable "disk_inodes_aggregation_function" {
+  description = "Aggregation function and group by for disk_inodes detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ""
+}
+
+variable "disk_inodes_transformation_function" {
+  description = "Transformation function for disk_inodes detector (i.e. \".mean(over='5m')\")"
+  type        = string
+  default     = ".max(over='5m')"
+}
+
+variable "disk_inodes_disabled" {
+  description = "Disable all alerting rules for disk_inodes detector"
+  type        = bool
+  default     = null
+}
+
+variable "disk_inodes_disabled_critical" {
+  description = "Disable critical alerting rule for disk_inodes detector"
+  type        = bool
+  default     = null
+}
+
+variable "disk_inodes_disabled_major" {
+  description = "Disable major alerting rule for disk_inodes detector"
+  type        = bool
+  default     = null
+}
+
+variable "disk_inodes_threshold_critical" {
+  description = "Critical threshold for disk_inodes detector"
+  type        = number
+  default     = 95
+}
+
+variable "disk_inodes_threshold_major" {
+  description = "Major threshold for disk_inodes detector"
+  type        = number
+  default     = 90
+}
+
+# memory detector
+
+variable "memory_notifications" {
+  description = "Notification recipients list per severity overridden for memory detector"
+  type        = map(list(string))
+  default     = {}
+}
+
+variable "memory_aggregation_function" {
+  description = "Aggregation function and group by for memory detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ""
+}
+
+variable "memory_transformation_function" {
+  description = "Transformation function for memory detector (i.e. \".mean(over='5m')\")"
+  type        = string
+  default     = ".min(over='5m')"
+}
+
+variable "memory_disabled" {
+  description = "Disable all alerting rules for memory detector"
+  type        = bool
+  default     = null
+}
+
+variable "memory_disabled_critical" {
+  description = "Disable critical alerting rule for memory detector"
+  type        = bool
+  default     = null
+}
+
+variable "memory_disabled_major" {
+  description = "Disable major alerting rule for memory detector"
+  type        = bool
+  default     = null
+}
+
+variable "memory_threshold_critical" {
+  description = "Critical threshold for memory detector"
+  type        = number
+  default     = 95
+}
+
+variable "memory_threshold_major" {
+  description = "Major threshold for memory detector"
+  type        = number
+  default     = 90
 }
 
--- scripts/module/gen_detectors.sh
+++ scripts/module/gen_detectors.sh
@@ -5,13 +5,13 @@ TARGET="${1:-}"
 MODULE=${TARGET#"modules/"}
 set -x
 
-if ! compgen -G ${TARGET}/conf/detector-*.yaml > /dev/null; then
+if ! compgen -G ${TARGET}/conf/[0-9][0-9]-*.yaml > /dev/null; then
     exit
 fi
 for dst in variables detectors; do
     > ${TARGET}/${dst}-gen.tf
 done
-for detector in ${TARGET}/conf/detector-*.yaml; do
+for detector in ${TARGET}/conf/[0-9][0-9]-*.yaml; do
     for dst in variables detectors; do
         j2 ./scripts/templates/$(echo ${dst} | sed 's/detectors/detector/').tf.j2 ${detector} >> ${TARGET}/${dst}-gen.tf
     done
