9d94b1d48fc7f61f745195d2532e1f49166fd5e5
Nicolas VION <nicolas.vion@macbook-pro-de-nvion.local>
2021-01-18 15:56:16 +0100

    add datafactory module
    - adding datafactory v1.0
    - adding rg and region filter, and some docs
    - updating detectors for datafactory part

---
+++ modules/integration_azure-datafactory/variables-gen.tf
@@ -0,0 +1,310 @@
+# activity_error_rate detector
+
+variable "activity_error_rate_notifications" {
+  description = "Notification recipients list per severity overridden for activity_error_rate detector"
+  type        = map(list(string))
+  default     = {}
+}
+
+variable "activity_error_rate_aggregation_function" {
+  description = "Aggregation function and group by for activity_error_rate detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ".sum(by=['name', 'azure_resource_group_name', 'azure_region'])"
+}
+
+variable "activity_error_rate_transformation_function" {
+  description = "Transformation function for activity_error_rate detector (i.e. \".mean(over='5m')\")"
+  type        = string
+  default     = ".min(over='5m')"
+}
+
+variable "activity_error_rate_tip" {
+  description = "Suggested first course of action or any note useful for incident handling"
+  type        = string
+  default     = ""
+}
+
+variable "activity_error_rate_runbook_url" {
+  description = "URL like SignalFx dashboard or wiki page which can help to troubleshoot the incident cause"
+  type        = string
+  default     = ""
+}
+
+variable "activity_error_rate_disabled" {
+  description = "Disable all alerting rules for activity_error_rate detector"
+  type        = bool
+  default     = null
+}
+
+variable "activity_error_rate_disabled_critical" {
+  description = "Disable critical alerting rule for activity_error_rate detector"
+  type        = bool
+  default     = null
+}
+
+variable "activity_error_rate_disabled_major" {
+  description = "Disable major alerting rule for activity_error_rate detector"
+  type        = bool
+  default     = null
+}
+
+variable "activity_error_rate_threshold_critical" {
+  description = "Critical threshold for activity_error_rate detector in %"
+  type        = number
+  default     = 20
+}
+
+variable "activity_error_rate_threshold_major" {
+  description = "Major threshold for activity_error_rate detector in %"
+  type        = number
+  default     = 10
+}
+
+# pipeline_error_rate detector
+
+variable "pipeline_error_rate_notifications" {
+  description = "Notification recipients list per severity overridden for pipeline_error_rate detector"
+  type        = map(list(string))
+  default     = {}
+}
+
+variable "pipeline_error_rate_aggregation_function" {
+  description = "Aggregation function and group by for pipeline_error_rate detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ".sum(by=['name', 'azure_resource_group_name', 'azure_region'])"
+}
+
+variable "pipeline_error_rate_transformation_function" {
+  description = "Transformation function for pipeline_error_rate detector (i.e. \".mean(over='5m')\")"
+  type        = string
+  default     = ".min(over='5m')"
+}
+
+variable "pipeline_error_rate_tip" {
+  description = "Suggested first course of action or any note useful for incident handling"
+  type        = string
+  default     = ""
+}
+
+variable "pipeline_error_rate_runbook_url" {
+  description = "URL like SignalFx dashboard or wiki page which can help to troubleshoot the incident cause"
+  type        = string
+  default     = ""
+}
+
+variable "pipeline_error_rate_disabled" {
+  description = "Disable all alerting rules for pipeline_error_rate detector"
+  type        = bool
+  default     = null
+}
+
+variable "pipeline_error_rate_disabled_critical" {
+  description = "Disable critical alerting rule for pipeline_error_rate detector"
+  type        = bool
+  default     = null
+}
+
+variable "pipeline_error_rate_disabled_major" {
+  description = "Disable major alerting rule for pipeline_error_rate detector"
+  type        = bool
+  default     = null
+}
+
+variable "pipeline_error_rate_threshold_critical" {
+  description = "Critical threshold for pipeline_error_rate detector in %"
+  type        = number
+  default     = 20
+}
+
+variable "pipeline_error_rate_threshold_major" {
+  description = "Major threshold for pipeline_error_rate detector in %"
+  type        = number
+  default     = 10
+}
+
+# trigger_error_rate detector
+
+variable "trigger_error_rate_notifications" {
+  description = "Notification recipients list per severity overridden for trigger_error_rate detector"
+  type        = map(list(string))
+  default     = {}
+}
+
+variable "trigger_error_rate_aggregation_function" {
+  description = "Aggregation function and group by for trigger_error_rate detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ".sum(by=['name', 'azure_resource_group_name', 'azure_region'])"
+}
+
+variable "trigger_error_rate_transformation_function" {
+  description = "Transformation function for trigger_error_rate detector (i.e. \".mean(over='5m')\")"
+  type        = string
+  default     = ".min(over='5m')"
+}
+
+variable "trigger_error_rate_tip" {
+  description = "Suggested first course of action or any note useful for incident handling"
+  type        = string
+  default     = ""
+}
+
+variable "trigger_error_rate_runbook_url" {
+  description = "URL like SignalFx dashboard or wiki page which can help to troubleshoot the incident cause"
+  type        = string
+  default     = ""
+}
+
+variable "trigger_error_rate_disabled" {
+  description = "Disable all alerting rules for trigger_error_rate detector"
+  type        = bool
+  default     = null
+}
+
+variable "trigger_error_rate_disabled_critical" {
+  description = "Disable critical alerting rule for trigger_error_rate detector"
+  type        = bool
+  default     = null
+}
+
+variable "trigger_error_rate_disabled_major" {
+  description = "Disable major alerting rule for trigger_error_rate detector"
+  type        = bool
+  default     = null
+}
+
+variable "trigger_error_rate_threshold_critical" {
+  description = "Critical threshold for trigger_error_rate detector"
+  type        = number
+  default     = 20
+}
+
+variable "trigger_error_rate_threshold_major" {
+  description = "Major threshold for trigger_error_rate detector"
+  type        = number
+  default     = 10
+}
+
+# available_memory detector
+
+variable "available_memory_notifications" {
+  description = "Notification recipients list per severity overridden for available_memory detector"
+  type        = map(list(string))
+  default     = {}
+}
+
+variable "available_memory_aggregation_function" {
+  description = "Aggregation function and group by for available_memory detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ".sum(by=['azure_resource_name', 'azure_resource_group_name', 'azure_region'])"
+}
+
+variable "available_memory_transformation_function" {
+  description = "Transformation function for available_memory detector (i.e. \".mean(over='5m')\")"
+  type        = string
+  default     = ".min(over='15m')"
+}
+
+variable "available_memory_tip" {
+  description = "Suggested first course of action or any note useful for incident handling"
+  type        = string
+  default     = ""
+}
+
+variable "available_memory_runbook_url" {
+  description = "URL like SignalFx dashboard or wiki page which can help to troubleshoot the incident cause"
+  type        = string
+  default     = ""
+}
+
+variable "available_memory_disabled" {
+  description = "Disable all alerting rules for available_memory detector"
+  type        = bool
+  default     = null
+}
+
+variable "available_memory_disabled_critical" {
+  description = "Disable critical alerting rule for available_memory detector"
+  type        = bool
+  default     = null
+}
+
+variable "available_memory_disabled_major" {
+  description = "Disable major alerting rule for available_memory detector"
+  type        = bool
+  default     = null
+}
+
+variable "available_memory_threshold_critical" {
+  description = "Critical threshold for available_memory detector in MB"
+  type        = number
+  default     = 256
+}
+
+variable "available_memory_threshold_major" {
+  description = "Major threshold for available_memory detector in MB"
+  type        = number
+  default     = 512
+}
+
+# cpu_percentage detector
+
+variable "cpu_percentage_notifications" {
+  description = "Notification recipients list per severity overridden for cpu_percentage detector"
+  type        = map(list(string))
+  default     = {}
+}
+
+variable "cpu_percentage_aggregation_function" {
+  description = "Aggregation function and group by for cpu_percentage detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ".sum(by=['azure_resource_name', 'azure_resource_group_name', 'azure_region'])"
+}
+
+variable "cpu_percentage_transformation_function" {
+  description = "Transformation function for cpu_percentage detector (i.e. \".mean(over='5m')\")"
+  type        = string
+  default     = ".min(over='15m')"
+}
+
+variable "cpu_percentage_tip" {
+  description = "Suggested first course of action or any note useful for incident handling"
+  type        = string
+  default     = ""
+}
+
+variable "cpu_percentage_runbook_url" {
+  description = "URL like SignalFx dashboard or wiki page which can help to troubleshoot the incident cause"
+  type        = string
+  default     = ""
+}
+
+variable "cpu_percentage_disabled" {
+  description = "Disable all alerting rules for cpu_percentage detector"
+  type        = bool
+  default     = null
+}
+
+variable "cpu_percentage_disabled_critical" {
+  description = "Disable critical alerting rule for cpu_percentage detector"
+  type        = bool
+  default     = null
+}
+
+variable "cpu_percentage_disabled_major" {
+  description = "Disable major alerting rule for cpu_percentage detector"
+  type        = bool
+  default     = null
+}
+
+variable "cpu_percentage_threshold_critical" {
+  description = "Critical threshold for cpu_percentage detector in %"
+  type        = number
+  default     = 90
+}
+
+variable "cpu_percentage_threshold_major" {
+  description = "Major threshold for cpu_percentage detector in %"
+  type        = number
+  default     = 80
+}
+
