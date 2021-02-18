6e3ce3ea889cc9517b22a37e1357142ab1a180c3
Jérémy MARMOL <jeremy.marmol@fr.clara.net>
2021-02-18 14:43:37 +0100

    #247 Add lasting option + force fmt in generator

--- scripts/module/gen_detectors.sh
+++ scripts/module/gen_detectors.sh
@@ -14,6 +14,6 @@ done
 for detector in ${TARGET}/conf/[0-9][0-9]-*.yaml; do
     for dst in variables detectors; do
         j2 ./scripts/templates/$(echo ${dst} | sed 's/detectors/detector/').tf.j2 ${detector} >> ${TARGET}/${dst}-gen.tf
+        terraform fmt ${TARGET}/${dst}-gen.tf
     done
 done
-
