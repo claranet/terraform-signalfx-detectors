0ba18fc3b454049de9cea444f1f626cf27a43d10
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2021-06-07 18:06:42 +0200

    rename local to filters to avoid duplication for lint check

--- common/module/locals.tf
+++ common/module/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  filter_defaults              = "filter('env', '${var.environment}') and filter('sfx_monitored', 'true')"
+  filters                      = "filter('env', '${var.environment}') and filter('sfx_monitored', 'true')"
   not_running_vm_filters_gcp   = "(not filter('gcp_status', '{Code=3, Name=STOPPING}', '{Code=4, Name=TERMINATED}'))"
   not_running_vm_filters_aws   = "(not filter('aws_state', '{Code: 32,Name: shutting-down}', '{Code: 48,Name: terminated}', '{Code: 62,Name: stopping}', '{Code: 80,Name: stopped}'))"
   not_running_vm_filters_azure = "(not filter('azure_power_state', 'PowerState/stopping', 'PowerState/stopped', 'PowerState/deallocating', 'PowerState/deallocated'))"
--- common/module/modules.tf
+++ common/module/modules.tf
@@ -1,7 +1,7 @@
 module "filter-tags" {
   source = "github.com/claranet/terraform-signalfx-detectors.git//common/filter-tags"
 
-  filter_defaults        = coalesce(var.filter_defaults, local.filter_defaults)
+  filter_defaults        = coalesce(var.filter_defaults, local.filters)
   filter_custom_includes = var.filter_custom_includes
   filter_custom_excludes = var.filter_custom_excludes
 }
