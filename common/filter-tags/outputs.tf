96ad6b1540bea457fe5bc26b1b00dc942b5bf9eb
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2021-06-10 16:55:13 +0200

    terraform fmt

--- common/filter-tags/outputs.tf
+++ common/filter-tags/outputs.tf
@@ -1,5 +1,5 @@
 output "filter_custom" {
   description = "The full filtering pattern to use in detectors"
-  value = var.filter_custom == "" ? var.filter_defaults : (var.append_mode ? format("(%s) and (%s)", var.filter_defaults, var.filter_custom) : var.filter_custom)
+  value       = var.filter_custom == "" ? var.filter_defaults : (var.append_mode ? format("(%s) and (%s)", var.filter_defaults, var.filter_custom) : var.filter_custom)
 }
 
