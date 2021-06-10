8a9fbb039577a823586ece6da4b4934c334cdd06
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2021-06-10 11:37:50 +0200

    change ref of filter tag module

--- common/module/modules.tf
+++ common/module/modules.tf
@@ -1,5 +1,5 @@
 module "filter-tags" {
-  source = "github.com/claranet/terraform-signalfx-detectors.git//common/filter-tags"
+  source = "github.com/claranet/terraform-signalfx-detectors.git//common/filter-tags?ref=103_improve_filtering_and_append_mode"
 
   filter_defaults = local.filters
   filter_custom   = var.filter_custom
