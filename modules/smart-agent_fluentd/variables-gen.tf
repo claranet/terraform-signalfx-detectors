eb22054338806637a0c66c0a0efb74bdf06ca599
Ignacio Rivas Mendez <ignacio.rivas@fr.clara.net>
2021-07-28 18:01:31 +0200

    Add detector for fluentd logs with prometheus

---
+++ modules/smart-agent_fluentd/variables-gen.tf
@@ -0,0 +1,81 @@
+# heartbeat detector
+
+variable "heartbeat_notifications" {
+  description = "Notification recipients list per severity overridden for heartbeat detector"
+  type        = map(list(string))
+  default     = {}
+}
+
+variable "heartbeat_aggregation_function" {
+  description = "Aggregation function and group by for heartbeat detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ".sum(by=['host'])"
+}
+
+variable "heartbeat_tip" {
+  description = "Suggested first course of action or any note useful for incident handling"
+  type        = string
+  default     = ""
+}
+
+variable "heartbeat_runbook_url" {
+  description = "URL like SignalFx dashboard or wiki page which can help to troubleshoot the incident cause"
+  type        = string
+  default     = ""
+}
+
+variable "heartbeat_disabled" {
+  description = "Disable all alerting rules for heartbeat detector"
+  type        = bool
+  default     = null
+}
+
+variable "heartbeat_timeframe" {
+  description = "Timeframe for heartbeat detector (i.e. \"10m\")"
+  type        = string
+  default     = "20m"
+}
+
+# buffer detector
+
+variable "buffer_notifications" {
+  description = "Notification recipients list per severity overridden for buffer detector"
+  type        = map(list(string))
+  default     = {}
+}
+
+variable "buffer_aggregation_function" {
+  description = "Aggregation function and group by for buffer detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ".sum(by=['host'])"
+}
+
+variable "buffer_transformation_function" {
+  description = "Transformation function for buffer detector (i.e. \".mean(over='5m')\")"
+  type        = string
+  default     = ".max(over='5min')"
+}
+
+variable "buffer_tip" {
+  description = "Suggested first course of action or any note useful for incident handling"
+  type        = string
+  default     = ""
+}
+
+variable "buffer_disabled" {
+  description = "Disable all alerting rules for buffer detector"
+  type        = bool
+  default     = null
+}
+
+variable "buffer_runbook_url" {
+  description = "URL like SignalFx dashboard or wiki page which can help to troubleshoot the incident cause"
+  type        = string
+  default     = ""
+}
+
+variable "buffer_threshold" {
+  description = "Major threshold for up detector"
+  type        = number
+  default     = 1
+}
