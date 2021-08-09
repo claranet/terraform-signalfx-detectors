684caa1a408e5cd8e160976f06a4a12cc5214e05
Maxime de Roucy <maxime.deroucy@fr.clara.net>
2021-08-09 19:53:04 +0200

    smart-agent_systemd-services: systemd_services → aliveness (+ aggregation false→true)

--- modules/smart-agent_systemd-services/detectors-gen.tf
+++ modules/smart-agent_systemd-services/detectors-gen.tf
@@ -1,4 +1,4 @@
-resource "signalfx_detector" "systemd_services" {
+resource "signalfx_detector" "aliveness" {
   name = format("%s %s", local.detector_name_prefix, "Systemd-services aliveness")
 
   authorized_writer_teams = var.authorized_writer_teams
@@ -6,18 +6,18 @@ resource "signalfx_detector" "systemd_services" {
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
   program_text = <<-EOF
-    signal = data('gauge.substate.running', filter=${module.filtering.signalflow})${var.systemd_services_transformation_function}.publish('signal')
-    detect(when(signal != ${var.systemd_services_threshold_critical}, lasting=%{if var.systemd_services_lasting_duration_critical == null}None%{else}'${var.systemd_services_lasting_duration_critical}'%{endif}, at_least=${var.systemd_services_at_least_percentage_critical})).publish('CRIT')
+    signal = data('gauge.substate.running', filter=${module.filtering.signalflow})${var.aliveness_aggregation_function}${var.aliveness_transformation_function}.publish('signal')
+    detect(when(signal != ${var.aliveness_threshold_critical})).publish('CRIT')
 EOF
 
   rule {
-    description           = "is != ${var.systemd_services_threshold_critical}"
+    description           = "is != ${var.aliveness_threshold_critical}"
     severity              = "Critical"
     detect_label          = "CRIT"
-    disabled              = coalesce(var.systemd_services_disabled, var.detectors_disabled)
-    notifications         = coalescelist(lookup(var.systemd_services_notifications, "critical", []), var.notifications.critical)
-    runbook_url           = try(coalesce(var.systemd_services_runbook_url, var.runbook_url), "")
-    tip                   = var.systemd_services_tip
+    disabled              = coalesce(var.aliveness_disabled, var.detectors_disabled)
+    notifications         = coalescelist(lookup(var.aliveness_notifications, "critical", []), var.notifications.critical)
+    runbook_url           = try(coalesce(var.aliveness_runbook_url, var.runbook_url), "")
+    tip                   = var.aliveness_tip
     parameterized_subject = var.message_subject == "" ? local.rule_subject : var.message_subject
     parameterized_body    = var.message_body == "" ? local.rule_body : var.message_body
   }
