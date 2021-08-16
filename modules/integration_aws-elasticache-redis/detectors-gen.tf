8c210091d87b013f221a167d5dc5b25a9d5dc424
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2021-08-16 12:00:18 +0200

    use generator for integration_aws-elasticache-redis

--- modules/integration_aws-elasticache-redis/detectors-gen.tf
+++ modules/integration_aws-elasticache-redis/detectors-gen.tf
@@ -5,16 +5,22 @@ resource "signalfx_detector" "cache_hits" {
   teams                   = try(coalescelist(var.teams, var.authorized_writer_teams), null)
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
+  viz_options {
+    label        = "signal"
+    value_suffix = "%"
+  }
+
   program_text = <<-EOF
-    A = data('CacheHits', filter=filter('namespace', 'AWS/ElastiCache') and filter('stat', 'mean') and filter('CacheNodeId', '*') and ${module.filtering.signalflow}, extrapolation='zero', rollup='sum')${var.cache_hits_aggregation_function}${var.cache_hits_transformation_function}
-    B = data('CacheMisses', filter=filter('namespace', 'AWS/ElastiCache') and filter('stat', 'mean') and filter('CacheNodeId', '*') and ${module.filtering.signalflow}, extrapolation='zero', rollup='sum')${var.cache_hits_aggregation_function}${var.cache_hits_transformation_function}
-    signal = (A / (A+B)).fill(value=1).scale(100).publish('signal')
-    detect(when(signal < threshold(${var.cache_hits_threshold_critical}), lasting='${var.cache_hits_lasting_duration_seconds}s', at_least=${var.cache_hits_at_least_percentage})).publish('CRIT')
-    detect((when(signal < threshold(${var.cache_hits_threshold_major}), lasting='${var.cache_hits_lasting_duration_seconds}s', at_least=${var.cache_hits_at_least_percentage}) and when(signal >= ${var.cache_hits_threshold_critical})), off=(when(signal >= ${var.cache_hits_threshold_major}, lasting='${var.cache_hits_lasting_duration_seconds / 2}s') or when(signal <= ${var.cache_hits_threshold_critical}, lasting='${var.cache_hits_lasting_duration_seconds}s', at_least=${var.cache_hits_at_least_percentage})), mode='paired').publish('MAJOR')
+    base_filtering = filter('namespace', 'AWS/ElastiCache') and filter('stat', 'mean') and filter('CacheNodeId', '*')
+    hits = data('CacheHits', filter=base_filtering and ${module.filtering.signalflow})${var.cache_hits_aggregation_function}${var.cache_hits_transformation_function}
+    misses = data('CacheMisses', filter=base_filtering and ${module.filtering.signalflow})${var.cache_hits_aggregation_function}${var.cache_hits_transformation_function}
+    signal = (hits/(hits+misses).fill(value=1).scale(100).publish('signal')
+    detect(when(signal < ${var.cache_hits_threshold_critical}, lasting=%{if var.cache_hits_lasting_duration_critical == null}None%{else}'${var.cache_hits_lasting_duration_critical}'%{endif}, at_least=${var.cache_hits_at_least_percentage_critical})).publish('CRIT')
+    detect(when(signal < ${var.cache_hits_threshold_major}, lasting=%{if var.cache_hits_lasting_duration_major == null}None%{else}'${var.cache_hits_lasting_duration_major}'%{endif}, at_least=${var.cache_hits_at_least_percentage_major}) and (not when(signal < ${var.cache_hits_threshold_critical}, lasting=%{if var.cache_hits_lasting_duration_critical == null}None%{else}'${var.cache_hits_lasting_duration_critical}'%{endif}, at_least=${var.cache_hits_at_least_percentage_critical}))).publish('MAJOR')
 EOF
 
   rule {
-    description           = "is too low < ${var.cache_hits_threshold_critical}"
+    description           = "is too low < ${var.cache_hits_threshold_critical}%"
     severity              = "Critical"
     detect_label          = "CRIT"
     disabled              = coalesce(var.cache_hits_disabled_critical, var.cache_hits_disabled, var.detectors_disabled)
@@ -26,7 +32,7 @@ EOF
   }
 
   rule {
-    description           = "is too low < ${var.cache_hits_threshold_major}"
+    description           = "is too low < ${var.cache_hits_threshold_major}%"
     severity              = "Major"
     detect_label          = "MAJOR"
     disabled              = coalesce(var.cache_hits_disabled_major, var.cache_hits_disabled, var.detectors_disabled)
@@ -39,20 +45,26 @@ EOF
 }
 
 resource "signalfx_detector" "cpu_high" {
-  name = format("%s %s", local.detector_name_prefix, "AWS ElastiCache redis CPU")
+  name = format("%s %s", local.detector_name_prefix, "AWS ElastiCache redis cpu")
 
   authorized_writer_teams = var.authorized_writer_teams
   teams                   = try(coalescelist(var.teams, var.authorized_writer_teams), null)
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
+  viz_options {
+    label        = "signal"
+    value_suffix = "%"
+  }
+
   program_text = <<-EOF
-    signal = data('EngineCPUUtilization', filter=filter('namespace', 'AWS/ElastiCache') and filter('stat', 'upper') and filter('CacheNodeId', '*') and ${module.filtering.signalflow})${var.cpu_high_aggregation_function}${var.cpu_high_transformation_function}.publish('signal')
-    detect(when(signal > ${var.cpu_high_threshold_critical})).publish('CRIT')
-    detect(when(signal > ${var.cpu_high_threshold_major}) and (not when(signal > ${var.cpu_high_threshold_critical}))).publish('MAJOR')
+    base_filtering = filter('namespace', 'AWS/ElastiCache') and filter('stat', 'upper') and filter('CacheNodeId', '*')
+    signal = data('EngineCPUUtilization', filter=base_filtering and ${module.filtering.signalflow})${var.cpu_high_aggregation_function}${var.cpu_high_transformation_function}.publish('signal')
+    detect(when(signal > ${var.cpu_high_threshold_critical}, lasting=%{if var.cpu_high_lasting_duration_critical == null}None%{else}'${var.cpu_high_lasting_duration_critical}'%{endif}, at_least=${var.cpu_high_at_least_percentage_critical})).publish('CRIT')
+    detect(when(signal > ${var.cpu_high_threshold_major}, lasting=%{if var.cpu_high_lasting_duration_major == null}None%{else}'${var.cpu_high_lasting_duration_major}'%{endif}, at_least=${var.cpu_high_at_least_percentage_major}) and (not when(signal > ${var.cpu_high_threshold_critical}, lasting=%{if var.cpu_high_lasting_duration_critical == null}None%{else}'${var.cpu_high_lasting_duration_critical}'%{endif}, at_least=${var.cpu_high_at_least_percentage_critical}))).publish('MAJOR')
 EOF
 
   rule {
-    description           = "is too high > ${var.cpu_high_threshold_critical}"
+    description           = "is too high > ${var.cpu_high_threshold_critical}%"
     severity              = "Critical"
     detect_label          = "CRIT"
     disabled              = coalesce(var.cpu_high_disabled_critical, var.cpu_high_disabled, var.detectors_disabled)
@@ -64,7 +76,7 @@ EOF
   }
 
   rule {
-    description           = "is too high > ${var.cpu_high_threshold_major}"
+    description           = "is too high > ${var.cpu_high_threshold_major}%"
     severity              = "Major"
     detect_label          = "MAJOR"
     disabled              = coalesce(var.cpu_high_disabled_major, var.cpu_high_disabled, var.detectors_disabled)
@@ -83,14 +95,20 @@ resource "signalfx_detector" "replication_lag" {
   teams                   = try(coalescelist(var.teams, var.authorized_writer_teams), null)
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
+  viz_options {
+    label        = "signal"
+    value_suffix = "s"
+  }
+
   program_text = <<-EOF
-    signal = data('ReplicationLag', filter=filter('namespace', 'AWS/ElastiCache') and filter('stat', 'upper') and filter('CacheNodeId', '*') and ${module.filtering.signalflow})${var.replication_lag_aggregation_function}${var.replication_lag_transformation_function}.publish('signal')
-    detect(when(signal > ${var.replication_lag_threshold_critical})).publish('CRIT')
-    detect(when(signal > ${var.replication_lag_threshold_major}) and (not when(signal > ${var.replication_lag_threshold_critical}))).publish('MAJOR')
+    base_filtering = filter('namespace', 'AWS/ElastiCache') and filter('stat', 'upper') and filter('CacheNodeId', '*')
+    signal = data('ReplicationLag', filter=base_filtering and ${module.filtering.signalflow})${var.replication_lag_aggregation_function}${var.replication_lag_transformation_function}.publish('signal')
+    detect(when(signal > ${var.replication_lag_threshold_critical}, lasting=%{if var.replication_lag_lasting_duration_critical == null}None%{else}'${var.replication_lag_lasting_duration_critical}'%{endif}, at_least=${var.replication_lag_at_least_percentage_critical})).publish('CRIT')
+    detect(when(signal > ${var.replication_lag_threshold_major}, lasting=%{if var.replication_lag_lasting_duration_major == null}None%{else}'${var.replication_lag_lasting_duration_major}'%{endif}, at_least=${var.replication_lag_at_least_percentage_major}) and (not when(signal > ${var.replication_lag_threshold_critical}, lasting=%{if var.replication_lag_lasting_duration_critical == null}None%{else}'${var.replication_lag_lasting_duration_critical}'%{endif}, at_least=${var.replication_lag_at_least_percentage_critical}))).publish('MAJOR')
 EOF
 
   rule {
-    description           = "is too high > ${var.replication_lag_threshold_critical}"
+    description           = "is too high > ${var.replication_lag_threshold_critical}s"
     severity              = "Critical"
     detect_label          = "CRIT"
     disabled              = coalesce(var.replication_lag_disabled_critical, var.replication_lag_disabled, var.detectors_disabled)
@@ -102,7 +120,7 @@ EOF
   }
 
   rule {
-    description           = "is too high > ${var.replication_lag_threshold_major}"
+    description           = "is too high > ${var.replication_lag_threshold_major}s"
     severity              = "Major"
     detect_label          = "MAJOR"
     disabled              = coalesce(var.replication_lag_disabled_major, var.replication_lag_disabled, var.detectors_disabled)
@@ -122,30 +140,18 @@ resource "signalfx_detector" "commands" {
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
   program_text = <<-EOF
-    A = data('GetTypeCmds', filter=filter('namespace', 'AWS/ElastiCache') and filter('stat', 'lower') and filter('CacheNodeId', '*') and ${module.filtering.signalflow})${var.commands_aggregation_function}${var.commands_transformation_function}
-    B = data('SetTypeCmds', filter=filter('namespace', 'AWS/ElastiCache') and filter('stat', 'lower') and filter('CacheNodeId', '*') and ${module.filtering.signalflow})${var.commands_aggregation_function}${var.commands_transformation_function}
-    signal = (A + B).publish('signal')
-    detect(when(signal <= ${var.commands_threshold_critical})).publish('CRIT')
-    detect(when(signal <= ${var.commands_threshold_major}) and (not when(signal <= ${var.commands_threshold_critical}))).publish('MAJOR')
+    base_filtering = filter('namespace', 'AWS/ElastiCache') and filter('stat', 'lower') and filter('CacheNodeId', '*')
+    get = data('GetTypeCmds', filter=base_filtering and ${module.filtering.signalflow})${var.commands_aggregation_function}${var.commands_transformation_function}
+    set = data('SetTypeCmds', filter=base_filtering and ${module.filtering.signalflow})${var.commands_aggregation_function}${var.commands_transformation_function}
+    signal = (get+set).publish('signal')
+    detect(when(signal <= ${var.commands_threshold_major}, lasting=%{if var.commands_lasting_duration_major == null}None%{else}'${var.commands_lasting_duration_major}'%{endif}, at_least=${var.commands_at_least_percentage_major})).publish('MAJOR')
 EOF
 
   rule {
-    description           = "are too low <= ${var.commands_threshold_critical}"
-    severity              = "Critical"
-    detect_label          = "CRIT"
-    disabled              = coalesce(var.commands_disabled_critical, var.commands_disabled, var.detectors_disabled)
-    notifications         = coalescelist(lookup(var.commands_notifications, "critical", []), var.notifications.critical)
-    runbook_url           = try(coalesce(var.commands_runbook_url, var.runbook_url), "")
-    tip                   = var.commands_tip
-    parameterized_subject = var.message_subject == "" ? local.rule_subject : var.message_subject
-    parameterized_body    = var.message_body == "" ? local.rule_body : var.message_body
-  }
-
-  rule {
-    description           = "are too low <= ${var.commands_threshold_major}"
+    description           = "is too low <= ${var.commands_threshold_major}"
     severity              = "Major"
     detect_label          = "MAJOR"
-    disabled              = coalesce(var.commands_disabled_major, var.commands_disabled, var.detectors_disabled)
+    disabled              = coalesce(var.commands_disabled, var.detectors_disabled)
     notifications         = coalescelist(lookup(var.commands_notifications, "major", []), var.notifications.major)
     runbook_url           = try(coalesce(var.commands_runbook_url, var.runbook_url), "")
     tip                   = var.commands_tip
