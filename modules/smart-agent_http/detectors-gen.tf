2ff2f9b2f5310a9cb160ab33a28e69019b78a1f4
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2021-08-16 12:16:28 +0200

    use generator for smart-agent_http

--- modules/smart-agent_http/detectors-gen.tf
+++ modules/smart-agent_http/detectors-gen.tf
@@ -34,22 +34,21 @@ resource "signalfx_detector" "http_code_matched" {
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
   program_text = <<-EOF
-    signal = data('http.code_matched', filter=${module.filtering.signalflow}, rollup='max')${var.http_code_matched_aggregation_function}${var.http_code_matched_transformation_function}.publish('signal')
-    detect(when(signal < 1, lasting='${var.http_code_matched_lasting_duration_seconds}s', at_least=${var.http_code_matched_at_least_percentage})).publish('CRIT')
+    signal = data('http.code_matched', filter=${module.filtering.signalflow}, rollup='min')${var.http_code_matched_aggregation_function}${var.http_code_matched_transformation_function}.publish('signal')
+    detect(when(signal < ${var.http_code_matched_threshold_critical}, lasting=%{if var.http_code_matched_lasting_duration_critical == null}None%{else}'${var.http_code_matched_lasting_duration_critical}'%{endif}, at_least=${var.http_code_matched_at_least_percentage_critical})).publish('CRIT')
 EOF
 
   rule {
-    description           = "does not match expected result"
+    description           = "does not match expected result < ${var.http_code_matched_threshold_critical}"
     severity              = "Critical"
     detect_label          = "CRIT"
-    disabled              = coalesce(var.http_code_matched_disabled_critical, var.http_code_matched_disabled, var.detectors_disabled)
+    disabled              = coalesce(var.http_code_matched_disabled, var.detectors_disabled)
     notifications         = coalescelist(lookup(var.http_code_matched_notifications, "critical", []), var.notifications.critical)
     runbook_url           = try(coalesce(var.http_code_matched_runbook_url, var.runbook_url), "")
     tip                   = var.http_code_matched_tip
     parameterized_subject = var.message_subject == "" ? local.rule_subject : var.message_subject
     parameterized_body    = var.message_body == "" ? local.rule_body : var.message_body
   }
-
 }
 
 resource "signalfx_detector" "http_regex_matched" {
@@ -61,21 +60,20 @@ resource "signalfx_detector" "http_regex_matched" {
 
   program_text = <<-EOF
     signal = data('http.regex_matched', filter=${module.filtering.signalflow}, rollup='min')${var.http_regex_matched_aggregation_function}${var.http_regex_matched_transformation_function}.publish('signal')
-    detect(when(signal < 1, lasting='${var.http_regex_matched_lasting_duration_seconds}s', at_least=${var.http_regex_matched_at_least_percentage})).publish('CRIT')
+    detect(when(signal < ${var.http_regex_matched_threshold_critical}, lasting=%{if var.http_regex_matched_lasting_duration_critical == null}None%{else}'${var.http_regex_matched_lasting_duration_critical}'%{endif}, at_least=${var.http_regex_matched_at_least_percentage_critical})).publish('CRIT')
 EOF
 
   rule {
-    description           = "does not match in body response"
+    description           = "does not match in body response < ${var.http_regex_matched_threshold_critical}"
     severity              = "Critical"
     detect_label          = "CRIT"
-    disabled              = coalesce(var.http_regex_matched_disabled_critical, var.http_regex_matched_disabled, var.detectors_disabled)
+    disabled              = coalesce(var.http_regex_matched_disabled, var.detectors_disabled)
     notifications         = coalescelist(lookup(var.http_regex_matched_notifications, "critical", []), var.notifications.critical)
     runbook_url           = try(coalesce(var.http_regex_matched_runbook_url, var.runbook_url), "")
     tip                   = var.http_regex_matched_tip
     parameterized_subject = var.message_subject == "" ? local.rule_subject : var.message_subject
     parameterized_body    = var.message_body == "" ? local.rule_body : var.message_body
   }
-
 }
 
 resource "signalfx_detector" "http_response_time" {
@@ -87,12 +85,12 @@ resource "signalfx_detector" "http_response_time" {
 
   program_text = <<-EOF
     signal = data('http.response_time', filter=${module.filtering.signalflow}, rollup='max')${var.http_response_time_aggregation_function}${var.http_response_time_transformation_function}.publish('signal')
-    detect(when(signal > ${var.http_response_time_threshold_critical})).publish('CRIT')
-    detect(when(signal > ${var.http_response_time_threshold_major}) and (not when(signal > ${var.http_response_time_threshold_critical}))).publish('MAJOR')
+    detect(when(signal > ${var.http_response_time_threshold_critical}, lasting=%{if var.http_response_time_lasting_duration_critical == null}None%{else}'${var.http_response_time_lasting_duration_critical}'%{endif}, at_least=${var.http_response_time_at_least_percentage_critical})).publish('CRIT')
+    detect(when(signal > ${var.http_response_time_threshold_major}, lasting=%{if var.http_response_time_lasting_duration_major == null}None%{else}'${var.http_response_time_lasting_duration_major}'%{endif}, at_least=${var.http_response_time_at_least_percentage_major})).publish('MAJOR')
 EOF
 
   rule {
-    description           = "is too high > ${var.http_response_time_threshold_critical} seconds"
+    description           = "is too high > ${var.http_response_time_threshold_critical}"
     severity              = "Critical"
     detect_label          = "CRIT"
     disabled              = coalesce(var.http_response_time_disabled_critical, var.http_response_time_disabled, var.detectors_disabled)
@@ -104,7 +102,7 @@ EOF
   }
 
   rule {
-    description           = "is too high > ${var.http_response_time_threshold_major} seconds"
+    description           = "is too high > ${var.http_response_time_threshold_major}"
     severity              = "Major"
     detect_label          = "MAJOR"
     disabled              = coalesce(var.http_response_time_disabled_major, var.http_response_time_disabled, var.detectors_disabled)
@@ -123,13 +121,18 @@ resource "signalfx_detector" "http_content_length" {
   teams                   = try(coalescelist(var.teams, var.authorized_writer_teams), null)
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
+  viz_options {
+    label        = "signal"
+    value_suffix = "bytes"
+  }
+
   program_text = <<-EOF
     signal = data('http.content_length', filter=${module.filtering.signalflow}, rollup='min')${var.http_content_length_aggregation_function}${var.http_content_length_transformation_function}.publish('signal')
-    detect(when(signal < ${var.http_content_length_threshold_warning})).publish('WARN')
+    detect(when(signal < ${var.http_content_length_threshold_warning}, lasting=%{if var.http_content_length_lasting_duration_warning == null}None%{else}'${var.http_content_length_lasting_duration_warning}'%{endif}, at_least=${var.http_content_length_at_least_percentage_warning})).publish('WARN')
 EOF
 
   rule {
-    description           = "is too low < ${var.http_content_length_threshold_warning} bytes"
+    description           = "is too low < ${var.http_content_length_threshold_warning}bytes"
     severity              = "Warning"
     detect_label          = "WARN"
     disabled              = coalesce(var.http_content_length_disabled, var.detectors_disabled)
@@ -148,15 +151,20 @@ resource "signalfx_detector" "certificate_expiration_date" {
   teams                   = try(coalescelist(var.teams, var.authorized_writer_teams), null)
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
+  viz_options {
+    label        = "signal"
+    value_suffix = "days"
+  }
+
   program_text = <<-EOF
-    A = data('http.cert_expiry', filter=${module.filtering.signalflow}, rollup='min')${var.certificate_expiration_date_aggregation_function}${var.certificate_expiration_date_transformation_function}
-    signal = (A/86400).publish('signal')
-    detect(when(signal < ${var.certificate_expiration_date_threshold_major})).publish('MAJOR')
-    detect(when(signal < ${var.certificate_expiration_date_threshold_minor}) and (not when(signal < ${var.certificate_expiration_date_threshold_major}))).publish('MINOR')
+    expiry = data('http.cert_expiry', filter=${module.filtering.signalflow}, rollup='min')${var.certificate_expiration_date_aggregation_function}${var.certificate_expiration_date_transformation_function}
+    signal = (expiry/86400).publish('signal')
+    detect(when(signal < ${var.certificate_expiration_date_threshold_major}, lasting=%{if var.certificate_expiration_date_lasting_duration_major == null}None%{else}'${var.certificate_expiration_date_lasting_duration_major}'%{endif}, at_least=${var.certificate_expiration_date_at_least_percentage_major})).publish('MAJOR')
+    detect(when(signal < ${var.certificate_expiration_date_threshold_minor}, lasting=%{if var.certificate_expiration_date_lasting_duration_minor == null}None%{else}'${var.certificate_expiration_date_lasting_duration_minor}'%{endif}, at_least=${var.certificate_expiration_date_at_least_percentage_minor}) and (not when(signal < ${var.certificate_expiration_date_threshold_major}, lasting=%{if var.certificate_expiration_date_lasting_duration_major == null}None%{else}'${var.certificate_expiration_date_lasting_duration_major}'%{endif}, at_least=${var.certificate_expiration_date_at_least_percentage_major}))).publish('MINOR')
 EOF
 
   rule {
-    description           = "is too soon < ${var.certificate_expiration_date_threshold_major} days"
+    description           = "is too soon < ${var.certificate_expiration_date_threshold_major}days"
     severity              = "Major"
     detect_label          = "MAJOR"
     disabled              = coalesce(var.certificate_expiration_date_disabled_major, var.certificate_expiration_date_disabled, var.detectors_disabled)
@@ -168,7 +176,7 @@ EOF
   }
 
   rule {
-    description           = "is too soon < ${var.certificate_expiration_date_threshold_minor} days"
+    description           = "is too soon < ${var.certificate_expiration_date_threshold_minor}days"
     severity              = "Minor"
     detect_label          = "MINOR"
     disabled              = coalesce(var.certificate_expiration_date_disabled_minor, var.certificate_expiration_date_disabled, var.detectors_disabled)
@@ -187,16 +195,21 @@ resource "signalfx_detector" "invalid_tls_certificate" {
   teams                   = try(coalescelist(var.teams, var.authorized_writer_teams), null)
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
+  viz_options {
+    label        = "signal"
+    value_suffix = "days"
+  }
+
   program_text = <<-EOF
     signal = data('http.cert_valid', filter=${module.filtering.signalflow}, rollup='min')${var.invalid_tls_certificate_aggregation_function}${var.invalid_tls_certificate_transformation_function}.publish('signal')
-    detect(when(signal < 1)).publish('CRIT')
+    detect(when(signal < ${var.invalid_tls_certificate_threshold_critical}, lasting=%{if var.invalid_tls_certificate_lasting_duration_critical == null}None%{else}'${var.invalid_tls_certificate_lasting_duration_critical}'%{endif}, at_least=${var.invalid_tls_certificate_at_least_percentage_critical})).publish('CRIT')
 EOF
 
   rule {
-    description           = "is invalid"
+    description           = "is invalid < ${var.invalid_tls_certificate_threshold_critical}days"
     severity              = "Critical"
     detect_label          = "CRIT"
-    disabled              = coalesce(var.invalid_tls_certificate_disabled_critical, var.invalid_tls_certificate_disabled, var.detectors_disabled)
+    disabled              = coalesce(var.invalid_tls_certificate_disabled, var.detectors_disabled)
     notifications         = coalescelist(lookup(var.invalid_tls_certificate_notifications, "critical", []), var.notifications.critical)
     runbook_url           = try(coalesce(var.invalid_tls_certificate_runbook_url, var.runbook_url), "")
     tip                   = var.invalid_tls_certificate_tip
@@ -204,3 +217,4 @@ EOF
     parameterized_body    = var.message_body == "" ? local.rule_body : var.message_body
   }
 }
+
