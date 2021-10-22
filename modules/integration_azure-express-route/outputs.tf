d6860f2e5a348ebeb2ceb3285aa352f712f195de
Jérémy Marmol <jeremy.marmol@fr.clara.net>
2021-10-22 11:18:28 +0200

    Create Azure Express Route detectors (#329)
    * Create Azure Express Route detectors
    
    * Change severity threshold and add lasting_duration
    
    * Fix code 30
    
    * auto update
    
    Co-authored-by: Quentin Manfroi <quentin.manfroi@fr.clara.net>

---
+++ modules/integration_azure-express-route/outputs.tf
@@ -0,0 +1,15 @@
+output "arp_availability" {
+  description = "Detector resource for arp_availability"
+  value       = signalfx_detector.arp_availability
+}
+
+output "bgp_availability" {
+  description = "Detector resource for bgp_availability"
+  value       = signalfx_detector.bgp_availability
+}
+
+output "heartbeat" {
+  description = "Detector resource for heartbeat"
+  value       = signalfx_detector.heartbeat
+}
+
