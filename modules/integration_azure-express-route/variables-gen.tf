0314abd52d3f5446c9b246cce7034685c6b40a71
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2021-10-22 11:11:56 +0200

    auto update

--- modules/integration_azure-express-route/variables-gen.tf
+++ modules/integration_azure-express-route/variables-gen.tf
@@ -39,7 +39,7 @@ variable "heartbeat_disabled" {
 variable "heartbeat_timeframe" {
   description = "Timeframe for heartbeat detector (i.e. \"10m\")"
   type        = string
-  default     = "20m"
+  default     = "10m"
 }
 
 # bgp_availability detector
