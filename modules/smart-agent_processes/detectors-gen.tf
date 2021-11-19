6dbd2b8b310fa7065aa8318714eea4ff4fe49137
Quentin Manfroi <2693213+xp-1000@users.noreply.github.com>
2021-11-19 09:39:17 +0100

    convert processes detector using generator (#354)
    * add disabled at rule level for generator
    
    * convert processes detector using generator
    
    * rebase on generator_disabled_rule

--- modules/smart-agent_processes/detectors-gen.tf
+++ modules/smart-agent_processes/detectors-gen.tf
@@ -1,18 +1,18 @@
 resource "signalfx_detector" "processes" {
-  name = format("%s %s", local.detector_name_prefix, "Processes aliveness")
+  name = format("%s %s", local.detector_name_prefix, "Processes aliveness count")
 
   authorized_writer_teams = var.authorized_writer_teams
   teams                   = try(coalescelist(var.teams, var.authorized_writer_teams), null)
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
   program_text = <<-EOF
-        signal = data('ps_count.processes', filter=${module.filtering.signalflow})${var.processes_aggregation_function}${var.processes_transformation_function}.publish('signal')
-        detect(when(signal < 1)).publish('CRIT')
-        detect(when(signal < ${var.processes_threshold_major}) and when (signal >= 1)).publish('MAJOR')
+    signal = data('ps_count.processes', filter=${module.filtering.signalflow})${var.processes_aggregation_function}${var.processes_transformation_function}.publish('signal')
+    detect(when(signal < ${var.processes_threshold_critical}, lasting=%{if var.processes_lasting_duration_critical == null}None%{else}'${var.processes_lasting_duration_critical}'%{endif}, at_least=${var.processes_at_least_percentage_critical})).publish('CRIT')
+    detect(when(signal < ${var.processes_threshold_major}, lasting=%{if var.processes_lasting_duration_major == null}None%{else}'${var.processes_lasting_duration_major}'%{endif}, at_least=${var.processes_at_least_percentage_major}) and (not when(signal < ${var.processes_threshold_critical}, lasting=%{if var.processes_lasting_duration_critical == null}None%{else}'${var.processes_lasting_duration_critical}'%{endif}, at_least=${var.processes_at_least_percentage_critical}))).publish('MAJOR')
 EOF
 
   rule {
-    description           = "count is too low < 1"
+    description           = "is too low < ${var.processes_threshold_critical}"
     severity              = "Critical"
     detect_label          = "CRIT"
     disabled              = coalesce(var.processes_disabled_critical, var.processes_disabled, var.detectors_disabled)
@@ -24,7 +24,7 @@ EOF
   }
 
   rule {
-    description           = "count is too low < ${var.processes_threshold_major}"
+    description           = "is too low < ${var.processes_threshold_major}"
     severity              = "Major"
     detect_label          = "MAJOR"
     disabled              = coalesce(var.processes_disabled_major, var.processes_disabled, var.detectors_disabled)
@@ -35,3 +35,4 @@ EOF
     parameterized_body    = var.message_body == "" ? local.rule_body : var.message_body
   }
 }
+
---
+++ modules/smart-agent_processes/variables-gen.tf
@@ -0,0 +1,84 @@
+# processes detector
+
+variable "processes_notifications" {
+  description = "Notification recipients list per severity overridden for processes detector"
+  type        = map(list(string))
+  default     = {}
+}
+
+variable "processes_aggregation_function" {
+  description = "Aggregation function and group by for processes detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ""
+}
+
+variable "processes_transformation_function" {
+  description = "Transformation function for processes detector (i.e. \".mean(over='5m')\")"
+  type        = string
+  default     = ".max(over='15m')"
+}
+
+variable "processes_tip" {
+  description = "Suggested first course of action or any note useful for incident handling"
+  type        = string
+  default     = ""
+}
+
+variable "processes_runbook_url" {
+  description = "URL like SignalFx dashboard or wiki page which can help to troubleshoot the incident cause"
+  type        = string
+  default     = ""
+}
+
+variable "processes_disabled" {
+  description = "Disable all alerting rules for processes detector"
+  type        = bool
+  default     = null
+}
+
+variable "processes_disabled_critical" {
+  description = "Disable critical alerting rule for processes detector"
+  type        = bool
+  default     = null
+}
+
+variable "processes_disabled_major" {
+  description = "Disable major alerting rule for processes detector"
+  type        = bool
+  default     = true
+}
+
+variable "processes_threshold_critical" {
+  description = "Critical threshold for processes detector"
+  type        = number
+  default     = 1
+}
+
+variable "processes_lasting_duration_critical" {
+  description = "Minimum duration that conditions must be true before raising alert"
+  type        = string
+  default     = null
+}
+
+variable "processes_at_least_percentage_critical" {
+  description = "Percentage of lasting that conditions must be true before raising alert (>= 0.0 and <= 1.0)"
+  type        = number
+  default     = 1
+}
+variable "processes_threshold_major" {
+  description = "Major threshold for processes detector"
+  type        = number
+  default     = 2
+}
+
+variable "processes_lasting_duration_major" {
+  description = "Minimum duration that conditions must be true before raising alert"
+  type        = string
+  default     = null
+}
+
+variable "processes_at_least_percentage_major" {
+  description = "Percentage of lasting that conditions must be true before raising alert (>= 0.0 and <= 1.0)"
+  type        = number
+  default     = 1
+}
