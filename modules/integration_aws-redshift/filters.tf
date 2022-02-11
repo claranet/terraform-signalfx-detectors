85386ee9e2c862a6b285fb3c13ce8bdea69d805f
Gregory Bloquel <gregory.bloquel@fr.clara.net>
2022-02-11 10:22:30 +0100

    fix #377: Using AWS common filters instead of default from generator

--- modules/integration_aws-redshift/filters.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  # See https://dev.splunk.com/observability/docs/signalflow/functions/filter_function/ to create filters string
-  filters = "filter('put a generic and source relevant', 'filter policy here') and filter('do not hesitate to use variable like', '${var.environment}')"
-}
-
