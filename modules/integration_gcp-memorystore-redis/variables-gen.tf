fa0edc14485bc15b11581e6bffcbcadf0dbd37c6
Benjamin Rabiller <benjamin.rabiller@fr.clara.net>
2022-06-03 16:46:42 +0200

    Add generated variables for blocked_over_connected_clients_ratio detector

--- modules/integration_gcp-memorystore-redis/variables-gen.tf
+++ modules/integration_gcp-memorystore-redis/variables-gen.tf
@@ -42,3 +42,93 @@ variable "heartbeat_timeframe" {
   default     = "10m"
 }
 
+# blocked_over_connected_clients_ratio detector
+
+variable "blocked_over_connected_clients_ratio_notifications" {
+  description = "Notification recipients list per severity overridden for blocked_over_connected_clients_ratio detector"
+  type        = map(list(string))
+  default     = {}
+}
+
+variable "blocked_over_connected_clients_ratio_aggregation_function" {
+  description = "Aggregation function and group by for blocked_over_connected_clients_ratio detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ""
+}
+
+variable "blocked_over_connected_clients_ratio_transformation_function" {
+  description = "Transformation function for blocked_over_connected_clients_ratio detector (i.e. \".mean(over='5m')\")"
+  type        = string
+  default     = ""
+}
+
+variable "blocked_over_connected_clients_ratio_max_delay" {
+  description = "Enforce max delay for blocked_over_connected_clients_ratio detector (use \"0\" or \"null\" for \"Auto\")"
+  type        = number
+  default     = null
+}
+
+variable "blocked_over_connected_clients_ratio_tip" {
+  description = "Suggested first course of action or any note useful for incident handling"
+  type        = string
+  default     = ""
+}
+
+variable "blocked_over_connected_clients_ratio_runbook_url" {
+  description = "URL like SignalFx dashboard or wiki page which can help to troubleshoot the incident cause"
+  type        = string
+  default     = ""
+}
+
+variable "blocked_over_connected_clients_ratio_disabled" {
+  description = "Disable all alerting rules for blocked_over_connected_clients_ratio detector"
+  type        = bool
+  default     = null
+}
+
+variable "blocked_over_connected_clients_ratio_disabled_critical" {
+  description = "Disable critical alerting rule for blocked_over_connected_clients_ratio detector"
+  type        = bool
+  default     = null
+}
+
+variable "blocked_over_connected_clients_ratio_disabled_major" {
+  description = "Disable major alerting rule for blocked_over_connected_clients_ratio detector"
+  type        = bool
+  default     = null
+}
+
+variable "blocked_over_connected_clients_ratio_threshold_critical" {
+  description = "Critical threshold for blocked_over_connected_clients_ratio detector in %"
+  type        = number
+  default     = 5
+}
+
+variable "blocked_over_connected_clients_ratio_lasting_duration_critical" {
+  description = "Minimum duration that conditions must be true before raising alert"
+  type        = string
+  default     = "1h"
+}
+
+variable "blocked_over_connected_clients_ratio_at_least_percentage_critical" {
+  description = "Percentage of lasting that conditions must be true before raising alert (>= 0.0 and <= 1.0)"
+  type        = number
+  default     = 0.5
+}
+variable "blocked_over_connected_clients_ratio_threshold_major" {
+  description = "Major threshold for blocked_over_connected_clients_ratio detector in %"
+  type        = number
+  default     = 0
+}
+
+variable "blocked_over_connected_clients_ratio_lasting_duration_major" {
+  description = "Minimum duration that conditions must be true before raising alert"
+  type        = string
+  default     = "1h"
+}
+
+variable "blocked_over_connected_clients_ratio_at_least_percentage_major" {
+  description = "Percentage of lasting that conditions must be true before raising alert (>= 0.0 and <= 1.0)"
+  type        = number
+  default     = 0.5
+}
