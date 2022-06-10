51e5aa188b115e0c9139e480ae0ed4d61e34d445
Benjamin Rabiller <benjamin.rabiller@fr.clara.net>
2022-06-10 17:00:43 +0200

    Add outputs

--- modules/integration_gcp-memorystore-redis/outputs.tf
+++ modules/integration_gcp-memorystore-redis/outputs.tf
@@ -3,3 +3,12 @@ output "heartbeat" {
   value       = signalfx_detector.heartbeat
 }
 
+output "blocked_over_connected_clients_ratio" {
+  description = "Detector resource for blocked_over_connected_clients_ratio"
+  value       = signalfx_detector.blocked_over_connected_clients_ratio
+}
+
+output "system_memory_usage_ratio" {
+  description = "Detector resource for system_memory_usage_ratio"
+  value       = signalfx_detector.system_memory_usage_ratio
+}
