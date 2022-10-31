c9611beb41f0cea72ec6e0b3997395b58c9526db
Gauthier AMPE <gauthier.ampe@fr.clara.net>
2022-10-31 11:50:37 +0100

    Improve cdn detector

--- modules/integration_azure-cdn/outputs.tf
+++ modules/integration_azure-cdn/outputs.tf
@@ -1,5 +1,5 @@
-output "latency" {
-  description = "Detector resource for latency"
-  value       = signalfx_detector.latency
+output "cdn_total_latency" {
+  description = "Detector resource for cdn_total_latency"
+  value       = signalfx_detector.cdn_total_latency
 }
 
--- modules/integration_azure-cdn/variables-gen.tf
+++ modules/integration_azure-cdn/variables-gen.tf
@@ -1,89 +1,89 @@
-# latency detector
+# cdn_total_latency detector
 
-variable "latency_notifications" {
-  description = "Notification recipients list per severity overridden for latency detector"
+variable "cdn_total_latency_notifications" {
+  description = "Notification recipients list per severity overridden for cdn_total_latency detector"
   type        = map(list(string))
   default     = {}
 }
 
-variable "latency_aggregation_function" {
-  description = "Aggregation function and group by for latency detector (i.e. \".mean(by=['host'])\")"
+variable "cdn_total_latency_aggregation_function" {
+  description = "Aggregation function and group by for cdn_total_latency detector (i.e. \".mean(by=['host'])\")"
   type        = string
   default     = ""
 }
 
-variable "latency_transformation_function" {
-  description = "Transformation function for latency detector (i.e. \".mean(over='5m')\")"
+variable "cdn_total_latency_transformation_function" {
+  description = "Transformation function for cdn_total_latency detector (i.e. \".mean(over='5m')\")"
   type        = string
-  default     = ".max(over='15m')"
+  default     = ""
 }
 
-variable "latency_max_delay" {
-  description = "Enforce max delay for latency detector (use \"0\" or \"null\" for \"Auto\")"
+variable "cdn_total_latency_max_delay" {
+  description = "Enforce max delay for cdn_total_latency detector (use \"0\" or \"null\" for \"Auto\")"
   type        = number
   default     = null
 }
 
-variable "latency_tip" {
+variable "cdn_total_latency_tip" {
   description = "Suggested first course of action or any note useful for incident handling"
   type        = string
   default     = ""
 }
 
-variable "latency_runbook_url" {
+variable "cdn_total_latency_runbook_url" {
   description = "URL like SignalFx dashboard or wiki page which can help to troubleshoot the incident cause"
   type        = string
   default     = ""
 }
 
-variable "latency_disabled" {
-  description = "Disable all alerting rules for latency detector"
+variable "cdn_total_latency_disabled" {
+  description = "Disable all alerting rules for cdn_total_latency detector"
   type        = bool
   default     = null
 }
 
-variable "latency_disabled_critical" {
-  description = "Disable critical alerting rule for latency detector"
+variable "cdn_total_latency_disabled_critical" {
+  description = "Disable critical alerting rule for cdn_total_latency detector"
   type        = bool
   default     = null
 }
 
-variable "latency_disabled_major" {
-  description = "Disable major alerting rule for latency detector"
+variable "cdn_total_latency_disabled_major" {
+  description = "Disable major alerting rule for cdn_total_latency detector"
   type        = bool
   default     = null
 }
 
-variable "latency_threshold_critical" {
-  description = "Critical threshold for latency detector"
+variable "cdn_total_latency_threshold_critical" {
+  description = "Critical threshold for cdn_total_latency detector"
   type        = number
   default     = 1500
 }
 
-variable "latency_lasting_duration_critical" {
+variable "cdn_total_latency_lasting_duration_critical" {
   description = "Minimum duration that conditions must be true before raising alert"
   type        = string
-  default     = null
+  default     = "15m"
 }
 
-variable "latency_at_least_percentage_critical" {
+variable "cdn_total_latency_at_least_percentage_critical" {
   description = "Percentage of lasting that conditions must be true before raising alert (>= 0.0 and <= 1.0)"
   type        = number
   default     = 1
 }
-variable "latency_threshold_major" {
-  description = "Major threshold for latency detector"
+variable "cdn_total_latency_threshold_major" {
+  description = "Major threshold for cdn_total_latency detector"
   type        = number
   default     = 3000
 }
 
-variable "latency_lasting_duration_major" {
+variable "cdn_total_latency_lasting_duration_major" {
   description = "Minimum duration that conditions must be true before raising alert"
   type        = string
-  default     = null
+  default     = "15m"
 }
 
-variable "latency_at_least_percentage_major" {
+variable "cdn_total_latency_at_least_percentage_major" {
   description = "Percentage of lasting that conditions must be true before raising alert (>= 0.0 and <= 1.0)"
   type        = number
   default     = 1
