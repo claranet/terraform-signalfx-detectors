1e38cfdf1d1764c0eaaeeea6cce05898ee3473be
Jérémy MARMOL <jeremy.marmol@fr.clara.net>
2023-02-02 18:24:15 +0100

    Revert scale factor

--- modules/integration_azure-automation-account/detectors-gen.tf
+++ modules/integration_azure-automation-account/detectors-gen.tf
@@ -9,7 +9,7 @@ resource "signalfx_detector" "failed_jobs" {
     base_filtering = filter('resource_type', 'Microsoft.Automation/automationAccounts') and filter('primary_aggregation_type', 'true')
     success = data('TotalJob', filter=base_filtering and filter('status', 'Completed') and ${module.filtering.signalflow})${var.failed_jobs_aggregation_function}${var.failed_jobs_transformation_function}
     failed = data('TotalJob', filter=base_filtering and filter('status', 'Failed') and ${module.filtering.signalflow})${var.failed_jobs_aggregation_function}${var.failed_jobs_transformation_function}
-    signal = (failed.fill(0) - success.fill(0).scale(2)).publish('signal')
+    signal = (failed.fill(0) - success.fill(0)).publish('signal')
     detect(when(signal > ${var.failed_jobs_threshold_critical}, lasting=%{if var.failed_jobs_lasting_duration_critical == null}None%{else}'${var.failed_jobs_lasting_duration_critical}'%{endif}, at_least=${var.failed_jobs_at_least_percentage_critical})).publish('CRIT')
 EOF
 
