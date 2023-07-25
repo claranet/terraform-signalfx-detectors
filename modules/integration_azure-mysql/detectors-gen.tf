9b13acf5d70c00f22c55eb2cfb9ce089c14fb18d
Antoine Ogereau <138576425+aogereau@users.noreply.github.com>
2023-07-25 10:47:17 +0200

    Fix of detector Azure MySQL memory usage for standard and flexible Azure MySQL (#479)
    * fix for integration_azure-flexible-mysql
    
    * fix for integration_azure-mysql

--- modules/integration_azure-mysql/detectors-gen.tf
+++ modules/integration_azure-mysql/detectors-gen.tf
@@ -180,21 +180,9 @@ resource "signalfx_detector" "memory" {
   program_text = <<-EOF
     base_filtering = filter('resource_type', 'Microsoft.DBforMySQL/servers') and filter('primary_aggregation_type', 'true')
     signal = data('memory_percent', filter=base_filtering and ${module.filtering.signalflow})${var.memory_aggregation_function}${var.memory_transformation_function}.publish('signal')
-    detect(when(signal > ${var.memory_threshold_major}, lasting=%{if var.memory_lasting_duration_major == null}None%{else}'${var.memory_lasting_duration_major}'%{endif}, at_least=${var.memory_at_least_percentage_major})).publish('MAJOR')
-    detect(when(signal > ${var.memory_threshold_critical}, lasting=%{if var.memory_lasting_duration_critical == null}None%{else}'${var.memory_lasting_duration_critical}'%{endif}, at_least=${var.memory_at_least_percentage_critical}) and (not when(signal > ${var.memory_threshold_major}, lasting=%{if var.memory_lasting_duration_major == null}None%{else}'${var.memory_lasting_duration_major}'%{endif}, at_least=${var.memory_at_least_percentage_major}))).publish('CRIT')
+    detect(when(signal > ${var.memory_threshold_critical}, lasting=%{if var.memory_lasting_duration_critical == null}None%{else}'${var.memory_lasting_duration_critical}'%{endif}, at_least=${var.memory_at_least_percentage_critical})).publish('CRIT')
+    detect(when(signal > ${var.memory_threshold_major}, lasting=%{if var.memory_lasting_duration_major == null}None%{else}'${var.memory_lasting_duration_major}'%{endif}, at_least=${var.memory_at_least_percentage_major}) and (not when(signal > ${var.memory_threshold_critical}, lasting=%{if var.memory_lasting_duration_critical == null}None%{else}'${var.memory_lasting_duration_critical}'%{endif}, at_least=${var.memory_at_least_percentage_critical}))).publish('MAJOR')
 EOF
-
-  rule {
-    description           = "is too high > ${var.memory_threshold_major}%"
-    severity              = "Major"
-    detect_label          = "MAJOR"
-    disabled              = coalesce(var.memory_disabled_major, var.memory_disabled, var.detectors_disabled)
-    notifications         = try(coalescelist(lookup(var.memory_notifications, "major", []), var.notifications.major), null)
-    runbook_url           = try(coalesce(var.memory_runbook_url, var.runbook_url), "")
-    tip                   = var.memory_tip
-    parameterized_subject = var.message_subject == "" ? local.rule_subject : var.message_subject
-    parameterized_body    = var.message_body == "" ? local.rule_body : var.message_body
-  }
 
   rule {
     description           = "is too high > ${var.memory_threshold_critical}%"
@@ -202,6 +190,18 @@ EOF
     detect_label          = "CRIT"
     disabled              = coalesce(var.memory_disabled_critical, var.memory_disabled, var.detectors_disabled)
     notifications         = try(coalescelist(lookup(var.memory_notifications, "critical", []), var.notifications.critical), null)
+    runbook_url           = try(coalesce(var.memory_runbook_url, var.runbook_url), "")
+    tip                   = var.memory_tip
+    parameterized_subject = var.message_subject == "" ? local.rule_subject : var.message_subject
+    parameterized_body    = var.message_body == "" ? local.rule_body : var.message_body
+  }
+
+  rule {
+    description           = "is too high > ${var.memory_threshold_major}%"
+    severity              = "Major"
+    detect_label          = "MAJOR"
+    disabled              = coalesce(var.memory_disabled_major, var.memory_disabled, var.detectors_disabled)
+    notifications         = try(coalescelist(lookup(var.memory_notifications, "major", []), var.notifications.major), null)
     runbook_url           = try(coalesce(var.memory_runbook_url, var.runbook_url), "")
     tip                   = var.memory_tip
     parameterized_subject = var.message_subject == "" ? local.rule_subject : var.message_subject
