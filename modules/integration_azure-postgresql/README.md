f2e5ec7a257a1d7b32c9e9c305856171d08a5700
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2023-10-03 16:33:36 +0200

    Rework Azure PostgreSQL

--- modules/integration_azure-postgresql/README.md
+++ modules/integration_azure-postgresql/README.md
@@ -57,7 +57,7 @@ Note the following parameters:
 
 These 3 parameters alongs with all variables defined in [common-variables.tf](common-variables.tf) are common to all
 [modules](../) in this repository. Other variables, specific to this module, are available in
-[variables.tf](variables.tf).
+[variables-gen.tf](variables-gen.tf).
 In general, the default configuration "works" but all of these Terraform
 [variables](https://www.terraform.io/language/values/variables) make it possible to
 customize the detectors behavior to better fit your needs.
@@ -76,12 +76,12 @@ This module creates the following SignalFx detectors which could contain one or 
 |Detector|Critical|Major|Minor|Warning|Info|
 |---|---|---|---|---|---|
 |Azure PostgreSQL heartbeat|X|-|-|-|-|
-|Azure PostgreSQL CPU usage|X|X|-|-|-|
-|Azure PostgreSQL has no connection|X|-|-|-|-|
+|Azure PostgreSQL cpu usage|X|X|-|-|-|
+|Azure PostgreSQL active connections|X|-|-|-|-|
 |Azure PostgreSQL storage usage|X|X|-|-|-|
-|Azure PostgreSQL IO consumption|X|X|-|-|-|
-|Azure PostgreSQL memory usage |X|X|-|-|-|
-||X|X|-|-|-|
+|Azure PostgreSQL io consumption|X|X|-|-|-|
+|Azure PostgreSQL memory usage|X|X|-|-|-|
+|Azure PostgreSQL serverlog storage usage|X|X|-|-|-|
 
 ## How to collect required metrics?
 
@@ -115,4 +115,4 @@ Here is the list of required metrics for detectors in this module.
 * [Terraform SignalFx provider](https://registry.terraform.io/providers/splunk-terraform/signalfx/latest/docs)
 * [Terraform SignalFx detector](https://registry.terraform.io/providers/splunk-terraform/signalfx/latest/docs/resources/detector)
 * [Splunk Observability integrations](https://docs.splunk.com/Observability/gdi/get-data-in/integrations.html)
-* [Azure Monitor metrics](https://docs.microsoft.com/en-us/azure/azure-monitor/essentials/metrics-supported#microsoftdbforpostgresqlservers)
+* [Azure Monitor metrics](https://learn.microsoft.com/en-us/azure/azure-monitor/reference/supported-metrics/microsoft-dbforpostgresql-servers-metrics)
--- modules/integration_azure-postgresql/detectors-gen.tf
+++ modules/integration_azure-postgresql/detectors-gen.tf
@@ -6,11 +6,11 @@ resource "signalfx_detector" "heartbeat" {
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
   program_text = <<-EOF
-        from signalfx.detectors.not_reporting import not_reporting
-        base_filter = filter('resource_type', 'Microsoft.DB*orPostgreSQL/servers') and filter('primary_aggregation_type', 'true')
-        signal = data('network_bytes_ingress', filter=base_filter and ${module.filtering.signalflow})${var.heartbeat_aggregation_function}.publish('signal')
-        not_reporting.detector(stream=signal, resource_identifier=None, duration='${var.heartbeat_timeframe}', auto_resolve_after='${local.heartbeat_auto_resolve_after}').publish('CRIT')
-    EOF
+    from signalfx.detectors.not_reporting import not_reporting
+    base_filtering = filter('resource_type', 'Microsoft.DB*orPostgreSQL/servers') and filter('primary_aggregation_type', 'true')
+    signal = data('network_bytes_ingress', filter=base_filtering and ${module.filtering.signalflow})${var.heartbeat_aggregation_function}${var.heartbeat_transformation_function}.publish('signal')
+    not_reporting.detector(stream=signal, resource_identifier=None, duration='${var.heartbeat_timeframe}', auto_resolve_after='${local.heartbeat_auto_resolve_after}').publish('CRIT')
+EOF
 
   rule {
     description           = "has not reported in ${var.heartbeat_timeframe}"
@@ -28,18 +28,23 @@ resource "signalfx_detector" "heartbeat" {
 }
 
 resource "signalfx_detector" "cpu_usage" {
-  name = format("%s %s", local.detector_name_prefix, "Azure PostgreSQL CPU usage")
+  name = format("%s %s", local.detector_name_prefix, "Azure PostgreSQL cpu usage")
 
   authorized_writer_teams = var.authorized_writer_teams
   teams                   = try(coalescelist(var.teams, var.authorized_writer_teams), null)
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
+  viz_options {
+    label        = "signal"
+    value_suffix = "%"
+  }
+
   program_text = <<-EOF
-        base_filter = filter('resource_type', 'Microsoft.DB*orPostgreSQL/servers') and filter('primary_aggregation_type', 'true')
-        signal = data('cpu_percent', filter=base_filter and ${module.filtering.signalflow})${var.cpu_usage_aggregation_function}.publish('signal')
-        detect(when(signal > threshold(${var.cpu_usage_threshold_critical}), lasting="${var.cpu_usage_lasting_duration_critical}")).publish('CRIT')
-        detect(when(signal > threshold(${var.cpu_usage_threshold_major}), lasting="${var.cpu_usage_lasting_duration_major}") and (not when(signal > ${var.cpu_usage_threshold_critical}, lasting="${var.cpu_usage_lasting_duration_critical}"))).publish('MAJOR')
-    EOF
+    base_filtering = filter('resource_type', 'Microsoft.DB*orPostgreSQL/servers') and filter('primary_aggregation_type', 'true')
+    signal = data('cpu_percent', filter=base_filtering and ${module.filtering.signalflow})${var.cpu_usage_aggregation_function}${var.cpu_usage_transformation_function}.publish('signal')
+    detect(when(signal > ${var.cpu_usage_threshold_critical}, lasting=%{if var.cpu_usage_lasting_duration_critical == null}None%{else}'${var.cpu_usage_lasting_duration_critical}'%{endif}, at_least=${var.cpu_usage_at_least_percentage_critical})).publish('CRIT')
+    detect(when(signal > ${var.cpu_usage_threshold_major}, lasting=%{if var.cpu_usage_lasting_duration_major == null}None%{else}'${var.cpu_usage_lasting_duration_major}'%{endif}, at_least=${var.cpu_usage_at_least_percentage_major}) and (not when(signal > ${var.cpu_usage_threshold_critical}, lasting=%{if var.cpu_usage_lasting_duration_critical == null}None%{else}'${var.cpu_usage_lasting_duration_critical}'%{endif}, at_least=${var.cpu_usage_at_least_percentage_critical}))).publish('MAJOR')
+EOF
 
   rule {
     description           = "is too high > ${var.cpu_usage_threshold_critical}%"
@@ -68,31 +73,32 @@ resource "signalfx_detector" "cpu_usage" {
   max_delay = var.cpu_usage_max_delay
 }
 
-resource "signalfx_detector" "no_connection" {
-  name = format("%s %s", local.detector_name_prefix, "Azure PostgreSQL has no connection")
+resource "signalfx_detector" "active_connections" {
+  name = format("%s %s", local.detector_name_prefix, "Azure PostgreSQL active connections")
 
   authorized_writer_teams = var.authorized_writer_teams
   teams                   = try(coalescelist(var.teams, var.authorized_writer_teams), null)
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
   program_text = <<-EOF
-        base_filter = filter('resource_type', 'Microsoft.DB*orPostgreSQL/servers') and filter('primary_aggregation_type', 'true')
-        signal = data('active_connections', extrapolation="zero", filter=base_filter and ${module.filtering.signalflow})${var.no_connection_aggregation_function}.publish('signal')
-        detect(when(signal < threshold(1), lasting="${var.no_connection_lasting_duration_critical}")).publish('CRIT')
-    EOF
+    base_filtering = filter('resource_type', 'Microsoft.DB*orPostgreSQL/servers') and filter('primary_aggregation_type', 'true')
+    signal = data('active_connections', filter=base_filtering and ${module.filtering.signalflow}, extrapolation='zero')${var.active_connections_aggregation_function}${var.active_connections_transformation_function}.publish('signal')
+    detect(when(signal < ${var.active_connections_threshold_critical}, lasting=%{if var.active_connections_lasting_duration_critical == null}None%{else}'${var.active_connections_lasting_duration_critical}'%{endif}, at_least=${var.active_connections_at_least_percentage_critical})).publish('CRIT')
+EOF
 
   rule {
+    description           = "is too low < ${var.active_connections_threshold_critical}"
     severity              = "Critical"
     detect_label          = "CRIT"
-    disabled              = coalesce(var.no_connection_disabled_critical, var.no_connection_disabled, var.detectors_disabled)
-    notifications         = try(coalescelist(lookup(var.no_connection_notifications, "critical", []), var.notifications.critical), null)
-    runbook_url           = try(coalesce(var.no_connection_runbook_url, var.runbook_url), "")
-    tip                   = var.no_connection_tip
+    disabled              = coalesce(var.active_connections_disabled, var.detectors_disabled)
+    notifications         = try(coalescelist(lookup(var.active_connections_notifications, "critical", []), var.notifications.critical), null)
+    runbook_url           = try(coalesce(var.active_connections_runbook_url, var.runbook_url), "")
+    tip                   = var.active_connections_tip
     parameterized_subject = var.message_subject == "" ? local.rule_subject : var.message_subject
     parameterized_body    = var.message_body == "" ? local.rule_body : var.message_body
   }
 
-  max_delay = var.no_connection_max_delay
+  max_delay = var.active_connections_max_delay
 }
 
 resource "signalfx_detector" "storage_usage" {
@@ -102,12 +108,17 @@ resource "signalfx_detector" "storage_usage" {
   teams                   = try(coalescelist(var.teams, var.authorized_writer_teams), null)
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
+  viz_options {
+    label        = "signal"
+    value_suffix = "%"
+  }
+
   program_text = <<-EOF
-        base_filter = filter('resource_type', 'Microsoft.DB*orPostgreSQL/servers') and filter('primary_aggregation_type', 'true')
-        signal = data('storage_percent', filter=base_filter and ${module.filtering.signalflow})${var.storage_usage_aggregation_function}.publish('signal')
-        detect(when(signal > threshold(${var.storage_usage_threshold_critical}), lasting="${var.storage_usage_lasting_duration_critical}")).publish('CRIT')
-        detect(when(signal > threshold(${var.storage_usage_threshold_major}), lasting="${var.storage_usage_lasting_duration_major}") and (not when(signal > ${var.storage_usage_threshold_critical}, lasting="${var.storage_usage_lasting_duration_critical}"))).publish('MAJOR')
-    EOF
+    base_filtering = filter('resource_type', 'Microsoft.DB*orPostgreSQL/servers') and filter('primary_aggregation_type', 'true')
+    signal = data('storage_percent', filter=base_filtering and ${module.filtering.signalflow})${var.storage_usage_aggregation_function}${var.storage_usage_transformation_function}.publish('signal')
+    detect(when(signal > ${var.storage_usage_threshold_critical}, lasting=%{if var.storage_usage_lasting_duration_critical == null}None%{else}'${var.storage_usage_lasting_duration_critical}'%{endif}, at_least=${var.storage_usage_at_least_percentage_critical})).publish('CRIT')
+    detect(when(signal > ${var.storage_usage_threshold_major}, lasting=%{if var.storage_usage_lasting_duration_major == null}None%{else}'${var.storage_usage_lasting_duration_major}'%{endif}, at_least=${var.storage_usage_at_least_percentage_major}) and (not when(signal > ${var.storage_usage_threshold_critical}, lasting=%{if var.storage_usage_lasting_duration_critical == null}None%{else}'${var.storage_usage_lasting_duration_critical}'%{endif}, at_least=${var.storage_usage_at_least_percentage_critical}))).publish('MAJOR')
+EOF
 
   rule {
     description           = "is too high > ${var.storage_usage_threshold_critical}%"
@@ -137,18 +148,23 @@ resource "signalfx_detector" "storage_usage" {
 }
 
 resource "signalfx_detector" "io_consumption" {
-  name = format("%s %s", local.detector_name_prefix, "Azure PostgreSQL IO consumption")
+  name = format("%s %s", local.detector_name_prefix, "Azure PostgreSQL io consumption")
 
   authorized_writer_teams = var.authorized_writer_teams
   teams                   = try(coalescelist(var.teams, var.authorized_writer_teams), null)
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
+  viz_options {
+    label        = "signal"
+    value_suffix = "%"
+  }
+
   program_text = <<-EOF
-        base_filter = filter('resource_type', 'Microsoft.DB*orPostgreSQL/servers') and filter('primary_aggregation_type', 'true')
-        signal = data('io_consumption_percent', filter=base_filter and ${module.filtering.signalflow})${var.io_consumption_aggregation_function}.publish('signal')
-        detect(when(signal > threshold(${var.io_consumption_threshold_critical}), lasting="${var.io_consumption_lasting_duration_critical}")).publish('CRIT')
-        detect(when(signal > threshold(${var.io_consumption_threshold_major}), lasting="${var.io_consumption_lasting_duration_major}") and (not when(signal > ${var.io_consumption_threshold_critical}, lasting="${var.io_consumption_lasting_duration_critical}"))).publish('MAJOR')
-    EOF
+    base_filtering = filter('resource_type', 'Microsoft.DB*orPostgreSQL/servers') and filter('primary_aggregation_type', 'true')
+    signal = data('io_consumption_percent', filter=base_filtering and ${module.filtering.signalflow})${var.io_consumption_aggregation_function}${var.io_consumption_transformation_function}.publish('signal')
+    detect(when(signal > ${var.io_consumption_threshold_critical}, lasting=%{if var.io_consumption_lasting_duration_critical == null}None%{else}'${var.io_consumption_lasting_duration_critical}'%{endif}, at_least=${var.io_consumption_at_least_percentage_critical})).publish('CRIT')
+    detect(when(signal > ${var.io_consumption_threshold_major}, lasting=%{if var.io_consumption_lasting_duration_major == null}None%{else}'${var.io_consumption_lasting_duration_major}'%{endif}, at_least=${var.io_consumption_at_least_percentage_major}) and (not when(signal > ${var.io_consumption_threshold_critical}, lasting=%{if var.io_consumption_lasting_duration_critical == null}None%{else}'${var.io_consumption_lasting_duration_critical}'%{endif}, at_least=${var.io_consumption_at_least_percentage_critical}))).publish('MAJOR')
+EOF
 
   rule {
     description           = "is too high > ${var.io_consumption_threshold_critical}%"
@@ -178,18 +194,23 @@ resource "signalfx_detector" "io_consumption" {
 }
 
 resource "signalfx_detector" "memory_usage" {
-  name = format("%s %s", local.detector_name_prefix, "Azure PostgreSQL memory usage ")
+  name = format("%s %s", local.detector_name_prefix, "Azure PostgreSQL memory usage")
 
   authorized_writer_teams = var.authorized_writer_teams
   teams                   = try(coalescelist(var.teams, var.authorized_writer_teams), null)
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
+  viz_options {
+    label        = "signal"
+    value_suffix = "%"
+  }
+
   program_text = <<-EOF
-        base_filter = filter('resource_type', 'Microsoft.DB*orPostgreSQL/servers') and filter('primary_aggregation_type', 'true')
-        signal = data('memory_percent', filter=base_filter and ${module.filtering.signalflow})${var.memory_usage_aggregation_function}.publish('signal')
-        detect(when(signal > threshold(${var.memory_usage_threshold_critical}), lasting="${var.memory_usage_lasting_duration_critical}")).publish('CRIT')
-        detect(when(signal > threshold(${var.memory_usage_threshold_major}), lasting="${var.memory_usage_lasting_duration_major}") and (not when(signal > ${var.memory_usage_threshold_critical}, lasting="${var.memory_usage_lasting_duration_critical}"))).publish('MAJOR')
-    EOF
+    base_filtering = filter('resource_type', 'Microsoft.DB*orPostgreSQL/servers') and filter('primary_aggregation_type', 'true')
+    signal = data('memory_percent', filter=base_filtering and ${module.filtering.signalflow})${var.memory_usage_aggregation_function}${var.memory_usage_transformation_function}.publish('signal')
+    detect(when(signal > ${var.memory_usage_threshold_critical}, lasting=%{if var.memory_usage_lasting_duration_critical == null}None%{else}'${var.memory_usage_lasting_duration_critical}'%{endif}, at_least=${var.memory_usage_at_least_percentage_critical})).publish('CRIT')
+    detect(when(signal > ${var.memory_usage_threshold_major}, lasting=%{if var.memory_usage_lasting_duration_major == null}None%{else}'${var.memory_usage_lasting_duration_major}'%{endif}, at_least=${var.memory_usage_at_least_percentage_major}) and (not when(signal > ${var.memory_usage_threshold_critical}, lasting=%{if var.memory_usage_lasting_duration_critical == null}None%{else}'${var.memory_usage_lasting_duration_critical}'%{endif}, at_least=${var.memory_usage_at_least_percentage_critical}))).publish('MAJOR')
+EOF
 
   rule {
     description           = "is too high > ${var.memory_usage_threshold_critical}%"
@@ -219,14 +240,23 @@ resource "signalfx_detector" "memory_usage" {
 }
 
 resource "signalfx_detector" "serverlog_storage_usage" {
-  name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure PostgreSQL serverlog storage usage"
+  name = format("%s %s", local.detector_name_prefix, "Azure PostgreSQL serverlog storage usage")
+
+  authorized_writer_teams = var.authorized_writer_teams
+  teams                   = try(coalescelist(var.teams, var.authorized_writer_teams), null)
+  tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
+
+  viz_options {
+    label        = "signal"
+    value_suffix = "%"
+  }
 
   program_text = <<-EOF
-        base_filter = filter('resource_type', 'Microsoft.DB*orPostgreSQL/servers') and filter('primary_aggregation_type', 'true')
-        signal = data('serverlog_storage_percent', filter=base_filter and ${module.filtering.signalflow})${var.serverlog_storage_usage_aggregation_function}.publish('signal')
-        detect(when(signal > threshold(${var.serverlog_storage_usage_threshold_critical}), lasting="${var.serverlog_storage_usage_lasting_duration_critical}")).publish('CRIT')
-        detect(when(signal > threshold(${var.serverlog_storage_usage_threshold_major}), lasting="${var.serverlog_storage_usage_lasting_duration_major}") and (not when(signal > ${var.serverlog_storage_usage_threshold_critical}, lasting="${var.serverlog_storage_usage_lasting_duration_critical}"))).publish('MAJOR')
-    EOF
+    base_filtering = filter('resource_type', 'Microsoft.DB*orPostgreSQL/servers') and filter('primary_aggregation_type', 'true')
+    signal = data('serverlog_storage_percent', filter=base_filtering and ${module.filtering.signalflow})${var.serverlog_storage_usage_aggregation_function}${var.serverlog_storage_usage_transformation_function}.publish('signal')
+    detect(when(signal > ${var.serverlog_storage_usage_threshold_critical}, lasting=%{if var.serverlog_storage_usage_lasting_duration_critical == null}None%{else}'${var.serverlog_storage_usage_lasting_duration_critical}'%{endif}, at_least=${var.serverlog_storage_usage_at_least_percentage_critical})).publish('CRIT')
+    detect(when(signal > ${var.serverlog_storage_usage_threshold_major}, lasting=%{if var.serverlog_storage_usage_lasting_duration_major == null}None%{else}'${var.serverlog_storage_usage_lasting_duration_major}'%{endif}, at_least=${var.serverlog_storage_usage_at_least_percentage_major}) and (not when(signal > ${var.serverlog_storage_usage_threshold_critical}, lasting=%{if var.serverlog_storage_usage_lasting_duration_critical == null}None%{else}'${var.serverlog_storage_usage_lasting_duration_critical}'%{endif}, at_least=${var.serverlog_storage_usage_at_least_percentage_critical}))).publish('MAJOR')
+EOF
 
   rule {
     description           = "is too high > ${var.serverlog_storage_usage_threshold_critical}%"
