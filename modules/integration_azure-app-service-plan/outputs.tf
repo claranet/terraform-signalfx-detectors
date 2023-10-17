17f3db4aa53787d906a65f01db6481e1f2c7f7c1
Spi <BzSpi@users.noreply.github.com>
2023-10-17 08:52:58 +0200

    Rework azure app service (#495)
    * Rework Azure App Service
    
    * Rework Azure App Service - fix

--- modules/integration_azure-app-service-plan/outputs.tf
+++ modules/integration_azure-app-service-plan/outputs.tf
@@ -1,6 +1,6 @@
-output "cpu_percentage" {
-  description = "Detector resource for cpu_percentage"
-  value       = signalfx_detector.cpu_percentage
+output "cpu" {
+  description = "Detector resource for cpu"
+  value       = signalfx_detector.cpu
 }
 
 output "heartbeat" {
@@ -8,8 +8,8 @@ output "heartbeat" {
   value       = signalfx_detector.heartbeat
 }
 
-output "memory_percentage" {
-  description = "Detector resource for memory_percentage"
-  value       = signalfx_detector.memory_percentage
+output "memory" {
+  description = "Detector resource for memory"
+  value       = signalfx_detector.memory
 }
 
