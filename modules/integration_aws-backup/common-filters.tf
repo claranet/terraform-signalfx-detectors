62fafbfdc245c0ab3f6f947feddd41c71fc92ba4
Nicolas Senaud <nicolas.senaud@fr.clara.net>
2023-10-19 13:25:14 +0200

    fix(aws-backup): avoid duplicated and ill-routed alerts

--- modules/integration_aws-backup/common-filters.tf
+++ modules/integration_aws-backup/common-filters.tf
@@ -1,3 +1,11 @@
+# Common filters at ../../common/module/filters-integration-aws.tf are not used
+# with AWS Backup because recovery points (snapshots) and jobs are unlikely to
+# be tagged with `env` and `sfx_monitored`.
+#
+# We are also assuming that we want to be notified for every backup vault and
+# resource types. `match_missing=False` is required to avoid duplicated signal
+# and alerts, since CloudWatch returns aggregated metric, and per vault and
+# resource type.
 locals {
-  filters = "filter('ResourceType', '*')"
+  filters = format("filter('namespace', 'AWS/Backup') and filter('stat', 'sum') and filter('BackupVaultName', '*', match_missing=False) and filter('ResourceType', '*', match_missing=False) and filter('aws_account_id', '%s')", var.aws_account_id)
 }
--- modules/integration_aws-backup/detectors-gen.tf
+++ modules/integration_aws-backup/detectors-gen.tf
@@ -6,7 +6,7 @@ resource "signalfx_detector" "backup_failed" {
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
   program_text = <<-EOF
-    base_filtering = filter('namespace', 'AWS/Backup') and filter('stat', 'sum')
+    base_filtering = filter('namespace', 'AWS/Backup') and filter('stat', 'sum') and filter('BackupVaultName', '*', match_missing=False) and filter('ResourceType', '*', match_missing=False)
     signal = data('NumberOfBackupJobsFailed', filter=base_filtering and ${module.filtering.signalflow})${var.backup_failed_aggregation_function}${var.backup_failed_transformation_function}.publish('signal')
     detect(when(signal > ${var.backup_failed_threshold_critical}, lasting=%{if var.backup_failed_lasting_duration_critical == null}None%{else}'${var.backup_failed_lasting_duration_critical}'%{endif}, at_least=${var.backup_failed_at_least_percentage_critical})).publish('CRIT')
 EOF
@@ -34,7 +34,7 @@ resource "signalfx_detector" "backup_job_expired" {
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
   program_text = <<-EOF
-    base_filtering = filter('namespace', 'AWS/Backup') and filter('stat', 'sum')
+    base_filtering = filter('namespace', 'AWS/Backup') and filter('stat', 'sum') and filter('BackupVaultName', '*', match_missing=False) and filter('ResourceType', '*', match_missing=False)
     signal = data('NumberOfBackupJobsExpired', filter=base_filtering and ${module.filtering.signalflow}, extrapolation='zero')${var.backup_job_expired_aggregation_function}${var.backup_job_expired_transformation_function}.publish('signal')
     detect(when(signal > ${var.backup_job_expired_threshold_critical}, lasting=%{if var.backup_job_expired_lasting_duration_critical == null}None%{else}'${var.backup_job_expired_lasting_duration_critical}'%{endif}, at_least=${var.backup_job_expired_at_least_percentage_critical})).publish('CRIT')
 EOF
@@ -90,7 +90,7 @@ resource "signalfx_detector" "backup_successful" {
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
   program_text = <<-EOF
-    base_filtering = filter('namespace', 'AWS/Backup') and filter('stat', 'sum')
+    base_filtering = filter('namespace', 'AWS/Backup') and filter('stat', 'sum') and filter('BackupVaultName', '*', match_missing=False) and filter('ResourceType', '*', match_missing=False)
     created = data('NumberOfBackupJobsCreated', filter=base_filtering and ${module.filtering.signalflow}, extrapolation='zero')${var.backup_successful_aggregation_function}${var.backup_successful_transformation_function}
     completed = data('NumberOfBackupJobsCompleted', filter=base_filtering and ${module.filtering.signalflow}, extrapolation='zero')${var.backup_successful_aggregation_function}${var.backup_successful_transformation_function}
     signal = (created-completed).publish('signal')
@@ -120,7 +120,7 @@ resource "signalfx_detector" "backup_rp_partial" {
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
   program_text = <<-EOF
-    base_filtering = filter('namespace', 'AWS/Backup') and filter('stat', 'sum')
+    base_filtering = filter('namespace', 'AWS/Backup') and filter('stat', 'sum') and filter('BackupVaultName', '*', match_missing=False) and filter('ResourceType', '*', match_missing=False)
     signal = data('NumberOfRecoveryPointsPartial', filter=base_filtering and ${module.filtering.signalflow})${var.backup_rp_partial_aggregation_function}${var.backup_rp_partial_transformation_function}.publish('signal')
     detect(when(signal > ${var.backup_rp_partial_threshold_minor}, lasting=%{if var.backup_rp_partial_lasting_duration_minor == null}None%{else}'${var.backup_rp_partial_lasting_duration_minor}'%{endif}, at_least=${var.backup_rp_partial_at_least_percentage_minor})).publish('MINOR')
 EOF
@@ -148,7 +148,7 @@ resource "signalfx_detector" "backup_rp_expired" {
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
   program_text = <<-EOF
-    base_filtering = filter('namespace', 'AWS/Backup') and filter('stat', 'sum')
+    base_filtering = filter('namespace', 'AWS/Backup') and filter('stat', 'sum') and filter('BackupVaultName', '*', match_missing=False) and filter('ResourceType', '*', match_missing=False)
     signal = data('NumberOfRecoveryPointsExpired', filter=base_filtering and ${module.filtering.signalflow})${var.backup_rp_expired_aggregation_function}${var.backup_rp_expired_transformation_function}.publish('signal')
     detect(when(signal > ${var.backup_rp_expired_threshold_major}, lasting=%{if var.backup_rp_expired_lasting_duration_major == null}None%{else}'${var.backup_rp_expired_lasting_duration_major}'%{endif}, at_least=${var.backup_rp_expired_at_least_percentage_major})).publish('MAJOR')
 EOF
