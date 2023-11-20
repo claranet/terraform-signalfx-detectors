93ed622c92a109ce3c6a05cff03b9c6624f5ef61
Gauthier AMPE <gauthier.ampe@fr.clara.net>
2023-11-20 17:38:31 +0100

    Create detector

--- modules/integration_azure-load-balancer/detectors-gen.tf
+++ modules/integration_azure-load-balancer/detectors-gen.tf
@@ -27,3 +27,48 @@ EOF
   max_delay = var.heartbeat_max_delay
 }
 
+resource "signalfx_detector" "healthprobe" {
+  name = format("%s %s", local.detector_name_prefix, "Azure Load Balancer healthprobe")
+
+  authorized_writer_teams = var.authorized_writer_teams
+  teams                   = try(coalescelist(var.teams, var.authorized_writer_teams), null)
+  tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
+
+  viz_options {
+    label        = "signal"
+    value_suffix = "%"
+  }
+
+  program_text = <<-EOF
+    base_filtering = filter('resource_type', 'Microsoft.Network/loadBalancers') and filter('primary_aggregation_type', 'true')
+    signal = data('DipAvailability', filter=base_filtering and ${module.filtering.signalflow})${var.healthprobe_aggregation_function}${var.healthprobe_transformation_function}.publish('signal')
+    detect(when(signal < ${var.healthprobe_threshold_critical}, lasting=%{if var.healthprobe_lasting_duration_critical == null}None%{else}'${var.healthprobe_lasting_duration_critical}'%{endif}, at_least=${var.healthprobe_at_least_percentage_critical})).publish('CRIT')
+    detect(when(signal < ${var.healthprobe_threshold_major}, lasting=%{if var.healthprobe_lasting_duration_major == null}None%{else}'${var.healthprobe_lasting_duration_major}'%{endif}, at_least=${var.healthprobe_at_least_percentage_major}) and (not when(signal < ${var.healthprobe_threshold_critical}, lasting=%{if var.healthprobe_lasting_duration_critical == null}None%{else}'${var.healthprobe_lasting_duration_critical}'%{endif}, at_least=${var.healthprobe_at_least_percentage_critical}))).publish('MAJOR')
+EOF
+
+  rule {
+    description           = "is too low < ${var.healthprobe_threshold_critical}%"
+    severity              = "Critical"
+    detect_label          = "CRIT"
+    disabled              = coalesce(var.healthprobe_disabled_critical, var.healthprobe_disabled, var.detectors_disabled)
+    notifications         = try(coalescelist(lookup(var.healthprobe_notifications, "critical", []), var.notifications.critical), null)
+    runbook_url           = try(coalesce(var.healthprobe_runbook_url, var.runbook_url), "")
+    tip                   = var.healthprobe_tip
+    parameterized_subject = var.message_subject == "" ? local.rule_subject : var.message_subject
+    parameterized_body    = var.message_body == "" ? local.rule_body : var.message_body
+  }
+
+  rule {
+    description           = "is too low < ${var.healthprobe_threshold_major}%"
+    severity              = "Major"
+    detect_label          = "MAJOR"
+    disabled              = coalesce(var.healthprobe_disabled_major, var.healthprobe_disabled, var.detectors_disabled)
+    notifications         = try(coalescelist(lookup(var.healthprobe_notifications, "major", []), var.notifications.major), null)
+    runbook_url           = try(coalesce(var.healthprobe_runbook_url, var.runbook_url), "")
+    tip                   = var.healthprobe_tip
+    parameterized_subject = var.message_subject == "" ? local.rule_subject : var.message_subject
+    parameterized_body    = var.message_body == "" ? local.rule_body : var.message_body
+  }
+
+  max_delay = var.healthprobe_max_delay
+}
--- modules/integration_azure-load-balancer/variables-gen.tf
+++ modules/integration_azure-load-balancer/variables-gen.tf
@@ -48,3 +48,93 @@ variable "heartbeat_timeframe" {
   default     = "10m"
 }
 
+# healthprobe detector
+
+variable "healthprobe_notifications" {
+  description = "Notification recipients list per severity overridden for healthprobe detector"
+  type        = map(list(string))
+  default     = {}
+}
+
+variable "healthprobe_aggregation_function" {
+  description = "Aggregation function and group by for healthprobe detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ".mean(by=['BackendIPAddress', 'azure_resource_name', 'azure_resource_group_name', 'azure_region'])"
+}
+
+variable "healthprobe_transformation_function" {
+  description = "Transformation function for healthprobe detector (i.e. \".mean(over='5m')\")"
+  type        = string
+  default     = ""
+}
+
+variable "healthprobe_max_delay" {
+  description = "Enforce max delay for healthprobe detector (use \"0\" or \"null\" for \"Auto\")"
+  type        = number
+  default     = null
+}
+
+variable "healthprobe_tip" {
+  description = "Suggested first course of action or any note useful for incident handling"
+  type        = string
+  default     = ""
+}
+
+variable "healthprobe_runbook_url" {
+  description = "URL like SignalFx dashboard or wiki page which can help to troubleshoot the incident cause"
+  type        = string
+  default     = ""
+}
+
+variable "healthprobe_disabled" {
+  description = "Disable all alerting rules for healthprobe detector"
+  type        = bool
+  default     = null
+}
+
+variable "healthprobe_disabled_critical" {
+  description = "Disable critical alerting rule for healthprobe detector"
+  type        = bool
+  default     = null
+}
+
+variable "healthprobe_disabled_major" {
+  description = "Disable major alerting rule for healthprobe detector"
+  type        = bool
+  default     = null
+}
+
+variable "healthprobe_threshold_critical" {
+  description = "Critical threshold for healthprobe detector in %"
+  type        = number
+  default     = 100
+}
+
+variable "healthprobe_lasting_duration_critical" {
+  description = "Minimum duration that conditions must be true before raising alert"
+  type        = string
+  default     = "15m"
+}
+
+variable "healthprobe_at_least_percentage_critical" {
+  description = "Percentage of lasting that conditions must be true before raising alert (>= 0.0 and <= 1.0)"
+  type        = number
+  default     = 1
+}
+variable "healthprobe_threshold_major" {
+  description = "Major threshold for healthprobe detector in %"
+  type        = number
+  default     = 50
+}
+
+variable "healthprobe_lasting_duration_major" {
+  description = "Minimum duration that conditions must be true before raising alert"
+  type        = string
+  default     = "15m"
+}
+
+variable "healthprobe_at_least_percentage_major" {
+  description = "Percentage of lasting that conditions must be true before raising alert (>= 0.0 and <= 1.0)"
+  type        = number
+  default     = 1
+}
