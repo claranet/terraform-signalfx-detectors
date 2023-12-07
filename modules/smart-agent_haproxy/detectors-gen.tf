54a31080edc00b40d8ea22d9c0d49d995c016879
Jean-Baptiste Simillon <jb.simillon@fr.clara.net>
2023-12-07 14:12:56 +0100

    feat : convert module smart-agent_haproxy to generator format

--- modules/smart-agent_haproxy/detectors-gen.tf
+++ modules/smart-agent_haproxy/detectors-gen.tf
@@ -7,7 +7,7 @@ resource "signalfx_detector" "heartbeat" {
 
   program_text = <<-EOF
     from signalfx.detectors.not_reporting import not_reporting
-    signal = data('haproxy_session_current', filter=${local.not_running_vm_filters} and ${module.filtering.signalflow})${var.heartbeat_aggregation_function}.publish('signal')
+    signal = data('haproxy_session_current', filter=${local.not_running_vm_filters} and ${module.filtering.signalflow})${var.heartbeat_aggregation_function}${var.heartbeat_transformation_function}.publish('signal')
     not_reporting.detector(stream=signal, resource_identifier=None, duration='${var.heartbeat_timeframe}', auto_resolve_after='${local.heartbeat_auto_resolve_after}').publish('CRIT')
 EOF
 
@@ -34,15 +34,16 @@ resource "signalfx_detector" "server_status" {
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
   program_text = <<-EOF
-    signal = data('haproxy_status', filter=filter('type', '2') and ${module.filtering.signalflow})${var.server_status_aggregation_function}${var.server_status_transformation_function}.publish('signal')
-    detect(when(signal < 1)).publish('CRIT')
+    base_filtering = filter('type', '2')
+    signal = data('haproxy_status', filter=base_filtering and ${module.filtering.signalflow})${var.server_status_aggregation_function}${var.server_status_transformation_function}.publish('signal')
+    detect(when(signal < ${var.server_status_threshold_critical}%{if var.server_status_lasting_duration_critical != null}, lasting='${var.server_status_lasting_duration_critical}', at_least=${var.server_status_at_least_percentage_critical}%{endif})).publish('CRIT')
 EOF
 
   rule {
-    description           = "is down or in maintenance"
+    description           = "is too low < ${var.server_status_threshold_critical}"
     severity              = "Critical"
     detect_label          = "CRIT"
-    disabled              = coalesce(var.server_status_disabled_critical, var.server_status_disabled, var.detectors_disabled)
+    disabled              = coalesce(var.server_status_disabled, var.detectors_disabled)
     notifications         = try(coalescelist(lookup(var.server_status_notifications, "critical", []), var.notifications.critical), null)
     runbook_url           = try(coalesce(var.server_status_runbook_url, var.runbook_url), "")
     tip                   = var.server_status_tip
@@ -61,15 +62,16 @@ resource "signalfx_detector" "backend_status" {
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
   program_text = <<-EOF
-    signal = data('haproxy_status', filter=filter('type', '1') and ${module.filtering.signalflow})${var.backend_status_aggregation_function}${var.backend_status_transformation_function}.publish('signal')
-    detect(when(signal < 1)).publish('CRIT')
+    base_filtering = filter('type', '1')
+    signal = data('haproxy_status', filter=base_filtering and ${module.filtering.signalflow})${var.backend_status_aggregation_function}${var.backend_status_transformation_function}.publish('signal')
+    detect(when(signal < ${var.backend_status_threshold_critical}%{if var.backend_status_lasting_duration_critical != null}, lasting='${var.backend_status_lasting_duration_critical}', at_least=${var.backend_status_at_least_percentage_critical}%{endif})).publish('CRIT')
 EOF
 
   rule {
-    description           = "is down (no available server left)"
+    description           = "is too low < ${var.backend_status_threshold_critical}"
     severity              = "Critical"
     detect_label          = "CRIT"
-    disabled              = coalesce(var.backend_status_disabled_critical, var.backend_status_disabled, var.detectors_disabled)
+    disabled              = coalesce(var.backend_status_disabled, var.detectors_disabled)
     notifications         = try(coalescelist(lookup(var.backend_status_notifications, "critical", []), var.notifications.critical), null)
     runbook_url           = try(coalesce(var.backend_status_runbook_url, var.runbook_url), "")
     tip                   = var.backend_status_tip
@@ -91,12 +93,12 @@ resource "signalfx_detector" "session_limit" {
     A = data('haproxy_session_current', filter=filter('type', '0', '2') and ${module.filtering.signalflow})${var.session_limit_aggregation_function}${var.session_limit_transformation_function}
     B = data('haproxy_session_limit', filter=${module.filtering.signalflow})${var.session_limit_aggregation_function}${var.session_limit_transformation_function}
     signal = (A/B).scale(100).publish('signal')
-    detect(when(signal > ${var.session_limit_threshold_critical})).publish('CRIT')
-    detect(when(signal > ${var.session_limit_threshold_major}) and (not when(signal > ${var.session_limit_threshold_critical}))).publish('MAJOR')
+    detect(when(signal > ${var.session_limit_threshold_critical}%{if var.session_limit_lasting_duration_critical != null}, lasting='${var.session_limit_lasting_duration_critical}', at_least=${var.session_limit_at_least_percentage_critical}%{endif})).publish('CRIT')
+    detect(when(signal > ${var.session_limit_threshold_major}%{if var.session_limit_lasting_duration_major != null}, lasting='${var.session_limit_lasting_duration_major}', at_least=${var.session_limit_at_least_percentage_major}%{endif}) and (not when(signal > ${var.session_limit_threshold_critical}%{if var.session_limit_lasting_duration_critical != null}, lasting='${var.session_limit_lasting_duration_critical}', at_least=${var.session_limit_at_least_percentage_critical}%{endif}))).publish('MAJOR')
 EOF
 
   rule {
-    description           = "is approaching the limit > ${var.session_limit_threshold_critical}%"
+    description           = "is too high > ${var.session_limit_threshold_critical}"
     severity              = "Critical"
     detect_label          = "CRIT"
     disabled              = coalesce(var.session_limit_disabled_critical, var.session_limit_disabled, var.detectors_disabled)
@@ -108,7 +110,7 @@ EOF
   }
 
   rule {
-    description           = "is approaching the limit > ${var.session_limit_threshold_major}%"
+    description           = "is too high > ${var.session_limit_threshold_major}"
     severity              = "Major"
     detect_label          = "MAJOR"
     disabled              = coalesce(var.session_limit_disabled_major, var.session_limit_disabled, var.detectors_disabled)
@@ -133,12 +135,12 @@ resource "signalfx_detector" "http_5xx_response" {
     A = data('haproxy_response_5xx', filter=${module.filtering.signalflow}, rollup='delta')${var.http_5xx_response_aggregation_function}${var.http_5xx_response_transformation_function}
     B = data('haproxy_request_total', filter=${module.filtering.signalflow}, rollup='delta')${var.http_5xx_response_aggregation_function}${var.http_5xx_response_transformation_function}
     signal = (A/B).scale(100).publish('signal')
-    detect(when(signal > ${var.http_5xx_response_threshold_critical})).publish('CRIT')
-    detect(when(signal > ${var.http_5xx_response_threshold_major}) and (not when(signal > ${var.http_5xx_response_threshold_critical}))).publish('MAJOR')
+    detect(when(signal > ${var.http_5xx_response_threshold_critical}%{if var.http_5xx_response_lasting_duration_critical != null}, lasting='${var.http_5xx_response_lasting_duration_critical}', at_least=${var.http_5xx_response_at_least_percentage_critical}%{endif})).publish('CRIT')
+    detect(when(signal > ${var.http_5xx_response_threshold_major}%{if var.http_5xx_response_lasting_duration_major != null}, lasting='${var.http_5xx_response_lasting_duration_major}', at_least=${var.http_5xx_response_at_least_percentage_major}%{endif}) and (not when(signal > ${var.http_5xx_response_threshold_critical}%{if var.http_5xx_response_lasting_duration_critical != null}, lasting='${var.http_5xx_response_lasting_duration_critical}', at_least=${var.http_5xx_response_at_least_percentage_critical}%{endif}))).publish('MAJOR')
 EOF
 
   rule {
-    description           = "is too high > ${var.http_5xx_response_threshold_critical}%"
+    description           = "is too high > ${var.http_5xx_response_threshold_critical}"
     severity              = "Critical"
     detect_label          = "CRIT"
     disabled              = coalesce(var.http_5xx_response_disabled_critical, var.http_5xx_response_disabled, var.detectors_disabled)
@@ -150,7 +152,7 @@ EOF
   }
 
   rule {
-    description           = "is too high > ${var.http_5xx_response_threshold_major}%"
+    description           = "is too high > ${var.http_5xx_response_threshold_major}"
     severity              = "Major"
     detect_label          = "MAJOR"
     disabled              = coalesce(var.http_5xx_response_disabled_major, var.http_5xx_response_disabled, var.detectors_disabled)
@@ -175,13 +177,13 @@ resource "signalfx_detector" "http_4xx_response" {
     A = data('haproxy_response_4xx', filter=${module.filtering.signalflow}, rollup='delta')${var.http_4xx_response_aggregation_function}${var.http_4xx_response_transformation_function}
     B = data('haproxy_request_total', filter=${module.filtering.signalflow}, rollup='delta')${var.http_4xx_response_aggregation_function}${var.http_4xx_response_transformation_function}
     signal = (A/B).scale(100).publish('signal')
-    detect(when(signal > ${var.http_4xx_response_threshold_critical})).publish('CRIT')
-    detect(when(signal > ${var.http_4xx_response_threshold_major}) and (not when(signal > ${var.http_4xx_response_threshold_critical}))).publish('MAJOR')
-    detect(when(signal > ${var.http_4xx_response_threshold_minor}) and (not when(signal > ${var.http_4xx_response_threshold_major}))).publish('MINOR')
+    detect(when(signal > ${var.http_4xx_response_threshold_critical}%{if var.http_4xx_response_lasting_duration_critical != null}, lasting='${var.http_4xx_response_lasting_duration_critical}', at_least=${var.http_4xx_response_at_least_percentage_critical}%{endif})).publish('CRIT')
+    detect(when(signal > ${var.http_4xx_response_threshold_major}%{if var.http_4xx_response_lasting_duration_major != null}, lasting='${var.http_4xx_response_lasting_duration_major}', at_least=${var.http_4xx_response_at_least_percentage_major}%{endif}) and (not when(signal > ${var.http_4xx_response_threshold_critical}%{if var.http_4xx_response_lasting_duration_critical != null}, lasting='${var.http_4xx_response_lasting_duration_critical}', at_least=${var.http_4xx_response_at_least_percentage_critical}%{endif}))).publish('MAJOR')
+    detect(when(signal > ${var.http_4xx_response_threshold_minor}%{if var.http_4xx_response_lasting_duration_minor != null}, lasting='${var.http_4xx_response_lasting_duration_minor}', at_least=${var.http_4xx_response_at_least_percentage_minor}%{endif}) and (not when(signal > ${var.http_4xx_response_threshold_major}%{if var.http_4xx_response_lasting_duration_major != null}, lasting='${var.http_4xx_response_lasting_duration_major}', at_least=${var.http_4xx_response_at_least_percentage_major}%{endif}))).publish('MINOR')
 EOF
 
   rule {
-    description           = "is too high > ${var.http_4xx_response_threshold_critical}%"
+    description           = "is too high > ${var.http_4xx_response_threshold_critical}"
     severity              = "Critical"
     detect_label          = "CRIT"
     disabled              = coalesce(var.http_4xx_response_disabled_critical, var.http_4xx_response_disabled, var.detectors_disabled)
@@ -193,7 +195,7 @@ EOF
   }
 
   rule {
-    description           = "is too high > ${var.http_4xx_response_threshold_major}%"
+    description           = "is too high > ${var.http_4xx_response_threshold_major}"
     severity              = "Major"
     detect_label          = "MAJOR"
     disabled              = coalesce(var.http_4xx_response_disabled_major, var.http_4xx_response_disabled, var.detectors_disabled)
@@ -205,7 +207,7 @@ EOF
   }
 
   rule {
-    description           = "is too high > ${var.http_4xx_response_threshold_minor}%"
+    description           = "is too high > ${var.http_4xx_response_threshold_minor}"
     severity              = "Minor"
     detect_label          = "MINOR"
     disabled              = coalesce(var.http_4xx_response_disabled_minor, var.http_4xx_response_disabled, var.detectors_disabled)
@@ -218,3 +220,4 @@ EOF
 
   max_delay = var.http_4xx_response_max_delay
 }
+
---
+++ modules/smart-agent_haproxy/variables-gen.tf
@@ -0,0 +1,465 @@
+# heartbeat detector
+
+variable "heartbeat_notifications" {
+  description = "Notification recipients list per severity overridden for heartbeat detector"
+  type        = map(list(string))
+  default     = {}
+}
+
+variable "heartbeat_aggregation_function" {
+  description = "Aggregation function and group by for heartbeat detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ""
+}
+
+variable "heartbeat_transformation_function" {
+  description = "Transformation function for heartbeat detector (i.e. \".mean(over='5m')\")"
+  type        = string
+  default     = ""
+}
+
+variable "heartbeat_max_delay" {
+  description = "Enforce max delay for heartbeat detector (use \"0\" or \"null\" for \"Auto\")"
+  type        = number
+  default     = 900
+}
+
+variable "heartbeat_tip" {
+  description = "Suggested first course of action or any note useful for incident handling"
+  type        = string
+  default     = ""
+}
+
+variable "heartbeat_runbook_url" {
+  description = "URL like SignalFx dashboard or wiki page which can help to troubleshoot the incident cause"
+  type        = string
+  default     = ""
+}
+
+variable "heartbeat_disabled" {
+  description = "Disable all alerting rules for heartbeat detector"
+  type        = bool
+  default     = null
+}
+
+variable "heartbeat_timeframe" {
+  description = "Timeframe for heartbeat detector (i.e. \"10m\")"
+  type        = string
+  default     = "10m"
+}
+
+# server_status detector
+
+variable "server_status_notifications" {
+  description = "Notification recipients list per severity overridden for server_status detector"
+  type        = map(list(string))
+  default     = {}
+}
+
+variable "server_status_aggregation_function" {
+  description = "Aggregation function and group by for server_status detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ""
+}
+
+variable "server_status_transformation_function" {
+  description = "Transformation function for server_status detector (i.e. \".mean(over='5m')\")"
+  type        = string
+  default     = ".max(over='5m')"
+}
+
+variable "server_status_max_delay" {
+  description = "Enforce max delay for server_status detector (use \"0\" or \"null\" for \"Auto\")"
+  type        = number
+  default     = null
+}
+
+variable "server_status_tip" {
+  description = "Suggested first course of action or any note useful for incident handling"
+  type        = string
+  default     = ""
+}
+
+variable "server_status_runbook_url" {
+  description = "URL like SignalFx dashboard or wiki page which can help to troubleshoot the incident cause"
+  type        = string
+  default     = ""
+}
+
+variable "server_status_disabled" {
+  description = "Disable all alerting rules for server_status detector"
+  type        = bool
+  default     = null
+}
+
+variable "server_status_threshold_critical" {
+  description = "Critical threshold for server_status detector"
+  type        = number
+  default     = 1
+}
+
+variable "server_status_lasting_duration_critical" {
+  description = "Minimum duration that conditions must be true before raising alert"
+  type        = string
+  default     = null
+}
+
+variable "server_status_at_least_percentage_critical" {
+  description = "Percentage of lasting that conditions must be true before raising alert (>= 0.0 and <= 1.0)"
+  type        = number
+  default     = 1
+}
+# backend_status detector
+
+variable "backend_status_notifications" {
+  description = "Notification recipients list per severity overridden for backend_status detector"
+  type        = map(list(string))
+  default     = {}
+}
+
+variable "backend_status_aggregation_function" {
+  description = "Aggregation function and group by for backend_status detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ""
+}
+
+variable "backend_status_transformation_function" {
+  description = "Transformation function for backend_status detector (i.e. \".mean(over='5m')\")"
+  type        = string
+  default     = ".max(over='5m')"
+}
+
+variable "backend_status_max_delay" {
+  description = "Enforce max delay for backend_status detector (use \"0\" or \"null\" for \"Auto\")"
+  type        = number
+  default     = null
+}
+
+variable "backend_status_tip" {
+  description = "Suggested first course of action or any note useful for incident handling"
+  type        = string
+  default     = ""
+}
+
+variable "backend_status_runbook_url" {
+  description = "URL like SignalFx dashboard or wiki page which can help to troubleshoot the incident cause"
+  type        = string
+  default     = ""
+}
+
+variable "backend_status_disabled" {
+  description = "Disable all alerting rules for backend_status detector"
+  type        = bool
+  default     = null
+}
+
+variable "backend_status_threshold_critical" {
+  description = "Critical threshold for backend_status detector"
+  type        = number
+  default     = 1
+}
+
+variable "backend_status_lasting_duration_critical" {
+  description = "Minimum duration that conditions must be true before raising alert"
+  type        = string
+  default     = null
+}
+
+variable "backend_status_at_least_percentage_critical" {
+  description = "Percentage of lasting that conditions must be true before raising alert (>= 0.0 and <= 1.0)"
+  type        = number
+  default     = 1
+}
+# session_limit detector
+
+variable "session_limit_notifications" {
+  description = "Notification recipients list per severity overridden for session_limit detector"
+  type        = map(list(string))
+  default     = {}
+}
+
+variable "session_limit_aggregation_function" {
+  description = "Aggregation function and group by for session_limit detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ""
+}
+
+variable "session_limit_transformation_function" {
+  description = "Transformation function for session_limit detector (i.e. \".mean(over='5m')\")"
+  type        = string
+  default     = ".min(over='10m')"
+}
+
+variable "session_limit_max_delay" {
+  description = "Enforce max delay for session_limit detector (use \"0\" or \"null\" for \"Auto\")"
+  type        = number
+  default     = null
+}
+
+variable "session_limit_tip" {
+  description = "Suggested first course of action or any note useful for incident handling"
+  type        = string
+  default     = ""
+}
+
+variable "session_limit_runbook_url" {
+  description = "URL like SignalFx dashboard or wiki page which can help to troubleshoot the incident cause"
+  type        = string
+  default     = ""
+}
+
+variable "session_limit_disabled" {
+  description = "Disable all alerting rules for session_limit detector"
+  type        = bool
+  default     = null
+}
+
+variable "session_limit_disabled_critical" {
+  description = "Disable critical alerting rule for session_limit detector"
+  type        = bool
+  default     = null
+}
+
+variable "session_limit_disabled_major" {
+  description = "Disable major alerting rule for session_limit detector"
+  type        = bool
+  default     = null
+}
+
+variable "session_limit_threshold_critical" {
+  description = "Critical threshold for session_limit detector"
+  type        = number
+  default     = 90
+}
+
+variable "session_limit_lasting_duration_critical" {
+  description = "Minimum duration that conditions must be true before raising alert"
+  type        = string
+  default     = null
+}
+
+variable "session_limit_at_least_percentage_critical" {
+  description = "Percentage of lasting that conditions must be true before raising alert (>= 0.0 and <= 1.0)"
+  type        = number
+  default     = 1
+}
+variable "session_limit_threshold_major" {
+  description = "Major threshold for session_limit detector"
+  type        = number
+  default     = 80
+}
+
+variable "session_limit_lasting_duration_major" {
+  description = "Minimum duration that conditions must be true before raising alert"
+  type        = string
+  default     = null
+}
+
+variable "session_limit_at_least_percentage_major" {
+  description = "Percentage of lasting that conditions must be true before raising alert (>= 0.0 and <= 1.0)"
+  type        = number
+  default     = 1
+}
+# http_5xx_response detector
+
+variable "http_5xx_response_notifications" {
+  description = "Notification recipients list per severity overridden for http_5xx_response detector"
+  type        = map(list(string))
+  default     = {}
+}
+
+variable "http_5xx_response_aggregation_function" {
+  description = "Aggregation function and group by for http_5xx_response detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ""
+}
+
+variable "http_5xx_response_transformation_function" {
+  description = "Transformation function for http_5xx_response detector (i.e. \".mean(over='5m')\")"
+  type        = string
+  default     = ".min(over='10m')"
+}
+
+variable "http_5xx_response_max_delay" {
+  description = "Enforce max delay for http_5xx_response detector (use \"0\" or \"null\" for \"Auto\")"
+  type        = number
+  default     = null
+}
+
+variable "http_5xx_response_tip" {
+  description = "Suggested first course of action or any note useful for incident handling"
+  type        = string
+  default     = ""
+}
+
+variable "http_5xx_response_runbook_url" {
+  description = "URL like SignalFx dashboard or wiki page which can help to troubleshoot the incident cause"
+  type        = string
+  default     = ""
+}
+
+variable "http_5xx_response_disabled" {
+  description = "Disable all alerting rules for http_5xx_response detector"
+  type        = bool
+  default     = null
+}
+
+variable "http_5xx_response_disabled_critical" {
+  description = "Disable critical alerting rule for http_5xx_response detector"
+  type        = bool
+  default     = null
+}
+
+variable "http_5xx_response_disabled_major" {
+  description = "Disable major alerting rule for http_5xx_response detector"
+  type        = bool
+  default     = null
+}
+
+variable "http_5xx_response_threshold_critical" {
+  description = "Critical threshold for http_5xx_response detector"
+  type        = number
+  default     = 80
+}
+
+variable "http_5xx_response_lasting_duration_critical" {
+  description = "Minimum duration that conditions must be true before raising alert"
+  type        = string
+  default     = null
+}
+
+variable "http_5xx_response_at_least_percentage_critical" {
+  description = "Percentage of lasting that conditions must be true before raising alert (>= 0.0 and <= 1.0)"
+  type        = number
+  default     = 1
+}
+variable "http_5xx_response_threshold_major" {
+  description = "Major threshold for http_5xx_response detector"
+  type        = number
+  default     = 50
+}
+
+variable "http_5xx_response_lasting_duration_major" {
+  description = "Minimum duration that conditions must be true before raising alert"
+  type        = string
+  default     = null
+}
+
+variable "http_5xx_response_at_least_percentage_major" {
+  description = "Percentage of lasting that conditions must be true before raising alert (>= 0.0 and <= 1.0)"
+  type        = number
+  default     = 1
+}
+# http_4xx_response detector
+
+variable "http_4xx_response_notifications" {
+  description = "Notification recipients list per severity overridden for http_4xx_response detector"
+  type        = map(list(string))
+  default     = {}
+}
+
+variable "http_4xx_response_aggregation_function" {
+  description = "Aggregation function and group by for http_4xx_response detector (i.e. \".mean(by=['host'])\")"
+  type        = string
+  default     = ""
+}
+
+variable "http_4xx_response_transformation_function" {
+  description = "Transformation function for http_4xx_response detector (i.e. \".mean(over='5m')\")"
+  type        = string
+  default     = ".min(over='15m')"
+}
+
+variable "http_4xx_response_max_delay" {
+  description = "Enforce max delay for http_4xx_response detector (use \"0\" or \"null\" for \"Auto\")"
+  type        = number
+  default     = null
+}
+
+variable "http_4xx_response_tip" {
+  description = "Suggested first course of action or any note useful for incident handling"
+  type        = string
+  default     = ""
+}
+
+variable "http_4xx_response_runbook_url" {
+  description = "URL like SignalFx dashboard or wiki page which can help to troubleshoot the incident cause"
+  type        = string
+  default     = ""
+}
+
+variable "http_4xx_response_disabled" {
+  description = "Disable all alerting rules for http_4xx_response detector"
+  type        = bool
+  default     = null
+}
+
+variable "http_4xx_response_disabled_critical" {
+  description = "Disable critical alerting rule for http_4xx_response detector"
+  type        = bool
+  default     = null
+}
+
+variable "http_4xx_response_disabled_major" {
+  description = "Disable major alerting rule for http_4xx_response detector"
+  type        = bool
+  default     = null
+}
+
+variable "http_4xx_response_disabled_minor" {
+  description = "Disable minor alerting rule for http_4xx_response detector"
+  type        = bool
+  default     = null
+}
+
+variable "http_4xx_response_threshold_critical" {
+  description = "Critical threshold for http_4xx_response detector"
+  type        = number
+  default     = 99
+}
+
+variable "http_4xx_response_lasting_duration_critical" {
+  description = "Minimum duration that conditions must be true before raising alert"
+  type        = string
+  default     = null
+}
+
+variable "http_4xx_response_at_least_percentage_critical" {
+  description = "Percentage of lasting that conditions must be true before raising alert (>= 0.0 and <= 1.0)"
+  type        = number
+  default     = 1
+}
+variable "http_4xx_response_threshold_major" {
+  description = "Major threshold for http_4xx_response detector"
+  type        = number
+  default     = 95
+}
+
+variable "http_4xx_response_lasting_duration_major" {
+  description = "Minimum duration that conditions must be true before raising alert"
+  type        = string
+  default     = null
+}
+
+variable "http_4xx_response_at_least_percentage_major" {
+  description = "Percentage of lasting that conditions must be true before raising alert (>= 0.0 and <= 1.0)"
+  type        = number
+  default     = 1
+}
+variable "http_4xx_response_threshold_minor" {
+  description = "Minor threshold for http_4xx_response detector"
+  type        = number
+  default     = 90
+}
+
+variable "http_4xx_response_lasting_duration_minor" {
+  description = "Minimum duration that conditions must be true before raising alert"
+  type        = string
+  default     = null
+}
+
+variable "http_4xx_response_at_least_percentage_minor" {
+  description = "Percentage of lasting that conditions must be true before raising alert (>= 0.0 and <= 1.0)"
+  type        = number
+  default     = 1
+}
