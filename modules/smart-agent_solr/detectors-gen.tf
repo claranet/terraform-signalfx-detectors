c5c84b8374618a7eacc22f0cd8787998c73e125c
Jean-Baptiste Simillon <jb.simillon@fr.clara.net>
2023-12-07 16:20:40 +0100

    feat : convert module smart-agent_solr to generator format

--- modules/smart-agent_solr/detectors-gen.tf
+++ modules/smart-agent_solr/detectors-gen.tf
@@ -7,7 +7,7 @@ resource "signalfx_detector" "heartbeat" {
 
   program_text = <<-EOF
     from signalfx.detectors.not_reporting import not_reporting
-    signal = data('counter.solr.http_requests', filter=${local.not_running_vm_filters} and ${module.filtering.signalflow})${var.heartbeat_aggregation_function}.publish('signal')
+    signal = data('counter.solr.http_requests', filter=${local.not_running_vm_filters} and ${module.filtering.signalflow})${var.heartbeat_aggregation_function}${var.heartbeat_transformation_function}.publish('signal')
     not_reporting.detector(stream=signal, resource_identifier=None, duration='${var.heartbeat_timeframe}', auto_resolve_after='${local.heartbeat_auto_resolve_after}').publish('CRIT')
 EOF
 
@@ -35,8 +35,8 @@ resource "signalfx_detector" "errors" {
 
   program_text = <<-EOF
     signal = data('counter.solr.zookeeper_errors', filter=${module.filtering.signalflow})${var.errors_aggregation_function}${var.errors_transformation_function}.publish('signal')
-    detect(when(signal >= ${var.errors_threshold_critical})).publish('CRIT')
-    detect(when(signal >= ${var.errors_threshold_major}) and (not when(signal >= ${var.errors_threshold_critical}))).publish('MAJOR')
+    detect(when(signal >= ${var.errors_threshold_critical}%{if var.errors_lasting_duration_critical != null}, lasting='${var.errors_lasting_duration_critical}', at_least=${var.errors_at_least_percentage_critical}%{endif})).publish('CRIT')
+    detect(when(signal >= ${var.errors_threshold_major}%{if var.errors_lasting_duration_major != null}, lasting='${var.errors_lasting_duration_major}', at_least=${var.errors_at_least_percentage_major}%{endif}) and (not when(signal >= ${var.errors_threshold_critical}%{if var.errors_lasting_duration_critical != null}, lasting='${var.errors_lasting_duration_critical}', at_least=${var.errors_at_least_percentage_critical}%{endif}))).publish('MAJOR')
 EOF
 
   rule {
@@ -75,8 +75,8 @@ resource "signalfx_detector" "searcher_warmup_time" {
 
   program_text = <<-EOF
     signal = data('gauge.solr.searcher_warmup', filter=${module.filtering.signalflow})${var.searcher_warmup_time_aggregation_function}${var.searcher_warmup_time_transformation_function}.publish('signal')
-    detect(when(signal >= ${var.searcher_warmup_time_threshold_critical})).publish('CRIT')
-    detect(when(signal >= ${var.searcher_warmup_time_threshold_major}) and (not when(signal >= ${var.searcher_warmup_time_threshold_critical}))).publish('MAJOR')
+    detect(when(signal >= ${var.searcher_warmup_time_threshold_critical}%{if var.searcher_warmup_time_lasting_duration_critical != null}, lasting='${var.searcher_warmup_time_lasting_duration_critical}', at_least=${var.searcher_warmup_time_at_least_percentage_critical}%{endif})).publish('CRIT')
+    detect(when(signal >= ${var.searcher_warmup_time_threshold_major}%{if var.searcher_warmup_time_lasting_duration_major != null}, lasting='${var.searcher_warmup_time_lasting_duration_major}', at_least=${var.searcher_warmup_time_at_least_percentage_major}%{endif}) and (not when(signal >= ${var.searcher_warmup_time_threshold_critical}%{if var.searcher_warmup_time_lasting_duration_critical != null}, lasting='${var.searcher_warmup_time_lasting_duration_critical}', at_least=${var.searcher_warmup_time_at_least_percentage_critical}%{endif}))).publish('MAJOR')
 EOF
 
   rule {
