ca0b9b0e16c41ff47983759b9557c3d756f1e191
Jean-Baptiste Simillon <jb.simillon@fr.clara.net>
2023-12-08 10:27:14 +0100

    feat : convert module integration_aws-rds-aurora-mysql to generator format

--- modules/integration_aws-rds-aurora-mysql/detectors-gen.tf
+++ modules/integration_aws-rds-aurora-mysql/detectors-gen.tf
@@ -6,9 +6,10 @@ resource "signalfx_detector" "aurora_mysql_replica_lag" {
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
   program_text = <<-EOF
-    signal = data('AuroraReplicaLag', filter=filter('namespace', 'AWS/RDS') and filter('stat', 'mean') and filter('DBInstanceIdentifier', '*') and ${module.filtering.signalflow})${var.aurora_mysql_replica_lag_aggregation_function}${var.aurora_mysql_replica_lag_transformation_function}.publish('signal')
-    detect(when(signal > ${var.aurora_mysql_replica_lag_threshold_critical})).publish('CRIT')
-    detect(when(signal > ${var.aurora_mysql_replica_lag_threshold_major}) and (not when(signal > ${var.aurora_mysql_replica_lag_threshold_critical}))).publish('MAJOR')
+    base_filtering = filter('namespace', 'AWS/RDS') and filter('stat', 'mean') and filter('DBInstanceIdentifier', '*')
+    signal = data('AuroraReplicaLag', filter=base_filtering and ${module.filtering.signalflow})${var.aurora_mysql_replica_lag_aggregation_function}${var.aurora_mysql_replica_lag_transformation_function}.publish('signal')
+    detect(when(signal > ${var.aurora_mysql_replica_lag_threshold_critical}%{if var.aurora_mysql_replica_lag_lasting_duration_critical != null}, lasting='${var.aurora_mysql_replica_lag_lasting_duration_critical}', at_least=${var.aurora_mysql_replica_lag_at_least_percentage_critical}%{endif})).publish('CRIT')
+    detect(when(signal > ${var.aurora_mysql_replica_lag_threshold_major}%{if var.aurora_mysql_replica_lag_lasting_duration_major != null}, lasting='${var.aurora_mysql_replica_lag_lasting_duration_major}', at_least=${var.aurora_mysql_replica_lag_at_least_percentage_major}%{endif}) and (not when(signal > ${var.aurora_mysql_replica_lag_threshold_critical}%{if var.aurora_mysql_replica_lag_lasting_duration_critical != null}, lasting='${var.aurora_mysql_replica_lag_lasting_duration_critical}', at_least=${var.aurora_mysql_replica_lag_at_least_percentage_critical}%{endif}))).publish('MAJOR')
 EOF
 
   rule {
