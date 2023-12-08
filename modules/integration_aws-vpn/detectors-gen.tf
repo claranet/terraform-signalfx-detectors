22647dae1f09576f1efb8421e2243a6d7157dc26
Jean-Baptiste Simillon <jb.simillon@fr.clara.net>
2023-12-08 10:34:01 +0100

    feat : convert module integration_aws-vpn to generator format

--- modules/integration_aws-vpn/detectors-gen.tf
+++ modules/integration_aws-vpn/detectors-gen.tf
@@ -7,7 +7,8 @@ resource "signalfx_detector" "heartbeat" {
 
   program_text = <<-EOF
     from signalfx.detectors.not_reporting import not_reporting
-    signal = data('TunnelState', filter=filter('stat', 'mean') and filter('namespace', 'AWS/VPN') and ${module.filtering.signalflow})${var.heartbeat_aggregation_function}.publish('signal')
+    base_filtering = filter('stat', 'mean') and filter('namespace', 'AWS/VPN')
+    signal = data('TunnelState', filter=base_filtering and ${module.filtering.signalflow})${var.heartbeat_aggregation_function}${var.heartbeat_transformation_function}.publish('signal')
     not_reporting.detector(stream=signal, resource_identifier=None, duration='${var.heartbeat_timeframe}', auto_resolve_after='${local.heartbeat_auto_resolve_after}').publish('CRIT')
 EOF
 
@@ -26,7 +27,7 @@ EOF
   max_delay = var.heartbeat_max_delay
 }
 
-resource "signalfx_detector" "VPN_status" {
+resource "signalfx_detector" "vpn_status" {
   name = format("%s %s", local.detector_name_prefix, "AWS VPN tunnel state")
 
   authorized_writer_teams = var.authorized_writer_teams
@@ -34,15 +35,16 @@ resource "signalfx_detector" "VPN_status" {
   tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
 
   program_text = <<-EOF
-    signal = data('TunnelState', filter=filter('namespace', 'AWS/VPN') and filter('stat', 'mean') and filter('VpnId', '*') and ${module.filtering.signalflow})${var.vpn_status_aggregation_function}${var.vpn_status_transformation_function}.publish('signal')
-    detect(when(signal < ${var.vpn_status_threshold_critical})).publish('CRIT')
+    base_filtering = filter('namespace', 'AWS/VPN') and filter('stat', 'mean') and filter('VpnId', '*')
+    signal = data('TunnelState', filter=base_filtering and ${module.filtering.signalflow})${var.vpn_status_aggregation_function}${var.vpn_status_transformation_function}.publish('signal')
+    detect(when(signal < ${var.vpn_status_threshold_critical}%{if var.vpn_status_lasting_duration_critical != null}, lasting='${var.vpn_status_lasting_duration_critical}', at_least=${var.vpn_status_at_least_percentage_critical}%{endif})).publish('CRIT')
 EOF
 
   rule {
-    description           = "is reporting a state other than up"
+    description           = "is too low < ${var.vpn_status_threshold_critical}"
     severity              = "Critical"
     detect_label          = "CRIT"
-    disabled              = coalesce(var.vpn_status_disabled_critical, var.vpn_status_disabled, var.detectors_disabled)
+    disabled              = coalesce(var.vpn_status_disabled, var.detectors_disabled)
     notifications         = try(coalescelist(lookup(var.vpn_status_notifications, "critical", []), var.notifications.critical), null)
     runbook_url           = try(coalesce(var.vpn_status_runbook_url, var.runbook_url), "")
     tip                   = var.vpn_status_tip
