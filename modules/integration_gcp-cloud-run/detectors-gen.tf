96cd29a0e1a76b44721f6464b80864fb576a8a93
nhat do <nhat.do@fr.clara.net>
2024-06-27 17:56:20 +0200

    fix duplicate entries variables

--- modules/integration_gcp-cloud-run/detectors-gen.tf
+++ modules/integration_gcp-cloud-run/detectors-gen.tf
@@ -72,52 +72,6 @@ EOF
   max_delay = var.cpu_usage_max_delay
 }
 
-resource "signalfx_detector" "cpu_usage" {
-  name = format("%s %s", local.detector_name_prefix, "GCP Cloud Run cpu usage")
-
-  authorized_writer_teams = var.authorized_writer_teams
-  teams                   = try(coalescelist(var.teams, var.authorized_writer_teams), null)
-  tags                    = compact(concat(local.common_tags, local.tags, var.extra_tags))
-
-  viz_options {
-    label        = "signal"
-    value_suffix = "%"
-  }
-
-  program_text = <<-EOF
-    A = data('container/cpu/utilizations', filter=${module.filtering.signalflow}, extrapolation='zero')${var.cpu_usage_aggregation_function}${var.cpu_usage_transformation_function}
-    signal = A.scale(100).publish('signal')
-    detect(when(signal > ${var.cpu_usage_threshold_critical}%{if var.cpu_usage_lasting_duration_critical != null}, lasting='${var.cpu_usage_lasting_duration_critical}', at_least=${var.cpu_usage_at_least_percentage_critical}%{endif})).publish('CRIT')
-    detect(when(signal > ${var.cpu_usage_threshold_major}%{if var.cpu_usage_lasting_duration_major != null}, lasting='${var.cpu_usage_lasting_duration_major}', at_least=${var.cpu_usage_at_least_percentage_major}%{endif}) and (not when(signal > ${var.cpu_usage_threshold_critical}%{if var.cpu_usage_lasting_duration_critical != null}, lasting='${var.cpu_usage_lasting_duration_critical}', at_least=${var.cpu_usage_at_least_percentage_critical}%{endif}))).publish('MAJOR')
-EOF
-
-  rule {
-    description           = "is too high > ${var.cpu_usage_threshold_critical}%"
-    severity              = "Critical"
-    detect_label          = "CRIT"
-    disabled              = coalesce(var.cpu_usage_disabled_critical, var.cpu_usage_disabled, var.detectors_disabled)
-    notifications         = try(coalescelist(lookup(var.cpu_usage_notifications, "critical", []), var.notifications.critical), null)
-    runbook_url           = try(coalesce(var.cpu_usage_runbook_url, var.runbook_url), "")
-    tip                   = var.cpu_usage_tip
-    parameterized_subject = var.message_subject == "" ? local.rule_subject : var.message_subject
-    parameterized_body    = var.message_body == "" ? local.rule_body : var.message_body
-  }
-
-  rule {
-    description           = "is too high > ${var.cpu_usage_threshold_major}%"
-    severity              = "Major"
-    detect_label          = "MAJOR"
-    disabled              = coalesce(var.cpu_usage_disabled_major, var.cpu_usage_disabled, var.detectors_disabled)
-    notifications         = try(coalescelist(lookup(var.cpu_usage_notifications, "major", []), var.notifications.major), null)
-    runbook_url           = try(coalesce(var.cpu_usage_runbook_url, var.runbook_url), "")
-    tip                   = var.cpu_usage_tip
-    parameterized_subject = var.message_subject == "" ? local.rule_subject : var.message_subject
-    parameterized_body    = var.message_body == "" ? local.rule_body : var.message_body
-  }
-
-  max_delay = var.cpu_usage_max_delay
-}
-
 resource "signalfx_detector" "connection_refused_to_sql_ratio" {
   name = format("%s %s", local.detector_name_prefix, "GCP Cloud Run connection refused to sql ratio")
 
