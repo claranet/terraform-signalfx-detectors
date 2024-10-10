f2c1a829e57e2be7ee710c30c54e3048baf49912
Julien LE SAUX <julien.le.saux@fr.clara.net>
2024-10-10 17:40:04 +0200

    fix: variables

---
+++ modules/integration_aws-direct-connect/variables.tf
@@ -0,0 +1,9 @@
+variable "connection_id" {
+  description = "The Direct Connect Connection ID"
+  type        = string
+}
+
+variable "virtual_interface_id" {
+  description = "The Direct Connect Virtual Interface ID"
+  type        = string
+}
