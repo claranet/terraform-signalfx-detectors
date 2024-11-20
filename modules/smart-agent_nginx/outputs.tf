42201eaaf80e7c31398a1dbf5b6f1fc04090449d
Florian Tremblot <florian.tremblot@claranet.com>
2024-11-20 15:44:18 +0100

    remove detector & update README

--- modules/smart-agent_nginx/outputs.tf
+++ modules/smart-agent_nginx/outputs.tf
@@ -1,8 +1,3 @@
-output "dropped_connections" {
-  description = "Detector resource for dropped_connections"
-  value       = signalfx_detector.dropped_connections
-}
-
 output "heartbeat" {
   description = "Detector resource for heartbeat"
   value       = signalfx_detector.heartbeat
